b015289650dd8a1b47cb0e883b5e6266ac64dd3a
Chuck Ritola <cjritola@gmail.com>
2013-12-20 21:30:11 -0500

    Added Velocible and VelocityDragged

---
+++ src/main/java/org/jtrfp/trcl/objects/Velocible.java
@@ -0,0 +1,8 @@
+package org.jtrfp.trcl.objects;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+public interface Velocible
+	{public void setVelocity(Vector3D vel);
+	public Vector3D getVelocity();
+	}
---
+++ src/main/java/org/jtrfp/trcl/objects/VelocityDragged.java
@@ -0,0 +1,6 @@
+package org.jtrfp.trcl.objects;
+
+public interface VelocityDragged
+	{public void setVelocityDrag(double dragCoefficientPerSecond);
+	public double getVelocityDrag();
+	}

a2567e2bf7e6ee251a247057ce68a0e49afbddb5
Chuck Ritola <cjritola@gmail.com>
2013-12-20 20:09:50 -0500

    Cleanup

--- src/main/java/org/jtrfp/trcl/ai/BouncesOffTunnelWalls.java
+++ src/main/java/org/jtrfp/trcl/ai/BouncesOffTunnelWalls.java
@@ -26,12 +26,13 @@ public class BouncesOffTunnelWalls <T extends RigidMobileObject> extends ObjectB
 	public BouncesOffTunnelWalls(ObjectBehavior<?> wrapped)
 		{super(wrapped);}
 	
-	protected void _proposeCollision(WorldObject other)
-		{if(other instanceof TunnelSegment)
+	protected void _proposeCollision(WorldObject other){
+		final T parent = getParent();
+		if(other instanceof TunnelSegment)
 			{TunnelSegment seg=(TunnelSegment)other;
-			if(getParent().getPosition().
+			if(parent.getPosition().
 					getX()>seg.getPosition().getX()&&
-					getParent().getPosition().getX()<seg.getPosition().getX()+seg.getSegmentLength())
+					parent.getPosition().getX()<seg.getPosition().getX()+seg.getSegmentLength())
 				{final Segment s = seg.getSegmentData();
 				final double segLen=seg.getSegmentLength();
 				final Vector3D start =seg.getPosition();
@@ -39,10 +40,10 @@ public class BouncesOffTunnelWalls <T extends RigidMobileObject> extends ObjectB
 				final Vector3D tunnelSpineNoNorm=end.subtract(start);
 				final Vector3D tunnelSpineNorm=tunnelSpineNoNorm.normalize();
 				
-				final double depthDownSeg=start.getX()-getParent().getPosition().getX();
+				final double depthDownSeg=start.getX()-parent.getPosition().getX();
 				final double pctDownSeg=depthDownSeg/segLen;
 				final Vector3D circleCenter=
-						start.add(tunnelSpineNorm.scalarMultiply(getParent().getPosition().getX()-start.getX()));
+						start.add(tunnelSpineNorm.scalarMultiply(parent.getPosition().getX()-start.getX()));
 				final double startWidth=TunnelSegment.getStartWidth(s);
 				final double startHeight=TunnelSegment.getStartHeight(s);
 				final double endWidth=TunnelSegment.getEndWidth(s);
@@ -51,23 +52,22 @@ public class BouncesOffTunnelWalls <T extends RigidMobileObject> extends ObjectB
 				final double widthHere=.8*(startWidth*(1.-pctDownSeg)+endWidth*pctDownSeg);
 				final double heightHere=.8*(startHeight*(1.-pctDownSeg)+endHeight*pctDownSeg);
 				//Parent position relative to tunnel
-				final Vector3D pprtt = getParent().getPosition().subtract(circleCenter);
-				if((pprtt.getZ()*pprtt.getZ())/(widthHere*widthHere)+(pprtt.getY()*pprtt.getY())/(heightHere*heightHere)>1)
-					{//System.out.println("TUNNEL COLLISION!!! player="+getParent()+" circleCenter="+circleCenter);
+				final Vector3D pprtt = parent.getPosition().subtract(circleCenter);
+				if((pprtt.getZ()*pprtt.getZ())/(widthHere*widthHere)+(pprtt.getY()*pprtt.getY())/(heightHere*heightHere)>1){
 					//Execute the "bounce"
-					final Vector3D oldPosition = getParent().getPosition();
-					getParent().setPosition(circleCenter.scalarMultiply(.2).add(oldPosition.scalarMultiply(.8)));
-					final Vector3D oldHeading = getParent().getHeading();
-					final Vector3D oldVelocity = getParent().getVelocity();
-					final Vector3D oldTop = getParent().getTop();
+					final Vector3D oldPosition = parent.getPosition();
+					parent.setPosition(circleCenter.scalarMultiply(.2).add(oldPosition.scalarMultiply(.8)));
+					final Vector3D oldHeading = parent.getHeading();
+					final Vector3D oldVelocity = parent.getVelocity();
+					final Vector3D oldTop = parent.getTop();
 					final Vector3D inwardNormal = circleCenter.subtract(oldPosition).normalize().negate();
 					//Bounce the heading and velocity
-					getParent().setHeading((inwardNormal.scalarMultiply(inwardNormal.dotProduct(oldHeading)*2).subtract(oldHeading)).negate());
-					getParent().setVelocity((inwardNormal.scalarMultiply(inwardNormal.dotProduct(oldHeading)*2).subtract(oldHeading)).negate());
-					//getParent().setTop((inwardNormal.scalarMultiply(inwardNormal.dotProduct(oldTop)*2).subtract(oldTop)));
-					//getParent().setHeading((inwardHeading.scalarMultiply(.5).add(oldHeading.scalarMultiply(.5))).normalize());
-					//getParent().setTop(getParent().getTop().crossProduct(getParent().getHeading()));
-					//getParent().setHeading(new Vector3D(oldHeading.getX()>0?oldHeading.getX():.00001,oldHeading.getY()*-1,oldHeading.getZ()*-1).normalize());
+					parent.setHeading((inwardNormal.scalarMultiply(inwardNormal.dotProduct(oldHeading)*2).subtract(oldHeading)).negate());
+					parent.setVelocity((inwardNormal.scalarMultiply(inwardNormal.dotProduct(oldHeading)*2).subtract(oldHeading)).negate());
+					//parent.setTop((inwardNormal.scalarMultiply(inwardNormal.dotProduct(oldTop)*2).subtract(oldTop)));
+					//parent.setHeading((inwardHeading.scalarMultiply(.5).add(oldHeading.scalarMultiply(.5))).normalize());
+					//parent.setTop(parent.getTop().crossProduct(parent.getHeading()));
+					//parent.setHeading(new Vector3D(oldHeading.getX()>0?oldHeading.getX():.00001,oldHeading.getY()*-1,oldHeading.getZ()*-1).normalize());
 					}
 				else{seg.setVisible(true);}
 				}//end if(in range of segment)

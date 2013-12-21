7e64d7f8a1f09f2f015a4fd200e9194c20dd8135
Chuck Ritola <cjritola@gmail.com>
2013-12-20 19:44:03 -0500

    Created GameplayObject

---
+++ src/main/java/org/jtrfp/trcl/objects/GameplayObject.java
@@ -0,0 +1,36 @@
+package org.jtrfp.trcl.objects;
+
+import org.jtrfp.trcl.Model;
+import org.jtrfp.trcl.World;
+import org.jtrfp.trcl.ai.ObjectBehavior;
+
+public class GameplayObject extends RigidMobileObject implements Damageable
+	{
+	private int health;
+	public GameplayObject(Model model, ObjectBehavior behavior, World world)
+		{
+		super(model, behavior, world);
+		health=65535;
+		}
+	
+	@Override
+	public void damage(int dmg)
+		{health-=dmg;
+		}
+
+	@Override
+	public int getHealth()
+		{return health;
+		}
+
+	@Override
+	public void unDamage(int amt)
+		{health+=amt;
+		if(health>65535)health=65535;
+		}
+
+	@Override
+	public void unDamage()
+		{health=65535;}
+
+	}//end GameplayObject

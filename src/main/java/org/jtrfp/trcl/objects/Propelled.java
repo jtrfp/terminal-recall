5ed12fa1f91d5dc09ffbae9120c7ba27933f6e4e
Chuck Ritola <cjritola@gmail.com>
2013-12-22 01:34:41 -0500

    Added value boundaries

--- src/main/java/org/jtrfp/trcl/objects/Propelled.java
+++ src/main/java/org/jtrfp/trcl/objects/Propelled.java
@@ -3,4 +3,9 @@ package org.jtrfp.trcl.objects;
 public interface Propelled
 	{public void setPropulsion(double magnitude);
 	public double getPropulsion();
+	public void setMaxPropulsion(double max);
+	public double getMaxPropulsion();
+	public void setMinPropulsion(double min);
+	public double getMinPropulsion();
+	public void deltaPropulsion(double delta);
 	}

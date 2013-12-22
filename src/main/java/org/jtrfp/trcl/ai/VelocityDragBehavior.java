343523462fe1630f31ca28c835d8da69216cd23c
Chuck Ritola <cjritola@gmail.com>
2013-12-22 01:34:17 -0500

    Finished implementation

--- src/main/java/org/jtrfp/trcl/ai/VelocityDragBehavior.java
+++ src/main/java/org/jtrfp/trcl/ai/VelocityDragBehavior.java
@@ -1,5 +1,6 @@
 package org.jtrfp.trcl.ai;
 
+import org.jtrfp.trcl.objects.Velocible;
 import org.jtrfp.trcl.objects.VelocityDragged;
 
 public class VelocityDragBehavior extends ObjectBehavior implements
@@ -13,5 +14,16 @@ public class VelocityDragBehavior extends ObjectBehavior implements
 	@Override
 	public double getVelocityDrag()
 		{return dragCoeff;}
+	
+	@Override
+	public void _tick(long tickTimeMillis){
+	    	final double timeProgressed=((double)getParent().getTr().getThreadManager().getElapsedTimeInMillisSinceLastGameTick()/1000.);
+	    	if(timeProgressed<=0)return;
+	    	Velocible v=getParent().getBehavior().probeForBehavior(Velocible.class);
+	    	//System.out.println(dragCoeff);
+	    	final double slowdown = 1-dragCoeff;
+	    	final double timeAdjSlowdown=1-(slowdown*timeProgressed);
+	    	v.setVelocity(v.getVelocity().scalarMultiply(Math.pow(timeAdjSlowdown,4)));
+		}
 
 	}//end VelocityDragBehavior

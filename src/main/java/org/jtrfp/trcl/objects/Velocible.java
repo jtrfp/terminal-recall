1c1f0e853eec4a022381f3cb79d396ac280e9671
Chuck Ritola <cjritola@gmail.com>
2013-12-21 20:53:52 -0500

    Added accelleration

--- src/main/java/org/jtrfp/trcl/objects/Velocible.java
+++ src/main/java/org/jtrfp/trcl/objects/Velocible.java
@@ -5,4 +5,5 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 public interface Velocible
 	{public void setVelocity(Vector3D vel);
 	public Vector3D getVelocity();
+	public void accellerate(Vector3D scalarMultiply);
 	}

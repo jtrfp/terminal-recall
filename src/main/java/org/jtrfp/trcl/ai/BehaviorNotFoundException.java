34761538891565ad2df1c08f57df27ec23c2f585
Chuck Ritola <cjritola@gmail.com>
2013-12-21 20:53:08 -0500

    Added BehaviorNotFoundException

---
+++ src/main/java/org/jtrfp/trcl/ai/BehaviorNotFoundException.java
@@ -0,0 +1,7 @@
+package org.jtrfp.trcl.ai;
+
+public class BehaviorNotFoundException extends RuntimeException
+	{
+	public BehaviorNotFoundException(){super();}
+	public BehaviorNotFoundException(String msg){super(msg);}
+	}

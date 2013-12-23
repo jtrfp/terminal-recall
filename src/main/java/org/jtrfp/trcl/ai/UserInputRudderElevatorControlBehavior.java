74c08aa8990180db52c840ce3822f0be5f9eebb1
Chuck Ritola <cjritola@gmail.com>
2013-12-22 22:49:24 -0500

    Fixed locked-velocity bug

--- src/main/java/org/jtrfp/trcl/ai/UserInputRudderElevatorControlBehavior.java
+++ src/main/java/org/jtrfp/trcl/ai/UserInputRudderElevatorControlBehavior.java
@@ -6,31 +6,41 @@ import org.jtrfp.trcl.KeyStatus;
 import org.jtrfp.trcl.objects.Player;
 
 public class UserInputRudderElevatorControlBehavior extends ObjectBehavior {
+    private  double accellerationFactor=.0005;
     @Override
     public void _tick(long tickTimeMillis){
 	final Player p = (Player)getParent();
 	final KeyStatus keyStatus = p.getTr().getKeyStatus();
 	final RotationalMomentumBehavior rmb = p.getBehavior().probeForBehavior(RotationalMomentumBehavior.class);
-	rmb.setEquatorialMomentum(0);
-	rmb.setLateralMomentum(0);
-	rmb.setPolarMomentum(0);
 	if (keyStatus.isPressed(KeyEvent.VK_UP)){
-		rmb.accelleratePolarMomentum(-2.*Math.PI*.003);
+		rmb.accelleratePolarMomentum(-2.*Math.PI*accellerationFactor);
 		}
 	if (keyStatus.isPressed(KeyEvent.VK_DOWN)){
-	    	rmb.accelleratePolarMomentum(2.*Math.PI*.003);
+	    	rmb.accelleratePolarMomentum(2.*Math.PI*accellerationFactor);
 		}
 	if (keyStatus.isPressed(KeyEvent.VK_LEFT)){
 	    	//Tilt
-		rmb.accellerateLateralMomentum(-2.*Math.PI*.003);
+		rmb.accellerateLateralMomentum(-2.*Math.PI*accellerationFactor);
 		//Turn
-		rmb.accellerateEquatorialMomentum(2*Math.PI*.003);
+		rmb.accellerateEquatorialMomentum(2*Math.PI*accellerationFactor);
 		}
 	if (keyStatus.isPressed(KeyEvent.VK_RIGHT)){
 	    	//Tilt
-		rmb.accellerateLateralMomentum(2.*Math.PI*.003);
+		rmb.accellerateLateralMomentum(2.*Math.PI*accellerationFactor);
 		//Turn
-		rmb.accellerateEquatorialMomentum(-2*Math.PI*.003);
+		rmb.accellerateEquatorialMomentum(-2*Math.PI*accellerationFactor);
 		}
     }//end UserInputRudderElevatorControlBehavior
+    /**
+     * @return the accellerationFactor
+     */
+    public double getAccellerationFactor() {
+        return accellerationFactor;
+    }
+    /**
+     * @param accellerationFactor the accellerationFactor to set
+     */
+    public void setAccellerationFactor(double accellerationFactor) {
+        this.accellerationFactor = accellerationFactor;
+    }
 }

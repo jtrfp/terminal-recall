28add1aa3e6dfb39836547861fe84441f97c3758
Chuck Ritola <cjritola@gmail.com>
2013-12-27 11:40:36 -0500

    Added DEF Object collision handling.

---
+++ src/main/java/org/jtrfp/trcl/beh/CollidesWithDEFObjects.java
@@ -0,0 +1,31 @@
+package org.jtrfp.trcl.beh;
+
+import org.jtrfp.trcl.AbstractSubmitter;
+import org.jtrfp.trcl.Submitter;
+import org.jtrfp.trcl.obj.DEFObject;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class CollidesWithDEFObjects extends Behavior {
+    private final double boundingRadius;
+    private DEFObject otherDEF;
+    public CollidesWithDEFObjects(double boundingRadius){
+	this.boundingRadius=boundingRadius;
+    }
+    @Override
+    public void _proposeCollision(WorldObject other){
+	if(other instanceof DEFObject){
+	    final double distance=other.getPosition().distance(getParent().getPosition());
+	    otherDEF=(DEFObject)other;
+	    if(distance<(boundingRadius+otherDEF.getBoundingRadius())){
+		getParent().getBehavior().probeForBehaviors(sub, DEFObjectCollisionListener.class);
+	    }
+	}
+    }//end _proposeCollision()
+    
+    private final Submitter<DEFObjectCollisionListener> sub = new AbstractSubmitter<DEFObjectCollisionListener>(){
+	@Override
+	public void submit(DEFObjectCollisionListener l){
+	    l.collidedWithDEFObject(otherDEF);
+	}//end submit(...)
+    };//end Submitter
+}
---
+++ src/main/java/org/jtrfp/trcl/beh/DEFObjectCollisionListener.java
@@ -0,0 +1,9 @@
+package org.jtrfp.trcl.beh;
+
+import org.jtrfp.trcl.obj.DEFObject;
+
+public interface DEFObjectCollisionListener {
+
+    void collidedWithDEFObject(DEFObject other);
+
+}

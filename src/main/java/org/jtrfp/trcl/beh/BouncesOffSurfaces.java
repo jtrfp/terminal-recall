072e0a27cb551310ab67f99a1b1fa7ce36b92f2b
Chuck Ritola <cjritola@gmail.com>
2013-12-30 10:54:47 -0500

    Added angle checking to filter unnecessary bounce.

--- src/main/java/org/jtrfp/trcl/beh/BouncesOffSurfaces.java
+++ src/main/java/org/jtrfp/trcl/beh/BouncesOffSurfaces.java
@@ -8,26 +8,46 @@ import org.jtrfp.trcl.obj.WorldObject;
 
 public class BouncesOffSurfaces extends Behavior implements
 	SurfaceImpactListener {
+    private boolean reflectHeading=true;
 
     @Override
     public void collidedWithSurface(WorldObject wo, Vector3D surfaceNormal) {
 	final WorldObject parent = getParent();
 	final Vector3D oldHeading = parent.getHeading();
 	final Vector3D oldTop = parent.getTop();
-	Vector3D newHeading = (surfaceNormal.scalarMultiply(surfaceNormal.dotProduct(oldHeading)*-2).add(oldHeading));
-	parent.setHeading(newHeading);
-	final Rotation resultingRotation = new Rotation(oldHeading,newHeading);
-	Vector3D newTop = resultingRotation.applyTo(oldTop);
-	if(newTop.getY()<0)newTop=newTop.negate();
-	parent.setTop(newTop);
+	if(reflectHeading && new Rotation(oldHeading,surfaceNormal).getAngle()>Math.PI/2.){
+	    Vector3D newHeading = (surfaceNormal.scalarMultiply(surfaceNormal.dotProduct(oldHeading)*-2).add(oldHeading));
+	    parent.setHeading(newHeading);
+	    final Rotation resultingRotation = new Rotation(oldHeading,newHeading);
+	    Vector3D newTop = resultingRotation.applyTo(oldTop);
+	    if(newTop.getY()<0)newTop=newTop.negate();
+	    System.out.println("reflecting heading of "+parent.getClass().getName());
+	    parent.setTop(newTop);
+	}//end if(should reflect)
 	if(parent instanceof Velocible){
 	    final Velocible velocible = (Velocible)parent;
 	    final Vector3D oldVelocity = velocible.getVelocity();
-	    
-	    velocible.setVelocity((surfaceNormal.scalarMultiply(surfaceNormal.dotProduct(oldVelocity)*-2).add(oldVelocity)));
-	    //Nudge
-	    parent.setPosition(parent.getPosition().add(velocible.getVelocity().scalarMultiply(2.)));
-	}
+	    if(new Rotation(oldVelocity.normalize(),surfaceNormal).getAngle()>Math.PI/2.){
+		velocible.setVelocity((surfaceNormal.scalarMultiply(surfaceNormal.dotProduct(oldVelocity)*-2).add(oldVelocity)));
+		//Nudge
+		//parent.setPosition(parent.getPosition().add(velocible.getVelocity().scalarMultiply(100.)));
+	    }//end if(should bounce)
+	}//end if(Velocible)
     }//end collidedWithSurface(...)
+
+    /**
+     * @return the reflectHeading
+     */
+    public boolean isReflectHeading() {
+        return reflectHeading;
+    }
+
+    /**
+     * @param reflectHeading the reflectHeading to set
+     */
+    public BouncesOffSurfaces setReflectHeading(boolean reflectHeading) {
+        this.reflectHeading = reflectHeading;
+        return this;
+    }
 
 }

1f999f399debdbeb797bddf670b58802a20aba25
Chuck Ritola <cjritola@gmail.com>
2013-12-30 09:14:11 -0500

    Bigfix: Incorrectly set countdown

--- src/main/java/org/jtrfp/trcl/beh/LimitedLifeSpan.java
+++ src/main/java/org/jtrfp/trcl/beh/LimitedLifeSpan.java
@@ -1,15 +1,20 @@
 package org.jtrfp.trcl.beh;
 
+import org.jtrfp.trcl.obj.WorldObject;
+
 public class LimitedLifeSpan extends Behavior {
     private long timeRemainingMillis=Long.MAX_VALUE;
     @Override
     public void _tick(long tickTimeInMillis){
-	timeRemainingMillis-=getParent().getTr().getThreadManager().getElapsedTimeInMillisSinceLastGameTick();
-	if(timeRemainingMillis<-0){getParent().destroy();System.out.println("LimiteDLifeSpan timed out.");}
+	final WorldObject p = getParent();
+	timeRemainingMillis-=p.getTr().getThreadManager().getElapsedTimeInMillisSinceLastGameTick();
+	if(timeRemainingMillis<=0){
+	    p.getBehavior().probeForBehavior(DeathBehavior.class).die();
+	}//end if(remainign)
     }
     
     public LimitedLifeSpan reset(long millisUntilDestroyed){
-	timeRemainingMillis=millisUntilDestroyed+System.currentTimeMillis();
+	timeRemainingMillis=millisUntilDestroyed;
 	return this;
     }
 }//end LimitedLifeSpan

0f858b45b583396baaa72eab6eea94897eaab140
Chuck Ritola <cjritola@gmail.com>
2014-01-01 18:49:43 -0500

    Implemented ProjectileFiringBehavior which will auto-upgrade on powerup (for lasers)

---
+++ src/main/java/org/jtrfp/trcl/beh/UpgradeableProjectileFiringBehavior.java
@@ -0,0 +1,95 @@
+package org.jtrfp.trcl.beh;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+
+public class UpgradeableProjectileFiringBehavior extends
+	ProjectileFiringBehavior {
+    private int capabilityLevel=0;
+    private int maxCapabilityLevel=2;
+    private boolean limitlessBottomLevel=true;
+    private Vector3D [][] firingMultiplexMap;
+    @Override
+    public void addSupply(double amt){
+	raiseCapabilityLevel();
+	super.addSupply(amt);
+    }
+    
+    @Override
+    protected boolean takeAmmo(){
+	if(capabilityLevel==0&&limitlessBottomLevel())return true;
+	boolean result = super.takeAmmo();
+	if(!result)resetCapabilityLevel();
+	return result;
+    }
+    
+    protected void raiseCapabilityLevel(){
+	capabilityLevel++;
+	super.setFiringPositions(firingMultiplexMap[capabilityLevel]);
+	capabilityLevel=Math.min(capabilityLevel,maxCapabilityLevel);
+    }
+    
+    protected void resetCapabilityLevel(){
+	capabilityLevel=0;
+    }
+    
+    protected boolean limitlessBottomLevel(){return limitlessBottomLevel;}
+    
+    public UpgradeableProjectileFiringBehavior setFiringMultiplexMap(Vector3D firingMultiplexMap [][]){
+	this.firingMultiplexMap=firingMultiplexMap;
+	this.setFiringPositions(firingMultiplexMap[0]);
+	return this;
+    }
+
+    /**
+     * @return the capabilityLevel
+     */
+    public int getCapabilityLevel() {
+        return capabilityLevel;
+    }
+
+    /**
+     * @param capabilityLevel the capabilityLevel to set
+     */
+    public UpgradeableProjectileFiringBehavior setCapabilityLevel(int capabilityLevel) {
+        this.capabilityLevel = capabilityLevel;
+        return this;
+    }
+
+    /**
+     * @return the maxCapabilityLevel
+     */
+    public int getMaxCapabilityLevel() {
+        return maxCapabilityLevel;
+    }
+
+    /**
+     * @param maxCapabilityLevel the maxCapabilityLevel to set
+     */
+    public UpgradeableProjectileFiringBehavior setMaxCapabilityLevel(int maxCapabilityLevel) {
+        this.maxCapabilityLevel = maxCapabilityLevel;
+        return this;
+    }
+
+    /**
+     * @return the limitlessBottomLevel
+     */
+    public boolean isLimitlessBottomLevel() {
+        return limitlessBottomLevel;
+    }
+
+    /**
+     * @param limitlessBottomLevel the limitlessBottomLevel to set
+     */
+    public UpgradeableProjectileFiringBehavior setLimitlessBottomLevel(boolean limitlessBottomLevel) {
+        this.limitlessBottomLevel = limitlessBottomLevel;
+        return this;
+    }
+
+    /**
+     * @return the firingMultiplexMap
+     */
+    public Vector3D[][] getFiringMultiplexMap() {
+        return firingMultiplexMap;
+    }
+}//end UpgradeableProjectilFiringBehavior

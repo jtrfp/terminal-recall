042b45b4795536c940d2793cbfb655fa57f4d9a6
Chuck Ritola <cjritola@gmail.com>
2014-01-01 19:59:12 -0500

    Added support for non-looping sequences.

--- src/main/java/org/jtrfp/trcl/Sequencer.java
+++ src/main/java/org/jtrfp/trcl/Sequencer.java
@@ -19,15 +19,22 @@ public final class Sequencer implements Controller{
 	private final int frameDelayMsec;
 	private final boolean interpolate;
 	private final int numFrames;
+	private final boolean loop;
 	private long timeOffset=0;
 	
+	public Sequencer(int frameDelayMsec, int numFrames, boolean interpolate, boolean loop){
+	    this.numFrames=numFrames;
+	    this.frameDelayMsec=frameDelayMsec;
+	    this.interpolate=interpolate;
+	    this.loop=loop;
+	}//end constructor(...)
+	
 	public Sequencer(int frameDelayMsec, int numFrames, boolean interpolate){
-		this.numFrames=numFrames;
-		this.frameDelayMsec=frameDelayMsec;
-		this.interpolate=interpolate;
+		this(frameDelayMsec, numFrames, interpolate, true);
 	}//end constructor(...)
 	
 	public double getCurrentFrame(){
+	    	if(!loop&&System.currentTimeMillis()-timeOffset>numFrames*(frameDelayMsec-1))return numFrames-1;//Freeze on last frame/
 		double result = (((double)(System.currentTimeMillis()-timeOffset)/(double)frameDelayMsec))%(double)numFrames;
 		return interpolate?result:(int)result;
 	}//end getCurentFrame()

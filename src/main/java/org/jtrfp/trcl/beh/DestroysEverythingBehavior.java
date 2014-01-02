d40549fdef2986fe07a885900a70bee7b34f3025
Chuck Ritola <cjritola@gmail.com>
2014-01-01 19:40:27 -0500

    Created DestroysEverythingBehavior

---
+++ src/main/java/org/jtrfp/trcl/beh/DestroysEverythingBehavior.java
@@ -0,0 +1,21 @@
+package org.jtrfp.trcl.beh;
+
+import org.jtrfp.trcl.obj.DEFObject;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class DestroysEverythingBehavior extends Behavior {
+    int counter=2;
+    @Override
+    public void _proposeCollision(WorldObject other){
+	if(other instanceof DEFObject){
+	    other.getBehavior().probeForBehavior(DamageableBehavior.class).damage(65536);//Really smash that sucker.
+	}//end if(DEFObject)
+    }//end proposeCollision()
+    @Override
+    public void _tick(long timeMillis){
+	counter--;
+	if(counter<=0){//We can't stick around for long. Not with all this destroying going on.
+	    getParent().destroy();counter=2;
+	}//end if(counter<=0)
+    }//end _tick(...)
+}//end DestroyesEverythinBehavior

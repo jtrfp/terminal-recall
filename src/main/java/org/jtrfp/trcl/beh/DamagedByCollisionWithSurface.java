e132a69895a5be6f9f5ca2b93230f84df6b8041b
Chuck Ritola <cjritola@gmail.com>
2014-01-06 17:43:29 -0500

    Created DamagedByCollisionWithSurface

---
+++ src/main/java/org/jtrfp/trcl/beh/DamagedByCollisionWithSurface.java
@@ -0,0 +1,35 @@
+package org.jtrfp.trcl.beh;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class DamagedByCollisionWithSurface extends Behavior implements SurfaceImpactListener {
+    private int collisionDamage=6554;
+    private final double MAX_SPEED=70000;
+    private final int MIN_FRAGS=6;
+    @Override
+    public void collidedWithSurface(WorldObject wo, Vector3D surfaceNormal) {
+	final WorldObject p = getParent();
+	p.getBehavior().probeForBehavior(DamageableBehavior.class).shearDamage(collisionDamage);
+	for(int i=0; i<MIN_FRAGS+p.getModel().getTriangleList().getMaximumVertexValue()/6000; i++){
+	    p.getTr().getResourceManager().getDebrisFactory().spawn(p.getPosition(), 
+	    new Vector3D(
+		Math.random()*MAX_SPEED-MAX_SPEED/2.,
+		Math.random()*MAX_SPEED+30000,
+		Math.random()*MAX_SPEED-MAX_SPEED/2.));}
+    }//end collidedWithSurface(...)
+    /**
+     * @return the collisionDamage
+     */
+    public int getCollisionDamage() {
+        return collisionDamage;
+    }
+    /**
+     * @param collisionDamage the collisionDamage to set
+     */
+    public DamagedByCollisionWithSurface setCollisionDamage(int collisionDamage) {
+        this.collisionDamage = collisionDamage;
+        return this;
+    }
+    
+}

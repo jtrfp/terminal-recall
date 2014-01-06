487e7c9c1499c14d9ab262ddcec37e0587a23577
Chuck Ritola <cjritola@gmail.com>
2014-01-06 16:44:42 -0500

    Created Cube-based collision behavior

---
+++ src/main/java/org/jtrfp/trcl/beh/CubeCollisionBehavior.java
@@ -0,0 +1,84 @@
+package org.jtrfp.trcl.beh;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.obj.BarrierCube;
+import org.jtrfp.trcl.obj.Player;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class CubeCollisionBehavior extends Behavior {
+private Vector3D dims;
+private Vector3D origin;
+private int damageOnImpact= 6554;
+	public CubeCollisionBehavior(){super();}
+	public CubeCollisionBehavior(BarrierCube bc){
+	    super();
+	    origin=bc.getOrigin();
+	    dims=bc.getDims();
+	}
+	public CubeCollisionBehavior(WorldObject wo) {
+	    super();
+	    Vector3D max= wo.getModel().getTriangleList().getMaximumVertexDims();
+	    Vector3D min= wo.getModel().getTriangleList().getMinimumVertexDims();
+	    origin = new Vector3D((max.getX()+min.getX())/2.,(max.getY()+min.getY())/2.,(max.getZ()+min.getZ())/2.);
+	    dims = new Vector3D(max.getX()-min.getX(),max.getY()-min.getY(),max.getZ()-min.getZ());
+	}
+	@Override
+	public void _proposeCollision(WorldObject obj){
+	    if(obj instanceof Player){
+		final WorldObject p = getParent();
+		final Vector3D relPos=TR.twosComplimentSubtract(obj.getPosition(), p.getPosition());
+		final Rotation rot = new Rotation(Vector3D.PLUS_K,Vector3D.PLUS_J,p.getHeading(),p.getTop());
+		final Vector3D rotPos=rot.applyInverseTo(relPos);
+		final Vector3D rotTransPos=rotPos.add(origin);
+		if(TR.twosComplimentDistance(obj.getPosition(), p.getPosition())<80000)
+		if(	rotTransPos.getX()>0 && rotTransPos.getX()<dims.getX() &&
+			rotTransPos.getY()>0 && rotTransPos.getY()<dims.getY() &&
+			rotTransPos.getZ()>0 && rotTransPos.getZ()<dims.getZ()){
+			    obj.getBehavior().probeForBehavior(DamageableBehavior.class).impactDamage(damageOnImpact);
+		    }//end if(withinRange)
+	    }//end if(Player)
+	}//end _proposeCollision(...)
+
+	/**
+	 * @return the dims
+	 */
+	public Vector3D getDims() {
+	    return dims;
+	}
+
+	/**
+	 * @param dims the dims to set
+	 */
+	public void setDims(Vector3D dims) {
+	    this.dims = dims;
+	}
+
+	/**
+	 * @return the origin
+	 */
+	public Vector3D getOrigin() {
+	    return origin;
+	}
+
+	/**
+	 * @param origin the origin to set
+	 */
+	public void setOrigin(Vector3D origin) {
+	    this.origin = origin;
+	}
+	/**
+	 * @return the damageOnImpact
+	 */
+	public int getDamageOnImpact() {
+	    return damageOnImpact;
+	}
+	/**
+	 * @param damageOnImpact the damageOnImpact to set
+	 */
+	public CubeCollisionBehavior setDamageOnImpact(int damageOnImpact) {
+	    this.damageOnImpact = damageOnImpact;
+	    return this;
+	}
+}//end CubeCollisionBehavior

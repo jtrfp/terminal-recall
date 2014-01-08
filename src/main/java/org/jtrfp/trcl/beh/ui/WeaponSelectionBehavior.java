60bbbf0fa60881d9af60abb7d3be8b95583989f5
Chuck Ritola <cjritola@gmail.com>
2014-01-08 18:27:45 -0500

    Ammo counter display is now active on HUD.

--- src/main/java/org/jtrfp/trcl/beh/ui/WeaponSelectionBehavior.java
+++ src/main/java/org/jtrfp/trcl/beh/ui/WeaponSelectionBehavior.java
@@ -6,16 +6,24 @@ import org.jtrfp.trcl.KeyStatus;
 import org.jtrfp.trcl.beh.Behavior;
 import org.jtrfp.trcl.beh.ProjectileFiringBehavior;
 import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.core.ThreadManager;
 import org.jtrfp.trcl.file.Weapon;
 import org.jtrfp.trcl.obj.WorldObject;
 
 public class WeaponSelectionBehavior extends Behavior {
     private ProjectileFiringBehavior [] behaviors;
     private ProjectileFiringBehavior activeBehavior;
+    private int ammoDisplayUpdateCounter=0;
+    public static final int AMMO_DISPLAY_UPDATE_INTERVAL_MS=80;
+    private static final int AMMO_DISPLAY_COUNTER_INTERVAL=(int)(AMMO_DISPLAY_UPDATE_INTERVAL_MS/ (1000./ThreadManager.GAMEPLAY_FPS));
     @Override
     public void _tick(long tickTimeMillis){
 	final WorldObject parent = getParent();
 	final KeyStatus keyStatus = parent.getTr().getKeyStatus();
+	if(++ammoDisplayUpdateCounter%AMMO_DISPLAY_COUNTER_INTERVAL==0){
+	    final int ammo = activeBehavior.getAmmo();
+	    parent.getTr().getHudSystem().getAmmo().setContent(""+(ammo!=-1?ammo:"INF"));
+	}//end if(update ammo display)
 	for(int k=0; k<7;k++){
 	    if(keyStatus.isPressed(KeyEvent.VK_1+k)){
 		final ProjectileFiringBehavior newBehavior = behaviors[k];

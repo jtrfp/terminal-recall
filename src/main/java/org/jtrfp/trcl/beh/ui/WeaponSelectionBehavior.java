accaa0d12cb5d125828d950603849b59f1a60a49
Chuck Ritola <cjritola@gmail.com>
2014-01-08 17:47:51 -0500

    Added HUD display of weapon type.

--- src/main/java/org/jtrfp/trcl/beh/ui/WeaponSelectionBehavior.java
+++ src/main/java/org/jtrfp/trcl/beh/ui/WeaponSelectionBehavior.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyEvent;
 import org.jtrfp.trcl.KeyStatus;
 import org.jtrfp.trcl.beh.Behavior;
 import org.jtrfp.trcl.beh.ProjectileFiringBehavior;
+import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.file.Weapon;
 import org.jtrfp.trcl.obj.WorldObject;
 
 public class WeaponSelectionBehavior extends Behavior {
@@ -16,8 +18,25 @@ public class WeaponSelectionBehavior extends Behavior {
 	final KeyStatus keyStatus = parent.getTr().getKeyStatus();
 	for(int k=0; k<7;k++){
 	    if(keyStatus.isPressed(KeyEvent.VK_1+k)){
-		activeBehavior=behaviors[k];
-		}//end if (selection key is pressed)
+		final ProjectileFiringBehavior newBehavior = behaviors[k];
+		if(activeBehavior!=newBehavior){
+		    activeBehavior=behaviors[k];
+		    final Weapon w = activeBehavior.getProjectileFactory().getWeapon();
+		    final TR tr = parent.getTr();
+		    String content="???";
+		    switch(tr.getTrConfig().getGameVersion()){
+		    case F3:{
+			content=w.getF3DisplayName();
+			break;
+		       }
+		    case TV:{
+			content=w.getTvDisplayName();
+			break;
+		        }
+		    }//end switch(game version)
+		    tr.getHudSystem().getWeapon().setContent(content);
+		}//end if(New Behavior)
+	    }//end if (selection key is pressed)
 	}//end for(keys)
 	if(keyStatus.isPressed(KeyEvent.VK_SPACE)){
 	    activeBehavior.requestFire();

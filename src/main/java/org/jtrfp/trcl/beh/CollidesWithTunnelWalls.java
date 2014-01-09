135efb5c4199c846feecb2cd52e048597101ccb7
Chuck Ritola <cjritola@gmail.com>
2014-01-08 20:38:32 -0500

    Bugfix: I hate Tunnels and Tunnels clearly hate me.

--- src/main/java/org/jtrfp/trcl/beh/CollidesWithTunnelWalls.java
+++ src/main/java/org/jtrfp/trcl/beh/CollidesWithTunnelWalls.java
@@ -17,11 +17,11 @@ package org.jtrfp.trcl.beh;
 
 import java.util.Collection;
 
-import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.jtrfp.trcl.Submitter;
 import org.jtrfp.trcl.core.TR;
 import org.jtrfp.trcl.file.TNLFile.Segment;
+import org.jtrfp.trcl.obj.Player;
 import org.jtrfp.trcl.obj.TunnelSegment;
 import org.jtrfp.trcl.obj.Velocible;
 import org.jtrfp.trcl.obj.WorldObject;
@@ -46,25 +46,30 @@ public class CollidesWithTunnelWalls extends Behavior{
 				{final Segment s = seg.getSegmentData();
 				final double segLen=seg.getSegmentLength();
 				final Vector3D start =seg.getPosition();
+				//I have no idea why Y has to be cut in half.
 				final Vector3D end = start.add(new Vector3D(segLen,seg.getEndY(),-seg.getEndX()));//ZYX
+				//final Vector3D end = (new Vector3D(start.getX()+segLen,seg.getEndY(),-seg.getEndX()));//ZYX
 				final Vector3D tunnelSpineNoNorm=TR.twosComplimentSubtract(end, start);
 				final Vector3D tunnelSpineNorm=tunnelSpineNoNorm.normalize();
 				
-				final double depthDownSeg=start.getX()-parent.getPosition().getX();
+				final double depthDownSeg=parent.getPosition().getX()-start.getX();
 				final double pctDownSeg=depthDownSeg/segLen;
 				Vector3D circleCenter=
 						start.add(tunnelSpineNorm.scalarMultiply(TR.deltaRollover(parent.getPosition().getX()-start.getX())));
 				circleCenter = (new Vector3D(parent.getPosition().getX(),circleCenter.getY(),circleCenter.getZ()));
+				//parent.setPosition(circleCenter);//Rail-mode debug code.
 				final double startWidth=TunnelSegment.getStartWidth(s);
 				final double startHeight=TunnelSegment.getStartHeight(s);
 				final double endWidth=TunnelSegment.getEndWidth(s);
 				final double endHeight=TunnelSegment.getEndHeight(s);
-				// 0.7 is a fudge-factor to ensure the player doesn't see the outside of the tunnel (due to clipping) before bouncing off.
-				final double widthHere=.7*(startWidth*(1.-pctDownSeg)+endWidth*pctDownSeg);
-				final double heightHere=.7*(startHeight*(1.-pctDownSeg)+endHeight*pctDownSeg);
+				// fudge-factor to ensure the player doesn't see the outside of the tunnel (due to clipping) before bouncing off.
+				final double widthHere=.9*(startWidth*(1.-pctDownSeg)+endWidth*pctDownSeg);
+				final double heightHere=.9*(startHeight*(1.-pctDownSeg)+endHeight*pctDownSeg);
 				//Parent position relative to tunnel
-				final Vector3D pprtt = TR.twosComplimentSubtract(parent.getPosition(), circleCenter);
+				//final Vector3D pprtt = TR.twosComplimentSubtract(parent.getPosition(), circleCenter);
+				final Vector3D pprtt = parent.getPosition().subtract(circleCenter);
 				final double protrusion =(pprtt.getZ()*pprtt.getZ())/(widthHere*widthHere)+(pprtt.getY()*pprtt.getY())/(heightHere*heightHere); 
+				//if(parent instanceof Player){System.out.println("width="+widthHere+" height="+heightHere+" pctDownSeg="+pctDownSeg);}//TODO
 				if(protrusion>1){
 					//Execute the "bounce"
 				    	final Vector3D oldPosition = parent.getPosition();
@@ -72,7 +77,6 @@ public class CollidesWithTunnelWalls extends Behavior{
 				    	parent.setPosition(circleCenter.scalarMultiply(.01).add(oldPosition.scalarMultiply(.99)));
 				    	final Vector3D inwardNormal = TR.twosComplimentSubtract(circleCenter, oldPosition).normalize();
 					surfaceNormalVar = inwardNormal;
-					//System.exit(10);
 					//Notify listeners
 					parent.getBehavior().probeForBehaviors(sub, SurfaceImpactListener.class);
 					//final RotationalMomentumBehavior rmb = parent.getBehavior().probeForBehavior(RotationalMomentumBehavior.class);

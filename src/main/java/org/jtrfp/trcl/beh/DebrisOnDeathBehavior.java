6cd841b9e219caa6ace294304c6fb62cdf375424
Chuck Ritola <cjritola@gmail.com>
2014-01-14 12:59:39 -0500

    Bugfix: Unchked null when querying max vertix fvalue ; use transpranret primitive lists too

--- src/main/java/org/jtrfp/trcl/beh/DebrisOnDeathBehavior.java
+++ src/main/java/org/jtrfp/trcl/beh/DebrisOnDeathBehavior.java
@@ -1,6 +1,7 @@
 package org.jtrfp.trcl.beh;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.Model;
 import org.jtrfp.trcl.obj.WorldObject;
 
 public class DebrisOnDeathBehavior extends Behavior implements DeathListener {
@@ -9,7 +10,13 @@ public class DebrisOnDeathBehavior extends Behavior implements DeathListener {
     @Override
     public void notifyDeath() {
 	WorldObject p = getParent();
-	for(int i=0; i<MIN_FRAGS+p.getModel().getTriangleList().getMaximumVertexValue()/6000; i++){
+	final double maxVertexValue;
+	final Model model = p.getModel();
+	if(model.getTriangleList()!=null)maxVertexValue=model.getTriangleList().getMaximumVertexValue();
+	else if(model.getTransparentTriangleList()!=null)maxVertexValue=model.getTransparentTriangleList().getMaximumVertexValue();
+	else if(model.getLineSegmentList()!=null)maxVertexValue=model.getLineSegmentList().getMaximumVertexValue();
+	else return;//Give up
+	for(int i=0; i<MIN_FRAGS+maxVertexValue/6000; i++){
 	    p.getTr().getResourceManager().getDebrisFactory().spawn(p.getPosition(), 
 	    new Vector3D(
 		Math.random()*MAX_SPEED-MAX_SPEED/2.,

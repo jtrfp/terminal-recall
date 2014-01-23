f2cf11e1945945f794c0df8acc26d85183310a40
Chuck Ritola <cjritola@gmail.com>
2014-01-23 16:27:54 -0500

    Bugfix: Incorrect index start.

--- src/main/java/org/jtrfp/trcl/core/IndexPool.java
+++ src/main/java/org/jtrfp/trcl/core/IndexPool.java
@@ -3,23 +3,21 @@ package org.jtrfp.trcl.core;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class IndexPool
-	{
+public class IndexPool{
 	private final Queue<Integer> freeIndices = new LinkedBlockingQueue<Integer>();
 	private int maxCapacity=1;
-	private int highestIndex=0;
+	private int highestIndex=-1;
 	private GrowthBehavior growthBehavior=new GrowthBehavior()
 		{public int grow(int index){return index*2;}};//Default is to double each time.
 	
 	public IndexPool(){
-	    freeIndices.add(0);
 	}
 		
 	public int pop()
 		{if(!freeIndices.isEmpty())
 			{return freeIndices.remove();}
 		else if(highestIndex+1<maxCapacity)
-			{return ++highestIndex;}
+			{return (++highestIndex);}
 		else//Need to allocate a new block of indices
 			{maxCapacity = growthBehavior.grow(maxCapacity);
 			return pop();//Try again.
@@ -36,11 +34,27 @@ public class IndexPool
 	public static void main(String [] args){
 	    System.out.println("Testing index pool");
 	    final IndexPool ip = new IndexPool();
-	    for(int i=0; i<200; i++){
+	    /*
+	    for(int i=0; i<20; i++){
 		System.out.println("pop(): "+ip.pop());
-	    }for(int i=0; i<100; i++){
+	    }for(int i=0; i<10; i++){
 		System.out.println("free(): "+ip.free(i));
 	    }//end for(i)
+	    */
+	   for(int i=0; i<5; i++){
+		System.out.println(ip.popConsecutive(5)+"");
+	    }
 	    System.out.println("free: "+ip.freeIndices.size()+" highest: "+ip.highestIndex+" max: "+ip.maxCapacity);
 	}
-	}//end IndexPool
+
+	public int popConsecutive(int numNewItems) {
+	    //TODO This should use the freed pool instead of always allocating new
+	    if(highestIndex+numNewItems<maxCapacity)
+		{final int result = highestIndex+1; highestIndex+=numNewItems;
+		return result;}
+	    else//Need to allocate a new block of indices
+		{maxCapacity = growthBehavior.grow(maxCapacity);
+		return popConsecutive(numNewItems);//Try again.
+		}
+	}//end popConsecutive(...)
+}//end IndexPool

ff787b9e5067ea966b6d3801224afa6155b03798
Chuck Ritola <cjritola@gmail.com>
2014-01-29 05:43:57 -0500

    Created Sprite2D

---
+++ src/main/java/org/jtrfp/trcl/obj/Sprite2D.java
@@ -0,0 +1,31 @@
+package org.jtrfp.trcl.obj;
+
+import java.util.concurrent.Future;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.Model;
+import org.jtrfp.trcl.RenderMode;
+import org.jtrfp.trcl.TextureDescription;
+import org.jtrfp.trcl.Triangle;
+import org.jtrfp.trcl.core.TR;
+
+public class Sprite2D extends WorldObject2DVisibleEverywhere {
+
+    public Sprite2D(TR tr, double z, double width, double height, Future<TextureDescription> tex, boolean useAlpha) {
+	super(tr);
+	final Model m = new Model(false,tr);
+	Triangle [] tris = Triangle.quad2Triangles(
+		new double[]{-width,width,width,-width}, 
+		new double[]{-height,-height,height,height}, 
+		new double[]{z,z,z,z},
+		new double[]{0,1,1,0},
+		new double[]{0,0,1,1}, tex, RenderMode.DYNAMIC, useAlpha);
+	m.addTriangles(tris);
+	m.finalizeModel();
+	setModel(m);
+	setTop(Vector3D.PLUS_J);
+	setActive(true);
+	setVisible(true);
+    }
+
+}

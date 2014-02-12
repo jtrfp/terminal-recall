3f5922080de629dcede287af7378eea1029355ff
Chuck Ritola <cjritola@gmail.com>
2014-02-12 02:18:29 -0500

    Created Bobbing (behavior)

---
+++ src/main/java/org/jtrfp/trcl/beh/Bobbing.java
@@ -0,0 +1,74 @@
+package org.jtrfp.trcl.beh;
+
+import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class Bobbing extends Behavior {
+    private double amplitude=TR.mapSquareSize/5;
+    private double additionalHeight=TR.mapSquareSize;
+    private double bobPeriodMillis=10*1000;
+    private double phase=0;
+    private double initialY=0;
+    private boolean first=true;
+    @Override
+    public void _tick(long timeMillis){
+	final WorldObject thisObject = getParent();
+	double [] thisPos=thisObject.getPosition();
+	if(first)initialY=thisPos[1];
+	thisPos[1]=Math.sin((timeMillis/bobPeriodMillis)*Math.PI*2+phase)*amplitude+amplitude+additionalHeight+initialY;
+	thisObject.notifyPositionChange();
+	first=false;
+    }//end _tick()
+    /**
+     * @return the amplitude
+     */
+    public double getAmplitude() {
+        return amplitude;
+    }
+    /**
+     * @param amplitude the amplitude to set
+     */
+    public Bobbing setAmplitude(double amplitude) {
+        this.amplitude = amplitude;
+        return this;
+    }
+    /**
+     * @return the additionalHeight
+     */
+    public double getAdditionalHeight() {
+        return additionalHeight;
+    }
+    /**
+     * @param additionalHeight the additionalHeight to set
+     */
+    public Bobbing setAdditionalHeight(double additionalHeight) {
+        this.additionalHeight = additionalHeight;
+        return this;
+    }
+    /**
+     * @return the bobPeriodMillis
+     */
+    public double getBobPeriodMillis() {
+        return bobPeriodMillis;
+    }
+    /**
+     * @param bobPeriodMillis the bobPeriodMillis to set
+     */
+    public Bobbing setBobPeriodMillis(double bobPeriodMillis) {
+        this.bobPeriodMillis = bobPeriodMillis;
+        return this;
+    }
+    /**
+     * @return the phase
+     */
+    public double getPhase() {
+        return phase;
+    }
+    /**
+     * @param phase the phase to set
+     */
+    public Bobbing setPhase(double phase) {
+        this.phase = phase;
+        return this;
+    }
+}//end BobbingBehavior

c32505956556973f46bcb67a06d8bfbbae82415c
Chuck Ritola <cjritola@gmail.com>
2014-02-12 23:54:28 -0500

    Initial creation of AutoFiring behavior.

---
+++ src/main/java/org/jtrfp/trcl/beh/AutoFiring.java
@@ -0,0 +1,130 @@
+package org.jtrfp.trcl.beh;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.math.Vect3D;
+import org.jtrfp.trcl.obj.Player;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class AutoFiring extends Behavior implements AIFiringBehavior {
+    private double maxFiringDistance=TR.mapSquareSize*15;
+    private double minFiringDistance=TR.mapSquareSize*0;
+    private int lastIndexVisited=0;
+    private boolean [] firingPattern = new boolean []
+	    {true,true,true,false,false,false,false,false,false,false,false,false,false,false,false};
+    private int timePerPatternEntry=250;
+    private int totalFiringPatternTimeMillis=firingPattern.length*timePerPatternEntry;
+    private ProjectileFiringBehavior projectileFiringBehavior;
+    private final double [] firingVector = new double[3];
+    @Override
+    public void _tick(long timeMillis){
+	final WorldObject thisObject = getParent();
+	final double [] thisPos = thisObject.getPosition();
+	final Player player = thisObject.getTr().getPlayer();
+	final double [] playerPos = player.getPosition();
+	final double dist = Vect3D.distance(thisPos, playerPos);
+	if(dist<maxFiringDistance||dist>minFiringDistance){
+	    final int patIndex=(int)((timeMillis%totalFiringPatternTimeMillis)/timePerPatternEntry);
+	    if(patIndex!=lastIndexVisited){//end if(lastVisited)
+		if(firingPattern[patIndex]){
+		    System.out.println("Requesting Fire...");
+		    Vect3D.subtract(thisPos, playerPos, firingVector);
+		    projectileFiringBehavior.requestFire(new Vector3D(Vect3D.normalize(firingVector,firingVector)));}}
+	    lastIndexVisited=patIndex;
+	}//end in range
+    }//end _tick(...)
+    /**
+     * @return the maxFiringDistance
+     */
+    public double getMaxFiringDistance() {
+        return maxFiringDistance;
+    }
+    /**
+     * @param maxFiringDistance the maxFiringDistance to set
+     */
+    public AutoFiring setMaxFiringDistance(double maxFiringDistance) {
+        this.maxFiringDistance = maxFiringDistance;
+        return this;
+    }
+    /**
+     * @return the minFiringDistance
+     */
+    public double getMinFiringDistance() {
+        return minFiringDistance;
+    }
+    /**
+     * @param minFiringDistance the minFiringDistance to set
+     */
+    public AutoFiring setMinFiringDistance(double minFiringDistance) {
+        this.minFiringDistance = minFiringDistance;
+        return this;
+    }
+    /**
+     * @return the lastIndexVisited
+     */
+    public int getLastIndexVisited() {
+        return lastIndexVisited;
+    }
+    /**
+     * @param lastIndexVisited the lastIndexVisited to set
+     */
+    public AutoFiring setLastIndexVisited(int lastIndexVisited) {
+        this.lastIndexVisited = lastIndexVisited;
+        return this;
+    }
+    /**
+     * @return the firingPattern
+     */
+    public boolean[] getFiringPattern() {
+        return firingPattern;
+    }
+    /**
+     * @param firingPattern the firingPattern to set
+     */
+    public AutoFiring setFiringPattern(boolean[] firingPattern) {
+        this.firingPattern = firingPattern;
+        totalFiringPatternTimeMillis=firingPattern.length*timePerPatternEntry;
+        return this;
+    }
+    /**
+     * @return the timePerPatternEntry
+     */
+    public int getTimePerPatternEntry() {
+        return timePerPatternEntry;
+    }
+    /**
+     * @param timePerPatternEntry the timePerPatternEntry to set
+     */
+    public AutoFiring setTimePerPatternEntry(int timePerPatternEntry) {
+        this.timePerPatternEntry = timePerPatternEntry;
+        totalFiringPatternTimeMillis=firingPattern.length*timePerPatternEntry;
+        return this;
+    }
+    /**
+     * @return the totalFiringPatternTimeMillis
+     */
+    public int getTotalFiringPatternTimeMillis() {
+        return totalFiringPatternTimeMillis;
+    }
+    /**
+     * @param totalFiringPatternTimeMillis the totalFiringPatternTimeMillis to set
+     */
+    public AutoFiring setTotalFiringPatternTimeMillis(int totalFiringPatternTimeMillis) {
+        this.totalFiringPatternTimeMillis = totalFiringPatternTimeMillis;
+        return this;
+    }
+    /**
+     * @return the projectileFiringBehavior
+     */
+    public ProjectileFiringBehavior getProjectileFiringBehavior() {
+        return projectileFiringBehavior;
+    }
+    /**
+     * @param projectileFiringBehavior the projectileFiringBehavior to set
+     */
+    public AutoFiring setProjectileFiringBehavior(
+    	ProjectileFiringBehavior projectileFiringBehavior) {
+        this.projectileFiringBehavior = projectileFiringBehavior;
+        return this;
+    }
+}//end AutoFiring

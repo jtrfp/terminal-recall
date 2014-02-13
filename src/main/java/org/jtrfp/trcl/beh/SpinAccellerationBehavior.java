9ad7c227cd964d1471706791b6dbde0f56ed1fd4
Chuck Ritola <cjritola@gmail.com>
2014-02-13 07:38:37 -0500

    Created SpinAccellerationBehavior.

---
+++ src/main/java/org/jtrfp/trcl/beh/SpinAccellerationBehavior.java
@@ -0,0 +1,54 @@
+package org.jtrfp.trcl.beh;
+
+import org.jtrfp.trcl.beh.phy.RotationalMomentumBehavior;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class SpinAccellerationBehavior extends Behavior {
+    private double spinAccelleration=.004;
+    private SpinMode spinMode=SpinMode.EQUATORIAL;
+    
+    @Override
+    public void _tick(long millis){
+	final WorldObject p = getParent();
+	final RotationalMomentumBehavior rmb = p.getBehavior().probeForBehavior(RotationalMomentumBehavior.class);
+	switch(spinMode){
+	 case LATERAL:rmb.accellerateLateralMomentum(spinAccelleration);break;
+	 case POLAR:rmb.accelleratePolarMomentum(spinAccelleration);break;
+	 case EQUATORIAL:rmb.accellerateEquatorialMomentum(spinAccelleration);break;
+	 }//end switch(spinMode)
+    }//end _tick
+
+public enum SpinMode{
+    POLAR,
+    EQUATORIAL,
+    LATERAL
+ }//end SpinMode
+
+/**
+ * @return the spinAccelleration
+ */
+public double getSpinAccelleration() {
+    return spinAccelleration;
+}
+
+/**
+ * @param spinAccelleration the spinAccelleration to set
+ */
+public void setSpinAccelleration(double spinAccelleration) {
+    this.spinAccelleration = spinAccelleration;
+}
+
+/**
+ * @return the spinMode
+ */
+public SpinMode getSpinMode() {
+    return spinMode;
+}
+
+/**
+ * @param spinMode the spinMode to set
+ */
+public void setSpinMode(SpinMode spinMode) {
+    this.spinMode = spinMode;
+}
+}//end SpinAlongHeading

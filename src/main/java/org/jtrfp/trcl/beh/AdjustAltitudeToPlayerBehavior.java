d4167dc370b49a03b81187bfbf8a134f1ce96975
Chuck Ritola <cjritola@gmail.com>
2014-02-13 20:19:41 -0500

    Added hysteresis acommodation to AdjustAltitudeToPlayerBehavior.

--- src/main/java/org/jtrfp/trcl/beh/AdjustAltitudeToPlayerBehavior.java
+++ src/main/java/org/jtrfp/trcl/beh/AdjustAltitudeToPlayerBehavior.java
@@ -2,6 +2,7 @@ package org.jtrfp.trcl.beh;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.jtrfp.trcl.beh.phy.MovesByVelocity;
+import org.jtrfp.trcl.math.Vect3D;
 import org.jtrfp.trcl.obj.Player;
 import org.jtrfp.trcl.obj.WorldObject;
 
@@ -9,6 +10,7 @@ public class AdjustAltitudeToPlayerBehavior extends Behavior {
     private final Player player;
     private Vector3D DOWN=new Vector3D(0,-80000,0),UP=new Vector3D(0,80000,0);
     private boolean reverse=false;
+    private double hysteresis = 50000;
     public AdjustAltitudeToPlayerBehavior(Player player){
 	this.player=player;
     }//end AdjustAltitudeToPlayerBehavior
@@ -16,6 +18,8 @@ public class AdjustAltitudeToPlayerBehavior extends Behavior {
     @Override
     public void _tick(long tickTimeInMillis){
 	final WorldObject thisObject = getParent();
+	if(!reverse&&Math.abs(player.getPosition()[1]-thisObject.getPosition()[1])<hysteresis)
+	    return;
 	final boolean up=(player.getPosition()[1]>thisObject.getPosition()[1])!=reverse;
 	    thisObject.getBehavior().probeForBehavior(MovesByVelocity.class).accellerate(up?UP:DOWN);
     }//end _tick(...)
@@ -28,5 +32,19 @@ public class AdjustAltitudeToPlayerBehavior extends Behavior {
 
     public void setReverse(boolean reverse) {
 	this.reverse=reverse;
+    }
+
+    /**
+     * @return the hysteresis
+     */
+    public double getHysteresis() {
+        return hysteresis;
+    }
+
+    /**
+     * @param hysteresis the hysteresis to set
+     */
+    public void setHysteresis(double hysteresis) {
+        this.hysteresis = hysteresis;
     }
 }//end AdjustAltitudeToPlayerBehavior

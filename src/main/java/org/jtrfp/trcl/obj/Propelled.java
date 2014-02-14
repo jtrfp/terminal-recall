fba56d1223e690b356e7e81fc6d480c0305683c4
Chuck Ritola <cjritola@gmail.com>
2014-02-14 05:24:45 -0500

    Added monad support to Propelled.

--- src/main/java/org/jtrfp/trcl/obj/Propelled.java
+++ src/main/java/org/jtrfp/trcl/obj/Propelled.java
@@ -3,9 +3,9 @@ package org.jtrfp.trcl.obj;
 public interface Propelled
 	{public Propelled setPropulsion(double magnitude);
 	public double getPropulsion();
-	public void setMaxPropulsion(double max);
+	public Propelled setMaxPropulsion(double max);
 	public double getMaxPropulsion();
-	public void setMinPropulsion(double min);
+	public Propelled setMinPropulsion(double min);
 	public double getMinPropulsion();
-	public void deltaPropulsion(double delta);
+	public Propelled deltaPropulsion(double delta);
 	}

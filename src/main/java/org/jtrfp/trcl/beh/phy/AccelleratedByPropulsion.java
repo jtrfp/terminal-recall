961df8b9431b4c367a897fa8ad3a4b9fc8ba1bf7
Chuck Ritola <cjritola@gmail.com>
2014-02-14 05:23:58 -0500

    Added support for custom thrust vector in AccelleratedByPropulsion.

--- src/main/java/org/jtrfp/trcl/beh/phy/AccelleratedByPropulsion.java
+++ src/main/java/org/jtrfp/trcl/beh/phy/AccelleratedByPropulsion.java
@@ -1,5 +1,6 @@
 package org.jtrfp.trcl.beh.phy;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.jtrfp.trcl.beh.Behavior;
 import org.jtrfp.trcl.obj.Propelled;
 import org.jtrfp.trcl.obj.Velocible;
@@ -7,12 +8,27 @@ import org.jtrfp.trcl.obj.WorldObject;
 
 
 public class AccelleratedByPropulsion extends Behavior{
+    	private Vector3D thrustVector = null;
 	@Override
 	public void _tick(long timeInMillis)
 		{WorldObject wo = getParent();
 		Propelled p = wo.getBehavior().probeForBehavior(Propelled.class);
 		Velocible v = wo.getBehavior().probeForBehavior(Velocible.class);
 		final double progressionInSeconds = (double)wo.getTr().getThreadManager().getElapsedTimeInMillisSinceLastGameTick()/1000.;
-		v.accellerate(wo.getHeading().scalarMultiply(p.getPropulsion()*progressionInSeconds));
+		Vector3D tVector = thrustVector!=null?thrustVector:wo.getHeading();
+		v.accellerate(tVector.scalarMultiply(p.getPropulsion()*progressionInSeconds));
 		}//end _tick(...)
-	}//end AccelleratedByPropulsion
+	/**
+	 * @return the thrustVector
+	 */
+	public Vector3D getThrustVector() {
+	    return thrustVector;
+	}
+	/**
+	 * @param thrustVector the thrustVector to set
+	 */
+	public AccelleratedByPropulsion setThrustVector(Vector3D thrustVector) {
+	    this.thrustVector = thrustVector;
+	    return this;
+	}
+}//end AccelleratedByPropulsion

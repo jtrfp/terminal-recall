9a1cad4158f1018ad5d88d0a1c13f2ee5fda5051
Chuck Ritola <cjritola@gmail.com>
2014-02-24 15:41:54 -0500

    Added setArrayui to GLUniform.

--- src/main/java/org/jtrfp/trcl/gpu/GLUniform.java
+++ src/main/java/org/jtrfp/trcl/gpu/GLUniform.java
@@ -1,29 +1,37 @@
 package org.jtrfp.trcl.gpu;
 
+import java.nio.IntBuffer;
+
 import javax.media.opengl.GL3;
 
-public class GLUniform
-	{
-	private final GLProgram prg;
-	private final int uniformID;
-	private static GL3 gl;
-	GLUniform(GLProgram prg, int uniformID)
-		{
-		this.prg=prg;
-		this.uniformID=uniformID;
-		gl=prg.getGl();
-		}
-	
-		public void set(float value)
-			{gl.glUniform1f(uniformID,value);}
-		public void set(int value)
-			{gl.glUniform1i(uniformID,value);}
-		public void setui(int value)
-			{gl.glUniform1ui(uniformID,value);}
-	
-		public void set(float float1, float float2,
-				float float3)
-			{gl.glUniform3f(uniformID,
-					float1, float2, float3);
-		}
-	}//end GLUniform
+public class GLUniform {
+    private final GLProgram prg;
+    private final int uniformID;
+    private static GL3 gl;
+
+    GLUniform(GLProgram prg, int uniformID) {
+	this.prg = prg;
+	this.uniformID = uniformID;
+	gl = prg.getGl();
+    }
+
+    public void set(float value) {
+	gl.glUniform1f(uniformID, value);
+    }
+
+    public void set(int value) {
+	gl.glUniform1i(uniformID, value);
+    }
+
+    public void setui(int value) {
+	gl.glUniform1ui(uniformID, value);
+    }
+
+    public void set(float float1, float float2, float float3) {
+	gl.glUniform3f(uniformID, float1, float2, float3);
+    }
+
+    public void setArrayui(int[] vals) {
+	gl.glUniform1uiv(uniformID, vals.length, IntBuffer.wrap(vals));
+    }
+}// end GLUniform

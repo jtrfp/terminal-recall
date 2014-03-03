73a322ef3048432b275ce6dd3c0e299a824e8601
Chuck Ritola <cjritola@gmail.com>
2014-03-02 21:52:08 -0500

    Cleanup.

--- src/main/java/org/jtrfp/trcl/gpu/ReallocatableGLTextureBuffer.java
+++ src/main/java/org/jtrfp/trcl/gpu/ReallocatableGLTextureBuffer.java
@@ -4,82 +4,87 @@ import java.nio.ByteBuffer;
 
 import javax.media.opengl.GL2;
 
-public class ReallocatableGLTextureBuffer implements ReallocatableGLMemory
-	{
-	private GLTextureBuffer buffer;
-	private GPU gpu;
-	private MemoryUsageHint usageHint;
-	
-	public ReallocatableGLTextureBuffer(GPU gpu)
-		{this.gpu=gpu;buffer=new GLTextureBuffer(1,gpu);}
-	
-	@Override
-	public ByteBuffer map()
-		{
-		buffer.map(gpu.getGl());
-		return buffer.getDuplicateReferenceOfUnderlyingBuffer();
-		}
+public class ReallocatableGLTextureBuffer implements ReallocatableGLMemory {
+    private GLTextureBuffer buffer;
+    private GPU gpu;
+    private MemoryUsageHint usageHint;
 
-	@Override
-	public int getSizeInBytes()
-		{return buffer.getUnderlyingBuffer().capacity();}
+    public ReallocatableGLTextureBuffer(GPU gpu) {
+	this.gpu = gpu;
+	buffer = new GLTextureBuffer(1, gpu);
+    }
 
-	@Override
-	public void setUsageHint(MemoryUsageHint hint)
-		{buffer.setUsageHint((usageHint=hint).getGLEnumInt());}
+    @Override
+    public ByteBuffer map() {
+	buffer.map(gpu.getGl());
+	return buffer.getDuplicateReferenceOfUnderlyingBuffer();
+    }
 
-	@Override
-	public MemoryUsageHint getUsageHint()
-		{return usageHint;}
+    @Override
+    public int getSizeInBytes() {
+	return buffer.getUnderlyingBuffer().capacity();
+    }
 
-	@Override
-	public void reallocate(ByteBuffer newData)
-		{buffer.free(gpu.getGl());
-		buffer = new GLTextureBuffer(newData.capacity(),gpu);
-		ByteBuffer bb = map();
-		bb.rewind();
-		bb.put(newData);
-		unmap();
-		}
+    @Override
+    public void setUsageHint(MemoryUsageHint hint) {
+	buffer.setUsageHint((usageHint = hint).getGLEnumInt());
+    }
 
-	@Override
-	public void unmap()
-		{buffer.unmap(gpu.getGl());}
+    @Override
+    public MemoryUsageHint getUsageHint() {
+	return usageHint;
+    }
 
-	@Override
-	public void bind()
-		{buffer.bind((GL2)gpu.getGl());}
+    @Override
+    public void reallocate(ByteBuffer newData) {
+	buffer.free(gpu.getGl());
+	buffer = new GLTextureBuffer(newData.capacity(), gpu);
+	ByteBuffer bb = map();
+	bb.rewind();
+	bb.put(newData);
+	unmap();
+    }
 
-	@Override
-	public void unbind()
-		{buffer.unbind((GL2)gpu.getGl());}
+    @Override
+    public void unmap() {
+	buffer.unmap(gpu.getGl());
+    }
 
-	@Override
-	public ByteBuffer getDuplicateReferenceOfUnderlyingBuffer()
-		{
-		return buffer.getDuplicateReferenceOfUnderlyingBuffer();
-		}
+    @Override
+    public void bind() {
+	buffer.bind((GL2) gpu.getGl());
+    }
 
-	@Override
-	public void bindToUniform(int textureUnit, GLProgram program,
-			GLUniform uniform)
-		{buffer.bindToUniform(gpu.getGl(), textureUnit, program, uniform);}
+    @Override
+    public void unbind() {
+	buffer.unbind((GL2) gpu.getGl());
+    }
 
-	@Override
-	public void reallocate(int sizeInBytes)
-		{
-		buffer.free(gpu.getGl());
-		buffer = new GLTextureBuffer(sizeInBytes,gpu);
-		}
+    @Override
+    public ByteBuffer getDuplicateReferenceOfUnderlyingBuffer() {
+	return buffer.getDuplicateReferenceOfUnderlyingBuffer();
+    }
 
-	@Override
-	public void putShort(int byteOffset, short val) {
-	    buffer.getUnderlyingBuffer().putShort(byteOffset, val);
-	}
+    @Override
+    public void bindToUniform(int textureUnit, GLProgram program,
+	    GLUniform uniform) {
+	buffer.bindToUniform(gpu.getGl(), textureUnit, program, uniform);
+    }
 
-	@Override
-	public void putFloat(int byteOffset, float val) {
-	    buffer.getUnderlyingBuffer().putFloat(byteOffset,val);
-	}
+    @Override
+    public void reallocate(int sizeInBytes) {
+	buffer.free(gpu.getGl());
+	buffer = new GLTextureBuffer(sizeInBytes, gpu);
+    }
 
-	}//end ReallocatableGLTextureBuffer
+    @Override
+    public void putShort(int byteOffset, short val) {
+	buffer.getUnderlyingBuffer().putShort(byteOffset, val);
+    }
+
+    @Override
+    public void putFloat(int byteOffset, float val) {
+	buffer.getUnderlyingBuffer().putFloat(byteOffset, val);
+    }
+
+}// end ReallocatableGLTextureBuffer

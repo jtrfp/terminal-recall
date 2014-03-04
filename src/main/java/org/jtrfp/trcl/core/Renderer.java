2b66899e22dfce50abd1e56818f31951a0fdb607
Chuck Ritola <cjritola@gmail.com>
2014-03-03 22:49:35 -0500

    WONTRUN Cleanup.

--- src/main/java/org/jtrfp/trcl/core/Renderer.java
+++ src/main/java/org/jtrfp/trcl/core/Renderer.java
@@ -24,156 +24,159 @@ import org.jtrfp.trcl.obj.WorldObject;
 
 import com.jogamp.common.util.IOUtil;
 
-public class Renderer
-	{
-	private final Camera camera;
-	private final GLProgram shaderProgram;
-	private final GLUniform fogStart,fogEnd,fogColor;
-	private boolean initialized=false;
-	private final GPU gpu;
-	private final RenderList [] renderList = new RenderList[2];
-	private boolean renderListToggle=false;
-	private RenderableSpacePartitioningGrid rootGrid;
-	private boolean active=false;//TODO: Remove when conversion is complete
-	
-	private int frameNumber;
-	private long lastTimeMillis;
-	
-	public Renderer(GPU gpu)
-		{this.gpu=gpu;this.camera=new Camera(gpu);
-		final GL3 gl = gpu.getGl();
-		//Fixed pipeline behavior
-		gl.glEnable(GL2.GL_DEPTH_TEST);
-		gl.glDepthFunc(GL2.GL_LESS);
-		gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
-		gl.glClearColor(0f, 0f, 0f, 0f);
-		
-		//Generate shader program
-		GLVertexShader vertexShader = gpu.newVertexShader();
-		GLFragmentShader fragmentShader = gpu.newFragmentShader();
-		shaderProgram = gpu.newProgram();
-		try{//Apache Commons to the rescue again. (:
-		    vertexShader.setSource(IOUtils.toString(getClass().getResourceAsStream("/vertexShader.glsl")));
-		    fragmentShader.setSource(IOUtils.toString(getClass().getResourceAsStream("/fragShader.glsl")));
-		}
-		catch(Exception e){e.printStackTrace();}
-		shaderProgram.attachShader(vertexShader);
-		shaderProgram.attachShader(fragmentShader);
-		shaderProgram.link();
-		shaderProgram.use();
-		
-		fogStart=shaderProgram.getUniform("fogStart");
-		fogEnd=shaderProgram.getUniform("fogEnd");
-		fogColor=shaderProgram.getUniform("fogColor");
-		
-		System.out.println("Initializing RenderList...");
-		final TR tr = gpu.getTr();
-		renderList[0] = new RenderList(gl, shaderProgram,tr);
-		renderList[1] = new RenderList(gl, shaderProgram,tr);
-		}
-	
-	private void ensureInit()
-		{if(initialized) return;
-		final GL3 gl = gpu.getGl();
-		
-		//GlobalDynamicTextureBuffer.getTextureBuffer().map();
-		gpu.getMemoryManager().map();
-		
-		System.out.println("Uploading vertex data to GPU...");
-		TriangleList.uploadAllListsToGPU(gl);
-		System.out.println("...Done.");
-		System.out.println("Uploading object defintion data to GPU...");
-		WorldObject.uploadAllObjectDefinitionsToGPU();
-		System.out.println("...Done.");
-		System.out.println("\t...World.init() complete.");
-		
-		//GlobalDynamicTextureBuffer.getTextureBuffer().unmap();
-		//gpu.getMemoryManager().unmap();
-		
-		try{
-		gpu.getMemoryManager().bindToUniform(1, shaderProgram, shaderProgram.getUniform("rootBuffer"));
-		shaderProgram.getUniform("textureMap").set((int)0);//Texture unit 0 mapped to textureMap
-		}
-	catch (RuntimeException e)
-		{e.printStackTrace();}
+public class Renderer {
+    private final Camera camera;
+    private final GLProgram shaderProgram;
+    private final GLUniform fogStart, fogEnd, fogColor;
+    private boolean initialized = false;
+    private final GPU gpu;
+    private final RenderList[] renderList = new RenderList[2];
+    private boolean renderListToggle = false;
+    private RenderableSpacePartitioningGrid rootGrid;
+    private boolean active = false;// TODO: Remove when conversion is complete
+
+    private int frameNumber;
+    private long lastTimeMillis;
+
+    public Renderer(GPU gpu) {
+	this.gpu = gpu;
+	this.camera = new Camera(gpu);
+	final GL3 gl = gpu.getGl();
+	// Fixed pipeline behavior
+	gl.glEnable(GL2.GL_DEPTH_TEST);
+	gl.glDepthFunc(GL2.GL_LESS);
+	gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+	gl.glClearColor(0f, 0f, 0f, 0f);
+
+	// Generate shader program
+	GLVertexShader vertexShader = gpu.newVertexShader();
+	GLFragmentShader fragmentShader = gpu.newFragmentShader();
+	shaderProgram = gpu.newProgram();
+	try {// Apache Commons to the rescue again. (:
+	    vertexShader.setSource(IOUtils.toString(getClass()
+		    .getResourceAsStream("/vertexShader.glsl")));
+	    fragmentShader.setSource(IOUtils.toString(getClass()
+		    .getResourceAsStream("/fragShader.glsl")));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	shaderProgram.attachShader(vertexShader);
+	shaderProgram.attachShader(fragmentShader);
+	shaderProgram.link();
+	shaderProgram.use();
+
+	fogStart = shaderProgram.getUniform("fogStart");
+	fogEnd = shaderProgram.getUniform("fogEnd");
+	fogColor = shaderProgram.getUniform("fogColor");
+
+	System.out.println("Initializing RenderList...");
+	final TR tr = gpu.getTr();
+	renderList[0] = new RenderList(gl, shaderProgram, tr);
+	renderList[1] = new RenderList(gl, shaderProgram, tr);
+    }
+
+    private void ensureInit() {
+	if (initialized)
+	    return;
+	final GL3 gl = gpu.getGl();
+
+	gpu.getMemoryManager().map();
+
+	System.out.println("Uploading vertex data to GPU...");
+	TriangleList.uploadAllListsToGPU(gl);
+	System.out.println("...Done.");
+	System.out.println("Uploading object defintion data to GPU...");
+	WorldObject.uploadAllObjectDefinitionsToGPU();
+	System.out.println("...Done.");
+	System.out.println("\t...World.init() complete.");
+
+	try {
+	    gpu.getMemoryManager().bindToUniform(1, shaderProgram,
+		    shaderProgram.getUniform("rootBuffer"));
+	    shaderProgram.getUniform("textureMap").set((int) 0);// Texture unit
+								// 0 mapped to
+								// textureMap
+	} catch (RuntimeException e) {
+	    e.printStackTrace();
+	}
 	GLTexture.specifyTextureUnit(gl, 0);
 	Texture.getGlobalTexture().bind(gl);
-	if(!shaderProgram.validate())
-		{
-		System.out.println(shaderProgram.getInfoLog());
-		System.exit(1);
-		}
+	if (!shaderProgram.validate()) {
+	    System.out.println(shaderProgram.getInfoLog());
+	    System.exit(1);
+	}
 	System.out.println("...Done.");
 	gpu.getMemoryManager().map();
-		initialized=true;
-		}//end ensureInit()
-	
-	private void fpsTracking()
-		{frameNumber++;
-		if ((frameNumber %= 20) == 0){
-		    	final int fps = (1000 / (int) (System.currentTimeMillis() - lastTimeMillis));
-		    	gpu.getTr().getReporter().report("org.jtrfp.trcl.core.Renderer.FPS", ""+fps);
-			}
-		lastTimeMillis = System.currentTimeMillis();
-		}
-	
-	public void render(){
-	    	if(!active)return;
-	    	fpsTracking();
-		// Update GPU
-		//try{gpu.getMemoryManager().map();}
-		//catch(NullPointerException e){return;}//Not ready yet; bail out.
-		PrimitiveList.tickAnimators();
-		ensureInit();
-		final GL3 gl = gpu.getGl();
-		setFogColor(gpu.getTr().getWorld().getFogColor());
-		gl.glClear(GL2.GL_DEPTH_BUFFER_BIT);
-		final double cameraViewDepth = camera.getViewDepth();
-		fogStart.set((float) (cameraViewDepth * 1.2) / 5f);
-		fogEnd.set((float) (cameraViewDepth * 1.5) * 1.3f);
-		int renderListIndex=0;
-		renderListIndex=renderListToggle?0:1;renderList[renderListIndex].sendToGPU(gl);
-		//gpu.getMemoryManager().unmap();
-		// Render objects
-		renderList[renderListIndex].render(gl);
-		}
-	
-	public void activate(){active=true;}//TODO: Remove this when paged conversion is complete.
-	
-	public void updateVisibilityList()
-		{
-			renderListToggle=!renderListToggle;
-			renderList[renderListToggle?0:1].reset();
-			rootGrid.itemsWithinRadiusOf(
-				camera.getCameraPosition().add(
-					camera.getLookAtVector().scalarMultiply(getCamera().getViewDepth() / 2.1)),
-					renderList[renderListToggle?0:1].getSubmitter());
-		}//end updateVisibilityList()
-	
-	public void setFogColor(Color c)
-		{
-	    	fogColor.set(
-			(float) c.getRed() / 255f, 
-			(float) c.getGreen() / 255f, 
-			(float)c.getBlue() / 255f);
-		}
-	/**
-	 * @return the camera
-	 */
-	public Camera getCamera()
-		{return camera;}
+	initialized = true;
+    }// end ensureInit()
 
-	/**
-	 * @return the rootGrid
-	 */
-	public RenderableSpacePartitioningGrid getRootGrid()
-		{return rootGrid;}
-
-	/**
-	 * @param rootGrid the rootGrid to set
-	 */
-	public void setRootGrid(RenderableSpacePartitioningGrid rootGrid)
-		{
-		this.rootGrid = rootGrid;
-		}
+    private void fpsTracking() {
+	frameNumber++;
+	if ((frameNumber %= 20) == 0) {
+	    final int fps = (1000 / (int) (System.currentTimeMillis() - lastTimeMillis));
+	    gpu.getTr().getReporter()
+		    .report("org.jtrfp.trcl.core.Renderer.FPS", "" + fps);
 	}
+	lastTimeMillis = System.currentTimeMillis();
+    }
+
+    public void render() {
+	if (!active)
+	    return;
+	fpsTracking();
+	// Update GPU
+	PrimitiveList.tickAnimators();
+	ensureInit();
+	final GL3 gl = gpu.getGl();
+	setFogColor(gpu.getTr().getWorld().getFogColor());
+	gl.glClear(GL2.GL_DEPTH_BUFFER_BIT);
+	final double cameraViewDepth = camera.getViewDepth();
+	fogStart.set((float) (cameraViewDepth * 1.2) / 5f);
+	fogEnd.set((float) (cameraViewDepth * 1.5) * 1.3f);
+	int renderListIndex = 0;
+	renderListIndex = renderListToggle ? 0 : 1;
+	renderList[renderListIndex].sendToGPU(gl);
+	renderList[renderListIndex].render(gl);
+    }
+
+    public void activate() {
+	active = true;
+    }// TODO: Remove this when paged conversion is complete.
+
+    public void updateVisibilityList() {
+	renderListToggle = !renderListToggle;
+	renderList[renderListToggle ? 0 : 1].reset();
+	rootGrid.itemsWithinRadiusOf(
+		camera.getCameraPosition().add(
+			camera.getLookAtVector().scalarMultiply(
+				getCamera().getViewDepth() / 2.1)),
+		renderList[renderListToggle ? 0 : 1].getSubmitter());
+    }// end updateVisibilityList()
+
+    public void setFogColor(Color c) {
+	fogColor.set((float) c.getRed() / 255f, (float) c.getGreen() / 255f,
+		(float) c.getBlue() / 255f);
+    }
+
+    /**
+     * @return the camera
+     */
+    public Camera getCamera() {
+	return camera;
+    }
+
+    /**
+     * @return the rootGrid
+     */
+    public RenderableSpacePartitioningGrid getRootGrid() {
+	return rootGrid;
+    }
+
+    /**
+     * @param rootGrid
+     *            the rootGrid to set
+     */
+    public void setRootGrid(RenderableSpacePartitioningGrid rootGrid) {
+	this.rootGrid = rootGrid;
+    }
+}

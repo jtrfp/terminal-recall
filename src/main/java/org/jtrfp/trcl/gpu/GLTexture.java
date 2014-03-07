dc8f50fa6c57a1cb98341b6c0861441ab9e3cca2
Chuck Ritola <cjritola@gmail.com>
2014-03-07 17:14:02 -0500

    Stylistic cleanup.

--- src/main/java/org/jtrfp/trcl/gpu/GLTexture.java
+++ src/main/java/org/jtrfp/trcl/gpu/GLTexture.java
@@ -6,65 +6,75 @@ import java.nio.IntBuffer;
 
 import javax.media.opengl.GL3;
 
-public class GLTexture
-	{
-	private final GPU gpu;
-	private final int textureID;
-	private int rawSideLength;
-	GLTexture(GPU gpu)
-		{
-		System.out.println("Creating GL Texture...");
-		this.gpu=gpu;
-		textureID=gpu.newTextureID();
-		//Setup the empty rows
-		System.out.println("...Done.");
-		}//end constructor
-	
-	/**
-	 * Takes a square texture in RGBA 8888 format. Automatically determines dimensions from buffer size.
-	 * @param buf	Directly-allocated buffer containing the image data.
-	 * @since Dec 11, 2013
-	 */
-	public void setTextureImageRGBA(ByteBuffer buf)
-		{
-		rawSideLength = (int)Math.sqrt(buf.capacity()/4);
-		buf.rewind();
-		System.out.println("Creating a new OpenGL texture for texture palette...");
-		
-		System.out.println("\t...Done.");
-		System.out.println("Uploading texture palette to OpenGL...");
-		
-		GL3 gl = gpu.getGl();
-		gl.glBindTexture(GL3.GL_TEXTURE_2D, textureID);
-		FloatBuffer isoSize = FloatBuffer.wrap(new float[]{0});
-		gl.glGetFloatv(GL3.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, isoSize);
-		System.out.println("Isotropy limit: "+isoSize.get(0));
-		gl.glTexParameterf(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_MAX_ANISOTROPY_EXT, isoSize.get(0));
-		gl.glTexParameteri(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_WRAP_S, GL3.GL_REPEAT);
-		gl.glTexParameteri(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_WRAP_T, GL3.GL_REPEAT);
-		gl.glTexParameteri(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_MIN_FILTER, 
-		                   GL3.GL_LINEAR_MIPMAP_LINEAR);
-		System.out.println("Uploading texture...");
-		gl.glTexImage2D(GL3.GL_TEXTURE_2D, 0, GL3.GL_RGBA4, rawSideLength, rawSideLength, 0, GL3.GL_RGBA, GL3.GL_UNSIGNED_BYTE, buf);
-		gl.glGenerateMipmap(GL3.GL_TEXTURE_2D);
-		System.out.println("\t...Done.");
-		}
-	
-	public void delete()
-		{
-	    	GL3 gl = gpu.getGl();
-		gl.glBindTexture(GL3.GL_TEXTURE_2D, textureID);
-		gl.glDeleteTextures(1, IntBuffer.wrap(new int[]{textureID}));
-		}
+public class GLTexture {
+    private final GPU gpu;
+    private final int textureID;
+    private int rawSideLength;
 
-	int getTextureID()
-		{return textureID;}
-	
-	public static void specifyTextureUnit(GL3 gl,int unitNumber)
-		{gl.glActiveTexture(GL3.GL_TEXTURE0+unitNumber);}
-	
-	public void bind(GL3 gl)
-		{gl.glBindTexture(GL3.GL_TEXTURE_2D, getTextureID());}
-	
-	public int getCurrentSideLength(){return rawSideLength;}
-	}//GLTexture
+    GLTexture(GPU gpu) {
+	System.out.println("Creating GL Texture...");
+	this.gpu = gpu;
+	textureID = gpu.newTextureID();
+	// Setup the empty rows
+	System.out.println("...Done.");
+    }// end constructor
+
+    /**
+     * Takes a square texture in RGBA 8888 format. Automatically determines
+     * dimensions from buffer size.
+     * 
+     * @param buf
+     *            Directly-allocated buffer containing the image data.
+     * @since Dec 11, 2013
+     */
+    public void setTextureImageRGBA(ByteBuffer buf) {
+	rawSideLength = (int) Math.sqrt(buf.capacity() / 4);
+	buf.rewind();
+	System.out
+		.println("Creating a new OpenGL texture for texture palette...");
+
+	System.out.println("\t...Done.");
+	System.out.println("Uploading texture palette to OpenGL...");
+
+	GL3 gl = gpu.getGl();
+	gl.glBindTexture(GL3.GL_TEXTURE_2D, textureID);
+	FloatBuffer isoSize = FloatBuffer.wrap(new float[] { 0 });
+	gl.glGetFloatv(GL3.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, isoSize);
+	System.out.println("Isotropy limit: " + isoSize.get(0));
+	gl.glTexParameterf(GL3.GL_TEXTURE_2D,
+		GL3.GL_TEXTURE_MAX_ANISOTROPY_EXT, isoSize.get(0));
+	gl.glTexParameteri(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_WRAP_S,
+		GL3.GL_REPEAT);
+	gl.glTexParameteri(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_WRAP_T,
+		GL3.GL_REPEAT);
+	gl.glTexParameteri(GL3.GL_TEXTURE_2D, GL3.GL_TEXTURE_MIN_FILTER,
+		GL3.GL_LINEAR_MIPMAP_LINEAR);
+	System.out.println("Uploading texture...");
+	gl.glTexImage2D(GL3.GL_TEXTURE_2D, 0, GL3.GL_RGBA4, rawSideLength,
+		rawSideLength, 0, GL3.GL_RGBA, GL3.GL_UNSIGNED_BYTE, buf);
+	gl.glGenerateMipmap(GL3.GL_TEXTURE_2D);
+	System.out.println("\t...Done.");
+    }
+
+    public void delete() {
+	GL3 gl = gpu.getGl();
+	gl.glBindTexture(GL3.GL_TEXTURE_2D, textureID);
+	gl.glDeleteTextures(1, IntBuffer.wrap(new int[] { textureID }));
+    }
+
+    int getTextureID() {
+	return textureID;
+    }
+
+    public static void specifyTextureUnit(GL3 gl, int unitNumber) {
+	gl.glActiveTexture(GL3.GL_TEXTURE0 + unitNumber);
+    }
+
+    public void bind(GL3 gl) {
+	gl.glBindTexture(GL3.GL_TEXTURE_2D, getTextureID());
+    }
+
+    public int getCurrentSideLength() {
+	return rawSideLength;
+    }
+}// GLTexture

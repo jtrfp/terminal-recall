ae1a3ee68b508cb2ae3160e9b3d7a05872c667e3
Chuck Ritola <cjritola@gmail.com>
2014-03-09 00:02:58 -0500

    Initial creation fo GLFrameBuffer.

---
+++ src/main/java/org/jtrfp/trcl/gpu/GLFrameBuffer.java
@@ -0,0 +1,43 @@
+package org.jtrfp.trcl.gpu;
+
+import java.nio.IntBuffer;
+
+import javax.media.opengl.GL3;
+
+public final class GLFrameBuffer {
+    private final int id;
+    private GL3 gl;
+
+    GLFrameBuffer(GL3 gl) {
+	this.gl=gl;
+	IntBuffer result = IntBuffer.wrap(new int[1]);
+	gl.glGenFramebuffers(1, result);
+	id = result.get(0);
+    }
+    
+    public GLFrameBuffer bindToRead(){
+	gl.glBindFramebuffer(GL3.GL_READ_FRAMEBUFFER, id);
+	return this;
+    }
+    public GLFrameBuffer bindToDraw(){
+	gl.glBindFramebuffer(GL3.GL_DRAW_FRAMEBUFFER, id);
+	return this;
+    }
+    
+    public GLFrameBuffer setGl(GL3 gl){
+	this.gl=gl;
+	return this;
+    }
+
+    public GLFrameBuffer attachDrawTexture(GLTexture texture,
+	    int attachmentIndex) {
+	gl.glFramebufferTexture(GL3.GL_FRAMEBUFFER, attachmentIndex, texture.getTextureID(), 0);
+	return this;
+    }
+
+    public GLFrameBuffer attachDepthRenderBuffer(
+	    GLRenderBuffer renderBuffer) {
+	gl.glFramebufferRenderbuffer(GL3.GL_FRAMEBUFFER, GL3.GL_DEPTH_ATTACHMENT, GL3.GL_RENDERBUFFER, renderBuffer.getId());
+	return this;
+    }
+}//end GLFrameBuffer

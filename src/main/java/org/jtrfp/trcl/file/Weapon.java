d9acf013581a553974035de0b2b7f27bafce9cd0
Chuck Ritola <cjritola@gmail.com>
2014-03-15 04:12:36 -0400

    Added honing property to Weapon.

--- src/main/java/org/jtrfp/trcl/file/Weapon.java
+++ src/main/java/org/jtrfp/trcl/file/Weapon.java
@@ -9,35 +9,36 @@ import org.jtrfp.trcl.file.ModelingType.FlatModelingType;
 
 public enum Weapon{
     	/// THESE ARE LISTED REPRESENTATIVE OF THE ORDINAL ORDER IN A DEF FILE. DO NOT RE-ORDER!!
-	purpleLaser(null,null,new FlatModelingType("PURFRIN2.RAW",new Dimension(80000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false),
-	PAC("PAC","SKL",new FlatModelingType("SQGLSR.RAW",new Dimension(120000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*6,1,2048,true),
-	ION("ION","DC1",new BillboardModelingType(new String[]{"PULSE0.RAW","PULSE1.RAW","PULSE2.RAW"},70,new Dimension(320000,320000)),8192,ModelingType.MAP_SQUARE_SIZE*6,2,2048,false),
-	RTL("RTL","RFL20",new FlatModelingType("NEWLASER.RAW",new Dimension(100000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*8,3,2048,true),
-	fireBall(null,null,new BINModelingType("FIREBALL.BIN"),8192,ModelingType.MAP_SQUARE_SIZE*8,-1,1024,false),
-	greenLaser(null,null,new FlatModelingType("GIDMIS.RAW",new Dimension(80000,560000)),8192,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false),
-	redLaser(null,null,new FlatModelingType("SQGLSR.RAW",new Dimension(80000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*7,-1,2048,false),
-	blueLaser(null,null,new FlatModelingType("BFIRJ0.RAW",new Dimension(80000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*7,-1,2048,false),
-	bullet(null,null,new BINModelingType("BULLET.BIN"),6554,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false),
-	purpleBall(null,null,new BillboardModelingType(new String[]{"PBALL1.RAW","PBALL3.RAW","PBALL4.RAW"},70,new Dimension(320000,320000)),6554,ModelingType.MAP_SQUARE_SIZE*3,-1,2048,false),
-	blueFireBall(null,null,new BillboardModelingType(new String[]{"BFIRJ0.RAW","BFIRJ2.RAW"},100,new Dimension(320000,320000)),6554,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false),
-	goldBall(null,null,new BINModelingType("FIREBALL.BIN"),8000,ModelingType.MAP_SQUARE_SIZE*10,-1,2048,false),
-	atomWeapon(null,null,new BillboardModelingType(new String[]{"ATM2.RAW","ATM3.RAW"},70,new Dimension(320000,320000)),10000,ModelingType.MAP_SQUARE_SIZE*4,-1,2048,false),
-	purpleRing(null,null,new BillboardModelingType(new String[]{"PURFRIN2.RAW","PURFRIN3.RAW"},8192,new Dimension(320000,320000)),8192,ModelingType.MAP_SQUARE_SIZE*3,-1,2048,false),
-	bossW6(null,null,new BINModelingType("MISSILE.BIN"),6554,ModelingType.MAP_SQUARE_SIZE*7,-1,2048,false),
-	bossW7(null,null,new BillboardModelingType(new String[]{"BFIRJ0.RAW","BFIRJ2.RAW"},100,new Dimension(320000,320000)),8192,ModelingType.MAP_SQUARE_SIZE*4,-1,2048,false),
-	bossW8(null,null,new BINModelingType("FIREBALL.BIN"),8192,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false),
-	enemyMissile(null,null,new BINModelingType("MISSILE.BIN"),8192,ModelingType.MAP_SQUARE_SIZE*2,-1,2048,false),
-	MAM("MAM","DOM",new BINModelingType("MISSILE.BIN"),16384,ModelingType.MAP_SQUARE_SIZE*5,4,2048,false),
+	purpleLaser(null,null,new FlatModelingType("PURFRIN2.RAW",new Dimension(80000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false,false),
+	PAC("PAC","SKL",new FlatModelingType("SQGLSR.RAW",new Dimension(120000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*6,1,2048,true,false),
+	ION("ION","DC1",new BillboardModelingType(new String[]{"PULSE0.RAW","PULSE1.RAW","PULSE2.RAW"},70,new Dimension(320000,320000)),8192,ModelingType.MAP_SQUARE_SIZE*6,2,2048,false,false),
+	RTL("RTL","RFL20",new FlatModelingType("NEWLASER.RAW",new Dimension(100000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*8,3,2048,true,false),
+	fireBall(null,null,new BINModelingType("FIREBALL.BIN"),8192,ModelingType.MAP_SQUARE_SIZE*8,-1,1024,false,false),
+	greenLaser(null,null,new FlatModelingType("GIDMIS.RAW",new Dimension(80000,560000)),8192,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false,false),
+	redLaser(null,null,new FlatModelingType("SQGLSR.RAW",new Dimension(80000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*7,-1,2048,false,false),
+	blueLaser(null,null,new FlatModelingType("BFIRJ0.RAW",new Dimension(80000,560000)),4096,ModelingType.MAP_SQUARE_SIZE*7,-1,2048,false,false),
+	bullet(null,null,new BINModelingType("BULLET.BIN"),6554,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false,false),
+	purpleBall(null,null,new BillboardModelingType(new String[]{"PBALL1.RAW","PBALL3.RAW","PBALL4.RAW"},70,new Dimension(320000,320000)),6554,ModelingType.MAP_SQUARE_SIZE*3,-1,2048,false,false),
+	blueFireBall(null,null,new BillboardModelingType(new String[]{"BFIRJ0.RAW","BFIRJ2.RAW"},100,new Dimension(320000,320000)),6554,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false,false),
+	goldBall(null,null,new BINModelingType("FIREBALL.BIN"),8000,ModelingType.MAP_SQUARE_SIZE*10,-1,2048,false,false),
+	atomWeapon(null,null,new BillboardModelingType(new String[]{"ATM2.RAW","ATM3.RAW"},70,new Dimension(320000,320000)),10000,ModelingType.MAP_SQUARE_SIZE*4,-1,2048,false,false),
+	purpleRing(null,null,new BillboardModelingType(new String[]{"PURFRIN2.RAW","PURFRIN3.RAW"},8192,new Dimension(320000,320000)),8192,ModelingType.MAP_SQUARE_SIZE*3,-1,2048,false,false),
+	bossW6(null,null,new BINModelingType("MISSILE.BIN"),6554,ModelingType.MAP_SQUARE_SIZE*7,-1,2048,false,false),
+	bossW7(null,null,new BillboardModelingType(new String[]{"BFIRJ0.RAW","BFIRJ2.RAW"},100,new Dimension(320000,320000)),8192,ModelingType.MAP_SQUARE_SIZE*4,-1,2048,false,false),
+	bossW8(null,null,new BINModelingType("FIREBALL.BIN"),8192,ModelingType.MAP_SQUARE_SIZE*6,-1,2048,false,false),
+	enemyMissile(null,null,new BINModelingType("MISSILE.BIN"),8192,ModelingType.MAP_SQUARE_SIZE*2,-1,2048,false,false),
+	MAM("MAM","DOM",new BINModelingType("MISSILE.BIN"),16384,ModelingType.MAP_SQUARE_SIZE*5,4,2048,false,false),
 	//////// THESE ARE NOT PART OF THE ORDINAL ORDER OF A DEF FILE AND MAY BE RE-ORDERED
-	SAD("SAD","VIP",new BINModelingType("MISSILE.BIN"),32768,ModelingType.MAP_SQUARE_SIZE*5,5,2048,false),
-	SWT("SWT","BFM",new BINModelingType("MISSILE.BIN"),65536,ModelingType.MAP_SQUARE_SIZE*7,6,2048,false),
-	DAM("DAM","FFF",new FlatModelingType("FIRBAL0.RAW",new Dimension(80000,560000)),Integer.MAX_VALUE,0,7,ModelingType.MAP_SQUARE_SIZE*15,false);
+	SAD("SAD","VIP",new BINModelingType("MISSILE.BIN"),32768,ModelingType.MAP_SQUARE_SIZE*5,5,2048,false,true),
+	SWT("SWT","BFM",new BINModelingType("MISSILE.BIN"),65536,ModelingType.MAP_SQUARE_SIZE*7,6,2048,false,true),
+	DAM("DAM","FFF",new FlatModelingType("FIRBAL0.RAW",new Dimension(80000,560000)),Integer.MAX_VALUE,0,7,ModelingType.MAP_SQUARE_SIZE*15,false,false);
 	private final String tvDisplayName,f3DisplayName;
 	private final int damage,speed,buttonToSelect;
 	private final ModelingType modelingType;
-	private final boolean laser;
+	private final boolean laser,honing;
 	Weapon(String tvDisplayName, String f3DisplayName, ModelingType modelingType,
-		int damage,int speed, int buttonToSelect, int hitRadius, boolean laser){
+		int damage,int speed, int buttonToSelect, int hitRadius, boolean laser, 
+		boolean honing){
 	    this.modelingType=modelingType;
 	    this.damage=damage;
 	    this.speed=speed;
@@ -45,6 +46,7 @@ public enum Weapon{
 	    this.tvDisplayName=tvDisplayName;
 	    this.f3DisplayName=f3DisplayName;
 	    this.laser=laser;
+	    this.honing=honing;
 	}//end constructor
 	/**
 	 * @return the damage
@@ -93,5 +95,11 @@ public enum Weapon{
 	    return laser;
 	   
 	
+	}
+	/**
+	 * @return the honing
+	 */
+	public boolean isHoning() {
+	    return honing;
 	}
 }//end Weapon

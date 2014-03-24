76902b4981ad1bc565d97ae93ad3773b755ebce6
Chuck Ritola <cjritola@gmail.com>
2014-03-24 19:01:17 -0400

    Cleanup, added performance hint improvements to RawGLBuffer.

--- src/main/java/org/jtrfp/trcl/gpu/RawGLBuffer.java
+++ src/main/java/org/jtrfp/trcl/gpu/RawGLBuffer.java
@@ -21,115 +21,116 @@ import java.nio.IntBuffer;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GL3;
 
-public class RawGLBuffer
-	{
-	private int bufferID;
-	protected ByteBuffer localBuffer;
-	private boolean mapped=false;
-	protected static GL3 gl;
-	protected static GPU gpu;
-	private int memoryUsageHint=MemoryUsageHint.DynamicRead.getGLEnumInt();
-	
-	protected RawGLBuffer(int sizeInBytes, GPU gpu)
-		{
-		if(sizeInBytes==0)throw new RuntimeException("Cannot allocate a buffer of size zero.");
-		RawGLBuffer.gpu=gpu;
-		gl=gpu.getGl();
-		final IntBuffer iBuf = IntBuffer.allocate(1);
-		gl.glGenBuffers(1, iBuf);
-		
-		bufferID=iBuf.get();
-		gl.glBindBuffer(getBindingTarget(), bufferID);
-		gl.glBufferData(getBindingTarget(), sizeInBytes, null, getReadWriteParameter());
-		gl.glBindBuffer(getBindingTarget(), 0);
-		//map(gl); //causes more problems than it solves
-		}//end constructor
-	
-	void setUsageHint(int hint){memoryUsageHint=hint;}
-	
-	protected int getReadWriteParameter()
-		{return memoryUsageHint;}
-	
-	protected int getBindingTarget()
-		{return GL2.GL_ARRAY_BUFFER;}
-	
-	public void rewind()
-		{localBuffer.rewind();}
-	
-	public void map(GL3 gl)
-		{
-		if(mapped)return;
-		gl.glBindBuffer(getBindingTarget(),bufferID);
-		localBuffer = gl.glMapBuffer(getBindingTarget(), GL3.GL_READ_WRITE);
-		if(localBuffer==null)
-			{throw new NullPointerException("Failed to map buffer.");}
-		gl.glBindBuffer(getBindingTarget(), 0);
-		mapped=true;
-		}
-	
-	public void unmap(GL3 gl)
-		{
-		if(!mapped)return;
-		gl.glBindBuffer(getBindingTarget(), bufferID);
-		gl.glUnmapBuffer(getBindingTarget());
-		gl.glBindBuffer(getBindingTarget(), 0);
-		mapped=false;
-		}
-	
-	public void free(GL3 gl)
-		{
-		unmap(gl);
-		gl.glDeleteBuffers(1, IntBuffer.wrap(new int [] {bufferID}));
-		localBuffer=null;
-		}
-	
-	public void bind(GL2 gl)
-		{
-		gl.glBindBuffer(getBindingTarget(), getBufferID());
-		}
-	
-	public void bindAsElementArrayBuffer(final GL2 gl)
-		{
-		//gl.glEnableClientState(GL2.GL_ELEMENT_ARRAY_BUFFER);
-		gl.glBindBuffer(GL2.GL_ELEMENT_ARRAY_BUFFER, getBufferID());
-		}
-	
-	public void unbind(GL2 gl)
-		{
-		gl.glBindBuffer(getBindingTarget(), 0);
-		}
-	
-	public void position(int pos)
-		{
-		localBuffer.position(pos);
-		}
-	
-	public int position()
-		{
-		return localBuffer.position();
-		}
+public class RawGLBuffer {
+    private int bufferID;
+    protected ByteBuffer localBuffer;
+    private boolean mapped = false;
+    protected static GL3 gl;
+    protected static GPU gpu;
+    private int memoryUsageHint = MemoryUsageHint.DymamicDraw.getGLEnumInt();
 
-	/**
-	 * @return the bufferID
-	 */
-	public int getBufferID()
-		{
-		return bufferID;
-		}
+    protected RawGLBuffer(int sizeInBytes, GPU gpu) {
+	if (sizeInBytes == 0)
+	    throw new RuntimeException("Cannot allocate a buffer of size zero.");
+	RawGLBuffer.gpu = gpu;
+	gl = gpu.getGl();
+	final IntBuffer iBuf = IntBuffer.allocate(1);
+	gl.glGenBuffers(1, iBuf);
 
-	/**
-	 * @param bufferID the bufferID to set
-	 */
-	public void setBufferID(int bufferID)
-		{
-		this.bufferID = bufferID;
-		}
-	
-	public ByteBuffer getUnderlyingBuffer(){return localBuffer;}
-	public ByteBuffer getDuplicateReferenceOfUnderlyingBuffer()
-		{
-		final ByteBuffer result = localBuffer.duplicate();
-		result.order(localBuffer.order()).clear();
-		return result;
-		}
-	}//end GLBuffer
+	bufferID = iBuf.get();
+	gl.glBindBuffer(getBindingTarget(), bufferID);
+	gl.glBufferData(getBindingTarget(), sizeInBytes, null,
+		getReadWriteParameter());
+	gl.glBindBuffer(getBindingTarget(), 0);
+	// map(gl); //causes more problems than it solves
+    }// end constructor
+
+    void setUsageHint(int hint) {
+	memoryUsageHint = hint;
+    }
+
+    protected int getReadWriteParameter() {
+	return memoryUsageHint;
+    }
+
+    protected int getBindingTarget() {
+	return GL2.GL_ARRAY_BUFFER;
+    }
+
+    public void rewind() {
+	localBuffer.rewind();
+    }
+
+    public void map(GL3 gl) {
+	if (mapped)
+	    return;
+	gl.glBindBuffer(getBindingTarget(), bufferID);
+	localBuffer = gl.glMapBuffer(getBindingTarget(), GL3.GL_WRITE_ONLY);
+	if (localBuffer == null) {
+	    throw new NullPointerException("Failed to map buffer.");
+	}
+	gl.glBindBuffer(getBindingTarget(), 0);
+	mapped = true;
+    }
+
+    public void unmap(GL3 gl) {
+	if (!mapped)
+	    return;
+	gl.glBindBuffer(getBindingTarget(), bufferID);
+	gl.glUnmapBuffer(getBindingTarget());
+	gl.glBindBuffer(getBindingTarget(), 0);
+	mapped = false;
+    }
+
+    public void free(GL3 gl) {
+	unmap(gl);
+	gl.glDeleteBuffers(1, IntBuffer.wrap(new int[] { bufferID }));
+	localBuffer = null;
+    }
+
+    public void bind(GL2 gl) {
+	gl.glBindBuffer(getBindingTarget(), getBufferID());
+    }
+
+    public void bindAsElementArrayBuffer(final GL2 gl) {
+	// gl.glEnableClientState(GL2.GL_ELEMENT_ARRAY_BUFFER);
+	gl.glBindBuffer(GL2.GL_ELEMENT_ARRAY_BUFFER, getBufferID());
+    }
+
+    public void unbind(GL2 gl) {
+	gl.glBindBuffer(getBindingTarget(), 0);
+    }
+
+    public void position(int pos) {
+	localBuffer.position(pos);
+    }
+
+    public int position() {
+	return localBuffer.position();
+    }
+
+    /**
+     * @return the bufferID
+     */
+    public int getBufferID() {
+	return bufferID;
+    }
+
+    /**
+     * @param bufferID
+     *            the bufferID to set
+     */
+    public void setBufferID(int bufferID) {
+	this.bufferID = bufferID;
+    }
+
+    public ByteBuffer getUnderlyingBuffer() {
+	return localBuffer;
+    }
+
+    public ByteBuffer getDuplicateReferenceOfUnderlyingBuffer() {
+	final ByteBuffer result = localBuffer.duplicate();
+	result.order(localBuffer.order()).clear();
+	return result;
+    }
+}// end GLBuffer

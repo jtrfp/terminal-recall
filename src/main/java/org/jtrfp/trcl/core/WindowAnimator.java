a14391d5115334aa0953e86c984f6784583dcd95
Chuck Ritola <cjritola@gmail.com>
2014-03-28 11:56:52 -0400

    Optimization: Switched to 1D array in WindowAnimator.

--- src/main/java/org/jtrfp/trcl/core/WindowAnimator.java
+++ src/main/java/org/jtrfp/trcl/core/WindowAnimator.java
@@ -7,7 +7,8 @@ import org.jtrfp.trcl.IntTransferFunction;
 public class WindowAnimator implements Tickable {
     private final TriangleVertex2FlatDoubleWindow w;
     private final int numElements, numFrames;
-    private final float[][] frames;
+   // private final float[][] frames;
+    private final float [] frames;
     private final boolean loopInterpolate;
     private final Controller controller;
     private int indexCounter=0;
@@ -23,13 +24,14 @@ public class WindowAnimator implements Tickable {
 	this.numFrames=numFrames;
 	this.indexXferFun=indexXferFun;
 	//this.startElement=startElement;
-	frames = new float[numElements*numFrames][];
+	frames = new float[numElements*numFrames];
 	this.loopInterpolate=loopInterpolate;
 	this.controller=controller;
     }
     public void addFrames(float[] newFrames) {
 	//indices[indexCounter]=globalFlatID;
-	frames[indexCounter]=(newFrames);
+	System.arraycopy(newFrames, 0, frames, indexCounter*numFrames, numFrames);
+	//frames[indexCounter]=(newFrames);
 	indexCounter++;
     }//end constructor
     
@@ -45,12 +47,15 @@ public class WindowAnimator implements Tickable {
 	if(hiFrame==0 && !loopInterpolate){lowFrame=0;hiFrame=1;}
 	double hI=interpolation;
 	double lI=1.-interpolation;
+	final int dHi=hiFrame-lowFrame;
+	int indexOffset=lowFrame;
 	for(int index=0; index<numElements; index++){
-	    final double val = frames[index][lowFrame]*lI+frames[index][hiFrame]*hI;
+	    //final double val = frames[index][lowFrame]*lI+frames[index][hiFrame]*hI;
+	    final double val = frames[indexOffset]*lI+frames[indexOffset+dHi]*hI;
 	    //System.out.println("Element index="+index+" location="+indexXferFun.transfer(index));
 	    //if(debugName!=null)if(debugName.contains("uvAnimator"))System.out.println("name: "+debugName+": "+indexXferFun.transfer(index)+" set to "+val);
-	    w.set(
-		    indexXferFun.transfer(index), val);
+	    w.set(indexXferFun.transfer(index), val);
+	    indexOffset+=numFrames;
 	}//end for(numElements)
     }//update()
     /**

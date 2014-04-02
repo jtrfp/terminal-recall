398fdfc2dbc8dd6f7f8cd2219b7c02e3d998a8de
Chuck Ritola <cjritola@gmail.com>
2014-04-01 20:39:48 -0400

    Added support for appending to existing arrays in Triangle.

--- src/main/java/org/jtrfp/trcl/Triangle.java
+++ src/main/java/org/jtrfp/trcl/Triangle.java
@@ -23,62 +23,67 @@ import org.jtrfp.trcl.gpu.Vertex;
 import org.jtrfp.trcl.tools.Util;
 
 public class Triangle {
-    private Vertex [] vertices = new Vertex[3];
-    private Vector2D [] uv = new Vector2D[3];
+    private Vertex[] vertices = new Vertex[3];
+    private Vector2D[] uv = new Vector2D[3];
     private RenderMode renderMode;
     private boolean isAlphaBlended = false;
     private Vector3D centroidNormal;
 
     private Future<TextureDescription> texture;
-    
-    public Triangle(){}
-    public Triangle(Vertex [] vertices){
-	setVertex(vertices[0],0);
-	setVertex(vertices[1],1);
-	setVertex(vertices[2],2);
-	}
-    
+
+    public Triangle() {
+    }
+
+    public Triangle(Vertex[] vertices) {
+	setVertex(vertices[0], 0);
+	setVertex(vertices[1], 1);
+	setVertex(vertices[2], 2);
+    }
+
+    public static Triangle[] quad2Triangles(Vertex[] vertices, Vector2D[] uvs,
+	    Future<TextureDescription> texture, RenderMode mode,
+	    boolean hasAlpha, Vector3D centroidNormal) {
+	return quad2Triangles(vertices, uvs, texture, mode, hasAlpha,
+		centroidNormal, new Triangle[2], 0);
+    }
+
     public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
 	    double[] u, double[] v, Future<TextureDescription> texture,
-	    RenderMode mode, Vector3D centroidNormal) {
-	return quad2Triangles(x, y, z, u, v, texture, mode, false, centroidNormal);
+	    RenderMode mode, Vector3D[] normals, int ringRotation,
+	    Triangle[] dest, int destOffset) {
+	return quad2Triangles(x, y, z, u, v, texture, mode, false, normals,
+		ringRotation, dest, destOffset);
     }
-    
-    public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
-	    double[] u, double[] v, Future<TextureDescription> texture,
-	    RenderMode mode, Vector3D [] normals, int ringRotation) {
-	return quad2Triangles(x, y, z, u, v, texture, mode, false, normals, ringRotation);
-    }
-    
-    public void setVertex(Vertex vtx, int index){
-	if(vertices[index]!=null){
+
+    public void setVertex(Vertex vtx, int index) {
+	if (vertices[index] != null) {
 	    vertices[index].removeTriangle(this);
 	}
-	vertices[index]=vtx;
+	vertices[index] = vtx;
 	vtx.addTriangle(this);
-    }//end setVertex(...)
-    
-    public void setUV(Vector2D uv, int index){
-	this.uv[index]=uv;
+    }// end setVertex(...)
+
+    public void setUV(Vector2D uv, int index) {
+	this.uv[index] = uv;
     }
-    public void setUVNoCopy(Vector2D [] uv){
-	this.uv=uv;
+
+    public void setUVNoCopy(Vector2D[] uv) {
+	this.uv = uv;
     }
-    public void setUVCopy(Vector2D [] uv){
-	for(int i=0; i<3; i++){
-	    this.uv[i]=uv[i];
+
+    public void setUVCopy(Vector2D[] uv) {
+	for (int i = 0; i < 3; i++) {
+	    this.uv[i] = uv[i];
 	}
-    }//end setUVCopy
-    
-    public Vector2D getUV(int index){
+    }// end setUVCopy
+
+    public Vector2D getUV(int index) {
 	return this.uv[index];
     }
-    
-    public static Triangle[] quad2Triangles(
-	    Vertex [] vertices, Vector2D [] uv,
-	    Future<TextureDescription> texture, 
-	    RenderMode mode, boolean hasAlpha){
-	Triangle[] result = new Triangle[2];
+
+    public static Triangle[] quad2Triangles(Vertex[] vertices, Vector2D[] uv,
+	    Future<TextureDescription> texture, RenderMode mode,
+	    boolean hasAlpha, Triangle[] dest, int destOffset) {
 	Triangle t;
 	t = new Triangle();
 	t.setTexture(texture);
@@ -87,30 +92,29 @@ public class Triangle {
 	t.setVertex(vertices[0], 0);
 	t.setVertex(vertices[1], 1);
 	t.setVertex(vertices[2], 2);
-	t.setUV(uv[0],0);
-	t.setUV(uv[1],1);
-	t.setUV(uv[2],2);
-	result[0]=t;
+	t.setUV(uv[0], 0);
+	t.setUV(uv[1], 1);
+	t.setUV(uv[2], 2);
+	dest[0 + destOffset] = t;
 	t = new Triangle();
 	t.setTexture(texture);
 	t.setRenderMode(mode);
 	t.setAlphaBlended(hasAlpha);
-	
+
 	t.setVertex(vertices[2], 0);
 	t.setVertex(vertices[3], 1);
 	t.setVertex(vertices[0], 2);
-	t.setUV(uv[2],0);
-	t.setUV(uv[3],1);
-	t.setUV(uv[0],2);
-	result[1] = t;
-	return result;
+	t.setUV(uv[2], 0);
+	t.setUV(uv[3], 1);
+	t.setUV(uv[0], 2);
+	dest[1 + destOffset] = t;
+	return dest;
     }
-    
-    public static Triangle[] quad2Triangles(
-	    Vertex [] vertices, Vector2D [] uv,
-	    Future<TextureDescription> texture, 
-	    RenderMode mode, boolean hasAlpha, Vector3D centroidNormal){
-	Triangle[] result = new Triangle[2];
+
+    public static Triangle[] quad2Triangles(Vertex[] vertices, Vector2D[] uv,
+	    Future<TextureDescription> texture, RenderMode mode,
+	    boolean hasAlpha, Vector3D centroidNormal, Triangle[] dest,
+	    int destOffset) {
 	Triangle t;
 	t = new Triangle();
 	t.setTexture(texture);
@@ -119,26 +123,34 @@ public class Triangle {
 	t.setVertex(vertices[0], 0);
 	t.setVertex(vertices[1], 1);
 	t.setVertex(vertices[2], 2);
-	t.setUV(uv[0],0);
-	t.setUV(uv[1],1);
-	t.setUV(uv[2],2);
+	t.setUV(uv[0], 0);
+	t.setUV(uv[1], 1);
+	t.setUV(uv[2], 2);
 	t.setCentroidNormal(centroidNormal);
-	result[0]=t;
+	dest[0 + destOffset] = t;
 	t = new Triangle();
 	t.setTexture(texture);
 	t.setRenderMode(mode);
 	t.setAlphaBlended(hasAlpha);
-	
+
 	t.setVertex(vertices[2], 0);
 	t.setVertex(vertices[3], 1);
 	t.setVertex(vertices[0], 2);
-	t.setUV(uv[2],0);
-	t.setUV(uv[3],1);
-	t.setUV(uv[0],2);
+	t.setUV(uv[2], 0);
+	t.setUV(uv[3], 1);
+	t.setUV(uv[0], 2);
 	t.setCentroidNormal(centroidNormal);
-	result[1] = t;
-	return result;
+	dest[1 + destOffset] = t;
+	return dest;
     }
+
+    public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
+	    double[] u, double[] v, Future<TextureDescription> texture,
+	    RenderMode mode, boolean hasAlpha, Vector3D centroidNormal) {
+	return quad2Triangles(x, y, z, u, v, texture, mode, hasAlpha,
+		centroidNormal, new Triangle[2],0);
+    }
+
     /**
      * Converts supplied quad coordinates to a pair of triangles in clockwise
      * order, top-left being index zero.
@@ -146,42 +158,69 @@ public class Triangle {
      */
     public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
 	    double[] u, double[] v, Future<TextureDescription> texture,
-	    RenderMode mode, boolean hasAlpha, Vector3D centroidNormal) {
-	final Vertex [] vertices = new Vertex[]{
-		new Vertex().setPosition(new Vector3D(x[0],y[0],z[0])),
-		new Vertex().setPosition(new Vector3D(x[1],y[1],z[1])),
-		new Vertex().setPosition(new Vector3D(x[2],y[2],z[2])),
-		new Vertex().setPosition(new Vector3D(x[3],y[3],z[3])),
-	};
-	Vector2D [] uvs = new Vector2D[4];
-	for(int i=0; i<4; i++){
-	    uvs[i]=new Vector2D(u[i],v[i]);
+	    RenderMode mode, boolean hasAlpha, Vector3D centroidNormal,
+	    Triangle[] dest, int destOffset) {
+	final Vertex[] vertices = new Vertex[] {
+		new Vertex().setPosition(new Vector3D(x[0], y[0], z[0])),
+		new Vertex().setPosition(new Vector3D(x[1], y[1], z[1])),
+		new Vertex().setPosition(new Vector3D(x[2], y[2], z[2])),
+		new Vertex().setPosition(new Vector3D(x[3], y[3], z[3])), };
+	Vector2D[] uvs = new Vector2D[4];
+	for (int i = 0; i < 4; i++) {
+	    uvs[i] = new Vector2D(u[i], v[i]);
 	}
-	return quad2Triangles(vertices, uvs, texture, mode, hasAlpha, centroidNormal);
-    }//end quad2Triangles(...)
-    
+	return quad2Triangles(vertices, uvs, texture, mode, hasAlpha,
+		centroidNormal, dest, destOffset);
+    }// end quad2Triangles(...)
+
+    public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
+	    double[] u, double[] v, Future<TextureDescription> texture,
+	    RenderMode mode, boolean hasAlpha, Vector3D[] normals,
+	    int ringRotation) {
+	return quad2Triangles(x, y, z, u, v, texture, mode, hasAlpha, normals,
+		ringRotation, new Triangle[2], 0);
+    }
+
     /**
      * Converts supplied quad coordinates to a pair of triangles in clockwise
      * order, top-left being index zero+ringRotation.
-     * @param ringRotation 
+     * 
+     * @param ringRotation
      * 
      */
     public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
 	    double[] u, double[] v, Future<TextureDescription> texture,
-	    RenderMode mode, boolean hasAlpha, Vector3D [] normals, int ringRotation) {
-	final Vertex [] vertices = new Vertex[]{
-		new Vertex().setPosition(new Vector3D(x[(0+ringRotation)%4],y[(0+ringRotation)%4],z[(0+ringRotation)%4])).setNormal(normals[(0+ringRotation)%4]),
-		new Vertex().setPosition(new Vector3D(x[(1+ringRotation)%4],y[(1+ringRotation)%4],z[(1+ringRotation)%4])).setNormal(normals[(1+ringRotation)%4]),
-		new Vertex().setPosition(new Vector3D(x[(2+ringRotation)%4],y[(2+ringRotation)%4],z[(2+ringRotation)%4])).setNormal(normals[(2+ringRotation)%4]),
-		new Vertex().setPosition(new Vector3D(x[(3+ringRotation)%4],y[(3+ringRotation)%4],z[(3+ringRotation)%4])).setNormal(normals[(3+ringRotation)%4]),
-	};
-	Vector2D [] uvs = new Vector2D[4];
-	for(int i=0; i<4; i++){
-	    final int rotI=(i+ringRotation)%4;
-	    uvs[i]=new Vector2D(u[rotI],v[rotI]);
+	    RenderMode mode, boolean hasAlpha, Vector3D[] normals,
+	    int ringRotation, Triangle[] dest, int destOffset) {
+	final Vertex[] vertices = new Vertex[] {
+		new Vertex().setPosition(
+			new Vector3D(x[(0 + ringRotation) % 4],
+				y[(0 + ringRotation) % 4],
+				z[(0 + ringRotation) % 4])).setNormal(
+			normals[(0 + ringRotation) % 4]),
+		new Vertex().setPosition(
+			new Vector3D(x[(1 + ringRotation) % 4],
+				y[(1 + ringRotation) % 4],
+				z[(1 + ringRotation) % 4])).setNormal(
+			normals[(1 + ringRotation) % 4]),
+		new Vertex().setPosition(
+			new Vector3D(x[(2 + ringRotation) % 4],
+				y[(2 + ringRotation) % 4],
+				z[(2 + ringRotation) % 4])).setNormal(
+			normals[(2 + ringRotation) % 4]),
+		new Vertex().setPosition(
+			new Vector3D(x[(3 + ringRotation) % 4],
+				y[(3 + ringRotation) % 4],
+				z[(3 + ringRotation) % 4])).setNormal(
+			normals[(3 + ringRotation) % 4]), };
+	Vector2D[] uvs = new Vector2D[4];
+	for (int i = 0; i < 4; i++) {
+	    final int rotI = (i + ringRotation) % 4;
+	    uvs[i] = new Vector2D(u[rotI], v[rotI]);
 	}
-	return quad2Triangles(vertices, uvs, texture, mode, hasAlpha);
-    }//end quad2Triangles(...)
+	return quad2Triangles(vertices, uvs, texture, mode, hasAlpha, dest,
+		destOffset);
+    }// end quad2Triangles(...)
 
     /**
      * @return the renderMode
@@ -227,29 +266,49 @@ public class Triangle {
     public void setAlphaBlended(boolean isAlphaBlended) {
 	this.isAlphaBlended = isAlphaBlended;
     }
+
     /**
      * @return the vertices
      */
     public Vertex[] getVertices() {
-        return vertices;
+	return vertices;
     }
+
     /**
-     * @param vertices the vertices to set
+     * @param vertices
+     *            the vertices to set
      */
     public void setVertices(Vertex[] vertices) {
-        this.vertices = vertices;
+	this.vertices = vertices;
     }
+
     /**
      * @return the centroidNormal
      */
     public Vector3D getCentroidNormal() {
-        return centroidNormal;
+	return centroidNormal;
     }
+
     /**
-     * @param centroidNormal the centroidNormal to set
+     * @param centroidNormal
+     *            the centroidNormal to set
      */
     public void setCentroidNormal(Vector3D centroidNormal) {
-        this.centroidNormal = centroidNormal;
+	this.centroidNormal = centroidNormal;
+    }
+
+    public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
+	    double[] u, double[] v, Future<TextureDescription> texture,
+	    RenderMode renderMode, Vector3D[] normals, int ringRotation) {
+	return quad2Triangles(x, y, z, u, v, texture, renderMode, normals,
+		ringRotation, new Triangle[2], 0);
+    }
+
+    public static Triangle[] quad2Triangles(double[] x, double[] y, double[] z,
+	    double[] u, double[] v, Future<TextureDescription> texture,
+	    RenderMode renderMode, Vector3D centroidNormal) {
+	return quad2Triangles(x, y, z, u, v, texture, renderMode, false,
+		centroidNormal, new Triangle[2], 0);
     }
 }// Triangle
 

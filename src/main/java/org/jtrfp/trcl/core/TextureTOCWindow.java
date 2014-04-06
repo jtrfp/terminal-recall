5d7689e7bec93841e8e7e4d41ba0e802162fbf27
Chuck Ritola <cjritola@gmail.com>
2014-04-05 22:43:06 -0400

    Initial creation of TextureTOCWindow.

---
+++ src/main/java/org/jtrfp/trcl/core/TextureTOCWindow.java
@@ -0,0 +1,13 @@
+package org.jtrfp.trcl.core;
+
+import org.jtrfp.trcl.mem.MemoryWindow;
+
+public class TextureTOCWindow extends MemoryWindow {
+    public final IntArrayVariable subtexturePageIndices = new IntArrayVariable(361);//1444 bytes
+    public final ShortVariable width = new ShortVariable();//2
+    public final ShortVariable height = new ShortVariable();//2
+    public final ByteVariable encodingMode = new ByteVariable();//1
+    public final IntArrayVariable codeBookPageIndices = new IntArrayVariable(4);//16
+    //Tally: 1465
+    public final ByteArrayVariable unused = new ByteArrayVariable(71);//71
+}

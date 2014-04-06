1024dcbe322fd83ec7439fb86dbbc2b8d1f2ecbc
Chuck Ritola <cjritola@gmail.com>
2014-04-06 03:17:05 -0400

    Initial creation of Gen2Texture.

---
+++ src/main/java/org/jtrfp/trcl/core/Gen2Texture.java
@@ -0,0 +1,19 @@
+package org.jtrfp.trcl.core;
+
+import java.awt.Dimension;
+
+/**
+ * INCOMPLETE - DO NOT USE
+ * @author Chuck Ritola
+ *
+ */
+
+public class Gen2Texture {
+    private final TextureManager textureSystem;
+    Gen2Texture(TextureManager textureSystem){
+	this.textureSystem=textureSystem;
+    }
+    public void setSize(Dimension dimension) {
+	// TODO Auto-generated method stub
+    }
+}//end Gen2Texture

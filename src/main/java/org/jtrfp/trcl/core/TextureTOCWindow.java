f9f32e936b7de58990a94e0293689c496a58b5fe
Chuck Ritola <cjritola@gmail.com>
2014-04-10 06:12:29 -0400

    Redesign of TextureTOCWindow.

--- src/main/java/org/jtrfp/trcl/core/TextureTOCWindow.java
+++ src/main/java/org/jtrfp/trcl/core/TextureTOCWindow.java
@@ -3,13 +3,14 @@ package org.jtrfp.trcl.core;
 import org.jtrfp.trcl.mem.MemoryWindow;
 
 public class TextureTOCWindow extends MemoryWindow {
-    public final IntArrayVariable subtexturePageIndices = new IntArrayVariable(361);//1444 bytes
-    public final ShortVariable width = new ShortVariable();//2
-    public final ShortVariable height = new ShortVariable();//2
-    public final ByteVariable encodingMode = new ByteVariable();//1
-    public final IntArrayVariable codeBookPageIndices = new IntArrayVariable(4);//16
-    //Tally: 1465
-    public final ByteArrayVariable unused = new ByteArrayVariable(71);//71
+    //19^2 * 4 = 1444 bytes
+    public final IntArrayVariable subtexturePageIndices = new IntArrayVariable(361);
+    //Offset 1456B, 91VEC4
+    public final IntVariable width = new IntVariable();//4B
+    public final IntVariable height = new IntVariable();//4B
+    public final IntVariable startTile = new IntVariable();//4B
+    //Tally: 1468B
+    public final ByteArrayVariable unused = new ByteArrayVariable(68);//68B
     
     public TextureTOCWindow(TR tr){
 	this.init(tr, "TextureTOCWindow");

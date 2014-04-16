03b0b01b6e50d75aded6660b3a0e86a78501ed6e
Chuck Ritola <cjritola@gmail.com>
2014-04-16 08:51:46 -0400

    Initial creation of RGBA8888VectorList.

---
+++ src/main/java/org/jtrfp/trcl/img/vq/RGBA8888VectorList.java
@@ -0,0 +1,37 @@
+package org.jtrfp.trcl.img.vq;
+
+public final class RGBA8888VectorList implements VectorList {
+    private final VectorList source;
+    private final int singletOffset;
+
+    public RGBA8888VectorList(VectorList source, int singletOffset) {
+	this.source = source;
+	this.singletOffset = singletOffset;
+    }
+
+    public RGBA8888VectorList(VectorList source) {
+	this(source, 0);
+    }
+
+    @Override
+    public int getNumVectors() {
+	return source.getNumVectors() / 4;
+    }
+
+    @Override
+    public int getNumComponentsPerVector() {
+	return 4;
+    }
+
+    @Override
+    public double componentAt(int vectorIndex, int componentIndex) {
+	return source.componentAt(vectorIndex / 4 + componentIndex,
+		singletOffset);
+    }
+
+    @Override
+    public void setComponentAt(int vectorIndex, int componentIndex, double value) {
+	source.setComponentAt(vectorIndex / 4 + componentIndex, singletOffset,
+		value);
+    }
+}// end RGBA8888VectorList

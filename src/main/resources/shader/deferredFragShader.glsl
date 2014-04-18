aedf8f2ae1d407f071d250f7b217b8bedaca6135
Chuck Ritola <cjritola@gmail.com>
2014-04-18 19:19:05 -0400

    Incremental implementation of new texturing system.

--- src/main/resources/shader/deferredFragShader.glsl
+++ src/main/resources/shader/deferredFragShader.glsl
@@ -33,8 +33,22 @@ uniform vec3 			sunVector;
 
 // OUTPUTS
 layout(location = 0) out vec4 fragColor;
-const vec3 sunColor = vec3(1.4,1.4,1.2);
 
+// CONSTANTS
+const uint TOC_OFFSET_VEC4_HEADER				=91u;//1456/16
+const uint TOC_HEADER_OFFSET_QUADS_WIDTH		=0u;
+const uint TOC_HEADER_OFFSET_QUADS_HEIGHT		=1u;
+const uint TOC_HEADER_OFFSET_QUADS_START_TILE	=2u;
+
+const float TILE_PAGE_SIDE_WIDTH_TEXELS = 128;
+const uint TILE_SIDE_WIDTH_TEXELS 		= 4u;
+const uint TILE_PAGE_SIDE_WIDTH_TILES	= uint(TILE_PAGE_SIDE_WIDTH_TEXELS) / TILE_SIDE_WIDTH_TEXELS;
+const uint TILES_PER_TILE_PAGE 			= TILE_PAGE_SIDE_WIDTH_TILES * TILE_PAGE_SIDE_WIDTH_TILES;
+
+const uint SUBTEXTURE_SIDE_WIDTH_TILES = 39u;
+const uint SUBTEXTURE_SIDE_WIDTH_TEXELS = SUBTEXTURE_SIDE_WIDTH_TILES * TILE_SIDE_WIDTH_TEXELS;
+
+const vec3 sunColor 					= vec3(1.4,1.4,1.2);
 
 //Adapted from http://www.geeks3d.com/20091216/geexlab-how-to-visualize-the-depth-buffer-in-glsl/
 float linearizeDepth(float z){
@@ -44,6 +58,19 @@ z = (2*zNear) / (zFar + zNear - z * (zFar - zNear));
 return z;
 }
 
+uint UByte(uint _input, uint index)
+	{return (_input >> 8u*index) & 0x000000FFu;}
+
+////////// STRUCT LAYOUT DOCUMENTATION ///////////////
+/**
+textureTOC{
+	19^2 x 4B
+	offset 1456B or 91VEC4:
+	TOCHeader {4B width, 4B height, 4B startTile, 4B ???}
+	Unused 54B
+	}
+**/
+
 void main(){
 vec2	screenLoc 	= vec2(gl_FragCoord.x/screenWidth,gl_FragCoord.y/screenHeight);
 float 	depth 		= texture(depthTexture,screenLoc)[0];
@@ -51,10 +78,27 @@ gl_FragDepth 		= depth;
 float 	linearDepth = linearizeDepth(depth);
 uint 	textureID 	= texture(textureIDTexture,screenLoc)[0u];
 fragColor 			= (textureID==10u?texture(primaryRendering,screenLoc):vec4(0,0,0,0));//GET UV
-vec3 origColor 		= texture(texturePalette,fragColor.xy).rgb;//GET COLOR
-vec3 norm 			= texture(normTexture,screenLoc).xyz*2-vec3(1,1,1);//UNPACK NORM
+vec3 	origColor 	= texture(texturePalette,fragColor.xy).rgb;//GET COLOR
+vec3 	norm 		= texture(normTexture,screenLoc).xyz*2-vec3(1,1,1);//UNPACK NORM
 
-uvec4 toc 			= texelFetch(rootBuffer,int(textureID));
+uvec4 	tocHeader 	= texelFetch(rootBuffer,int(textureID+TOC_OFFSET_VEC4_HEADER));
+vec2	tDims		= vec2(float(tocHeader[TOC_HEADER_OFFSET_QUADS_WIDTH]),float(tocHeader[TOC_HEADER_OFFSET_QUADS_HEIGHT]));
+uint	startTile	= tocHeader[TOC_HEADER_OFFSET_QUADS_START_TILE];
+vec2	texelUV		= tDims*fragColor.xy;
+uint	tTOCIdx		= uint(texelUV.x)/SUBTEXTURE_SIDE_WIDTH_TEXELS + (uint(texelUV.y)/SUBTEXTURE_SIDE_WIDTH_TEXELS) * 19u;
+uint	tTOCvec4Idx	= tTOCIdx / 4u;
+uint	tTOCsubIdx	= tTOCIdx % 4u;
+uint	tilePgAddr	= texelFetch(rootBuffer,int(textureID+tTOCvec4Idx))[tTOCsubIdx];
+uvec2	tilePgXY	= uvec2(mod(uvec2(texelUV),SUBTEXTURE_SIDE_WIDTH_TEXELS));
+uint	tilePgBytIdx= (uint(tilePgXY.x)/TILE_SIDE_WIDTH_TEXELS + (uint(tilePgXY.y)/TILE_SIDE_WIDTH_TEXELS) * 39u);
+uint	tilePgv4Idx	= tilePgBytIdx / 16u;
+uint	tilePgv4Sub = tilePgBytIdx % 16u;
+uint	tileIdx		= UByte((texelFetch(rootBuffer,int(tilePgv4Idx))[tilePgv4Sub/4u]),tilePgv4Sub%4u);
+uint	tilePgIdx	= (tileIdx+startTile) / TILES_PER_TILE_PAGE;
+uvec2	tilePgUV	= uvec2((tilePgIdx % TILE_PAGE_SIDE_WIDTH_TILES),(tilePgIdx / TILE_PAGE_SIDE_WIDTH_TILES)%TILE_PAGE_SIDE_WIDTH_TILES)*TILE_SIDE_WIDTH_TEXELS;
+uint	tilePgArrID = tilePgIdx / TILES_PER_TILE_PAGE;
+//uvec2	tilePgUVsub = ;
+
 uint indexPage;
 uint codeBook;
 uint tileID;
@@ -63,7 +107,7 @@ uvec4 tile;
 
 // DUMMY CODE TO SIMULATE PROCESSING LOAD OF FUTURE IMPLEMENTATION
 for(int i=0;i<1;i++){
-	indexPage 	= texelFetch(rootBuffer,int(toc[0u])+i).x;
+	indexPage 	= texelFetch(rootBuffer,int(tocHeader[0u])+i).x;
 	codeBook 	= texelFetch(rootBuffer,int(indexPage)).y;
 	tileID 		= texelFetch(rootBuffer,int(codeBook)).z;
 	tileID 		= texelFetch(rootBuffer,int(tileID)).z;
@@ -76,4 +120,4 @@ float sunIllumination	= length(norm)>.1?clamp(dot(sunVector,normalize(norm)),0,1
 fragColor.rgb 			= origColor*fogColor+origColor*sunIllumination*sunColor;
 fragColor.r 			+=(texture(rgbaTiles,vec3(.5,.5,1)).x*.00000000001);//DUMMY
 fragColor 				= mix(fragColor,vec4(fogColor*sunColor,1),clamp(pow(linearDepth,3)*1.5,0,1));//FOG
-}
+}//end main()

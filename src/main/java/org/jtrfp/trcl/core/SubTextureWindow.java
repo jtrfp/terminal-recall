dab8ecc79ca848e7391ad91169b04fcc4717d38c
Chuck Ritola <cjritola@gmail.com>
2014-05-01 17:21:54 -0400

    Bugfix: Incorrect visibility in SubTextureWindow.codeIDs resulted in missed init.

--- src/main/java/org/jtrfp/trcl/core/SubTextureWindow.java
+++ src/main/java/org/jtrfp/trcl/core/SubTextureWindow.java
@@ -5,7 +5,7 @@ import org.jtrfp.trcl.mem.MemoryWindow;
 public class SubTextureWindow extends MemoryWindow {
     public static final int SIDE_LENGTH_CODES = 39;
     //39 tiles squared = 1521
-    final ByteArrayVariable codeIDs = new ByteArrayVariable(1521);
+    public final ByteArrayVariable codeIDs = new ByteArrayVariable(1521);
     public final ByteArrayVariable unused = new ByteArrayVariable(15);// 15
     public void setTile(int objectID, int x, int y, byte tileID){
 	codeIDs.setAt(objectID,x+y*SIDE_LENGTH_CODES,tileID);

8b0d275f9987db93b01f4bb233dae199109039f9
Chuck Ritola <cjritola@gmail.com>
2014-05-05 04:39:53 -0400

    Initial creation of GLExecutorThread.

---
+++ src/main/java/org/jtrfp/trcl/core/GLExecutorThread.java
@@ -0,0 +1,20 @@
+package org.jtrfp.trcl.core;
+
+import javax.media.opengl.GLContext;
+
+public class GLExecutorThread extends Thread {
+    private final GLContext context;
+    public GLExecutorThread(GLContext context, Runnable runnable) {
+	super(runnable);
+	setName("glThreadPool");
+	setPriority(ThreadManager.RENDERING_PRIORITY);
+	this.context=context;
+    }
+    /**
+     * @return the context
+     */
+    public GLContext getContext() {
+        return context;
+    }
+
+}//end GLExecutorThread

bb9b2a2a3c0dc434599f82ec086fc75df5d31380
Chuck Ritola <cjritola@gmail.com>
2014-05-08 09:29:42 -0400

    Changed class modifier of WorldObject2DVisibleEverywhere to abstract.

--- src/main/java/org/jtrfp/trcl/obj/WorldObject2DVisibleEverywhere.java
+++ src/main/java/org/jtrfp/trcl/obj/WorldObject2DVisibleEverywhere.java
@@ -3,7 +3,7 @@ package org.jtrfp.trcl.obj;
 import org.jtrfp.trcl.Model;
 import org.jtrfp.trcl.core.TR;
 
-public class WorldObject2DVisibleEverywhere extends WorldObject2D implements
+public abstract class WorldObject2DVisibleEverywhere extends WorldObject2D implements
 	VisibleEverywhere {
 
     public WorldObject2DVisibleEverywhere(TR tr) {
@@ -14,4 +14,4 @@ public class WorldObject2DVisibleEverywhere extends WorldObject2D implements
 	super(tr,model);
     }
     
-}
+}//end WorldOBject2DVisibleEverywhere

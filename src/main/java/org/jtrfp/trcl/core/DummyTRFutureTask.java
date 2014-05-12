dfef077662998e996891c0d930fb97e615b881d4
Chuck Ritola <cjritola@gmail.com>
2014-05-12 03:19:05 -0400

    Added dummy CALLABLE to DummyTRFutureTask to avoid NullPointerException.

--- src/main/java/org/jtrfp/trcl/core/DummyTRFutureTask.java
+++ src/main/java/org/jtrfp/trcl/core/DummyTRFutureTask.java
@@ -1,15 +1,28 @@
 package org.jtrfp.trcl.core;
 
+import java.util.concurrent.Callable;
+
 
 public class DummyTRFutureTask<T> extends TRFutureTask<T> {
     private final T valueToReturn;
+    private static final Callable CALLABLE = new Callable(){
+	@Override
+	public Object call() throws Exception {
+	    return null;
+	}
+    };
     public DummyTRFutureTask(T valueToReturn) {
-	super(null,null);
+	super(null,CALLABLE);
 	this.valueToReturn=valueToReturn;
     }
 
     @Override
     public T get(){
 	return valueToReturn;
+    }
+    
+    @Override
+    public boolean isDone(){
+	return true;
     }
 }//end DummyTRFutureTask

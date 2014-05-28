9c755bb08672c848e76a87a8ef42f5ab2e793a06
Chuck Ritola <cjritola@gmail.com>
2014-05-28 18:50:24 -0400

    Migrated start code to SubTextureWindow as a 6-element lookup table.

--- src/main/java/org/jtrfp/trcl/core/SubTextureWindow.java
+++ src/main/java/org/jtrfp/trcl/core/SubTextureWindow.java
@@ -6,7 +6,11 @@ public class SubTextureWindow extends MemoryWindow {
     public static final int SIDE_LENGTH_CODES = 38;
     //38 tiles squared = 1444
     public final ByteArrayVariable codeIDs = new ByteArrayVariable(1444);
-    public final ByteArrayVariable unused = new ByteArrayVariable(92);// 19
+    public final ByteArrayVariable unusedCID = new ByteArrayVariable(12);
+    //91VEC4
+    public final IntArrayVariable codeStartOffsetTable = new IntArrayVariable(6);// 6x4B = 24B.
+    //1480
+    public final ByteArrayVariable unused = new ByteArrayVariable(56);
     public void setTile(int objectID, int x, int y, byte tileID){
 	codeIDs.setAt(objectID,x+y*SIDE_LENGTH_CODES,tileID);
     }//end MemoryWindow

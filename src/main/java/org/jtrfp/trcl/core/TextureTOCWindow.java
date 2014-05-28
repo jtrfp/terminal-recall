6bd04af753608e7e5889d6275b54630d3c8cd93c
Chuck Ritola <cjritola@gmail.com>
2014-05-28 18:49:43 -0400

    Removed startTile from TextureTOCWindow.

--- src/main/java/org/jtrfp/trcl/core/TextureTOCWindow.java
+++ src/main/java/org/jtrfp/trcl/core/TextureTOCWindow.java
@@ -10,9 +10,8 @@ public class TextureTOCWindow extends MemoryWindow {
     //Offset 1456B, 91VEC4
     public final IntVariable width = new IntVariable();//4B
     public final IntVariable height = new IntVariable();//4B
-    public final IntVariable startTile = new IntVariable();//4B
-    //Tally: 1468B
-    public final ByteArrayVariable unused = new ByteArrayVariable(68);//68B
+    //Tally: 1464B
+    public final ByteArrayVariable unused = new ByteArrayVariable(72);//72B
     
     public TextureTOCWindow(TR tr){
 	this.init(tr, "TextureTOCWindow");

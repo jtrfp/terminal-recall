9ee4a6d57c7bd1cea1a3d65683979b8a0c090e8f
Chuck Ritola <cjritola@gmail.com>
2014-06-11 13:16:30 -0400

    Initial creation of GLFutureTask.

---
+++ src/main/java/org/jtrfp/trcl/core/GLFutureTask.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL 
+ * Copyright (c) 2012-2014 Chuck Ritola.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the COPYING and CREDITS files for more details.
+ * 
+ * Contributors:
+ *      chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.core;
+
+import java.util.concurrent.Callable;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLRunnable;
+
+public class GLFutureTask<V> extends TRFutureTask<V> implements GLRunnable {
+
+    public GLFutureTask(TR tr, Callable<V> callable) {
+	super(tr, callable);
+    }
+    @Override
+    public void run(){
+	super.run();}
+    
+    public void enqueue(){
+	tr.getRootWindow().getCanvas().invoke(false, this);
+    }
+
+    @Override
+    public boolean run(GLAutoDrawable drawable) {
+	super.run();
+	return true;
+    }//end run(GL)
+
+}//end GLFutureTask

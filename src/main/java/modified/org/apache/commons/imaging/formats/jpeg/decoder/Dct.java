211224ff47d9b9c2b52c1b1236e9c7346ab835c3
Chuck Ritola <cjritola@gmail.com>
2014-06-15 22:49:49 -0400

    Updated copyrights.

--- src/main/java/modified/org/apache/commons/imaging/formats/jpeg/decoder/Dct.java
+++ src/main/java/modified/org/apache/commons/imaging/formats/jpeg/decoder/Dct.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2014 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
 /*
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- src/main/java/org/jtrfp/trcl/gpu/GLTextureBuffer.java
+++ src/main/java/org/jtrfp/trcl/gpu/GLTextureBuffer.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
- * This file is part of TERMINAL RECALL 
- * Copyright (c) 2012, 2013 Chuck Ritola.
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2014 Chuck Ritola
+ * Part of the jTRFP.org project
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the COPYING and CREDITS files for more details.
  * 
  * Contributors:
- *      chuck - initial API and implementation
+ *     chuck - initial API and implementation
  ******************************************************************************/
 package org.jtrfp.trcl.gpu;
 

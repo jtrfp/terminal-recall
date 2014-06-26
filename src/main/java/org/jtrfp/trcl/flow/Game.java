46a86639e9b778a2c5d3ff8e178c2e640c71014e
Chuck Ritola <cjritola@gmail.com>
2014-06-25 22:00:32 -0400

    Stylistic cleanup.

--- src/main/java/org/jtrfp/trcl/flow/Game.java
+++ src/main/java/org/jtrfp/trcl/flow/Game.java
@@ -22,171 +22,165 @@ import org.jtrfp.trcl.core.TR;
 import org.jtrfp.trcl.file.VOXFile;
 import org.jtrfp.trcl.file.VOXFile.MissionLevel;
 
-public class Game
-	{
-	private TR tr;
-	private VOXFile vox;
-	private int levelIndex=0;
-	private String playerName;
-	private Difficulty difficulty;
-	private Mission currentMission;
-	
-	public Game(TR tr, VOXFile vox)
-		{
-		setTr(tr);
-		setVox(vox);
-		if(!tr.getTrConfig().isDebugMode())setupNameWithUser();
-		}//end constructor
-	
-	private void setupNameWithUser()
-		{
-		setPlayerName((String)JOptionPane.showInputDialog(tr.getRootWindow(),
-				"Callsign:","Pilot Registration",JOptionPane.PLAIN_MESSAGE,
-				null,null,"Councilor"));
-		String difficulty=(String)JOptionPane.showInputDialog(tr.getRootWindow(),
-				"Difficulty:","Pilot Registration",JOptionPane.PLAIN_MESSAGE,
-				null,new String[]{"Easy","Normal","Hard","Furious"},"Normal");
-		if(difficulty.contentEquals("Easy"))
-			{setDifficulty(Difficulty.EASY);}
-		if(difficulty.contentEquals("Normal"))
-			{setDifficulty(Difficulty.NORMAL);}
-		if(difficulty.contentEquals("Hard"))
-			{setDifficulty(Difficulty.HARD);}
-		if(difficulty.contentEquals("Furious"))
-			{setDifficulty(Difficulty.FURIOUS);}
-		}//end setupNameWithUser()
-	
-	public void save(File fileToSaveTo)
-		{
-		//TODO
-		}
+public class Game {
+    private TR tr;
+    private VOXFile vox;
+    private int levelIndex = 0;
+    private String playerName;
+    private Difficulty difficulty;
+    private Mission currentMission;
 
-	/**
-	 * @return the tr
-	 */
-	public TR getTr()
-		{
-		return tr;
-		}
+    public Game(TR tr, VOXFile vox) {
+	setTr(tr);
+	setVox(vox);
+	if (!tr.getTrConfig().isDebugMode())
+	    setupNameWithUser();
+    }// end constructor
 
-	/**
-	 * @param tr the tr to set
-	 */
-	public void setTr(TR tr)
-		{
-		this.tr = tr;
-		}
-
-	/**
-	 * @return the vox
-	 */
-	public VOXFile getVox()
-		{
-		return vox;
-		}
-
-	/**
-	 * @param vox the vox to set
-	 */
-	public void setVox(VOXFile vox)
-		{
-		this.vox = vox;
-		}
-
-	/**
-	 * @return the levelIndex
-	 */
-	public int getLevelIndex()
-		{
-		return levelIndex;
-		}
-
-	/**
-	 * @param levelIndex the levelIndex to set
-	 */
-	public void setLevelIndex(int levelIndex)
-		{
-		this.levelIndex = levelIndex;
-		}
-
-	/**
-	 * @return the playerName
-	 */
-	public String getPlayerName()
-		{
-		return playerName;
-		}
-
-	/**
-	 * @param playerName the playerName to set
-	 */
-	public void setPlayerName(String playerName)
-		{
-		this.playerName = playerName;
-		}
-	
-	enum Difficulty
-		{
-		EASY,
-		NORMAL,
-		HARD,
-		FURIOUS
-		}
-
-	/**
-	 * @return the difficulty
-	 */
-	public Difficulty getDifficulty()
-		{
-		return difficulty;
-		}
-
-	/**
-	 * @param difficulty the difficulty to set
-	 */
-	public void setDifficulty(Difficulty difficulty)
-		{
-		this.difficulty = difficulty;
-		}
-	
-	    private void startMissionSequence(String lvlFileName) {
-		recursiveMissionSequence(lvlFileName);
-	    }
-
-	    private void recursiveMissionSequence(String lvlFileName) {
-		try {
-		    currentMission = new Mission(tr, this, tr.getResourceManager().getLVL(
-			    lvlFileName));
-		    Mission.Result result = currentMission.go();
-		    final String nextLVL = result.getNextLVL();
-		    if (nextLVL != null)
-			recursiveMissionSequence(nextLVL);
-		} catch (IllegalAccessException e) {
-		    tr.showStopper(e);
-		} catch (FileLoadException e) {
-		    tr.showStopper(e);
-		} catch (IOException e) {
-		    tr.showStopper(e);
-		}
-	    }//end recursiveMissionSequence(...)
-
-	public void setLevel(String skipToLevel) {
-	    final MissionLevel [] levs = vox.getLevels();
-	    for(int index=0; index<levs.length; index++){
-		if(levs[index].getLvlFile().toUpperCase().contentEquals(skipToLevel.toUpperCase()))
-		    setLevelIndex(index);
-	    }//end for(levs)
-	}//end setLevel()
-
-	public void go() {
-	    //Set up player, HUD, fonts...
-	    System.out.println("Game.go()...");
-	    System.out.println("Initializing general resources...");
-	    //TODO: Player, fonts
-	    startMissionSequence(vox.getLevels()[getLevelIndex()].getLvlFile());
-	}//end go()
-
-	public Mission getCurrentMission() {
-	    return currentMission;
+    private void setupNameWithUser() {
+	setPlayerName((String) JOptionPane.showInputDialog(tr.getRootWindow(),
+		"Callsign:", "Pilot Registration", JOptionPane.PLAIN_MESSAGE,
+		null, null, "Councilor"));
+	String difficulty = (String) JOptionPane.showInputDialog(
+		tr.getRootWindow(), "Difficulty:", "Pilot Registration",
+		JOptionPane.PLAIN_MESSAGE, null, new String[] { "Easy",
+			"Normal", "Hard", "Furious" }, "Normal");
+	if (difficulty.contentEquals("Easy")) {
+	    setDifficulty(Difficulty.EASY);
 	}
-	}//end Game
+	if (difficulty.contentEquals("Normal")) {
+	    setDifficulty(Difficulty.NORMAL);
+	}
+	if (difficulty.contentEquals("Hard")) {
+	    setDifficulty(Difficulty.HARD);
+	}
+	if (difficulty.contentEquals("Furious")) {
+	    setDifficulty(Difficulty.FURIOUS);
+	}
+    }// end setupNameWithUser()
+
+    public void save(File fileToSaveTo) {
+	// TODO
+    }
+
+    /**
+     * @return the tr
+     */
+    public TR getTr() {
+	return tr;
+    }
+
+    /**
+     * @param tr
+     *            the tr to set
+     */
+    public void setTr(TR tr) {
+	this.tr = tr;
+    }
+
+    /**
+     * @return the vox
+     */
+    public VOXFile getVox() {
+	return vox;
+    }
+
+    /**
+     * @param vox
+     *            the vox to set
+     */
+    public void setVox(VOXFile vox) {
+	this.vox = vox;
+    }
+
+    /**
+     * @return the levelIndex
+     */
+    public int getLevelIndex() {
+	return levelIndex;
+    }
+
+    /**
+     * @param levelIndex
+     *            the levelIndex to set
+     */
+    public void setLevelIndex(int levelIndex) {
+	this.levelIndex = levelIndex;
+    }
+
+    /**
+     * @return the playerName
+     */
+    public String getPlayerName() {
+	return playerName;
+    }
+
+    /**
+     * @param playerName
+     *            the playerName to set
+     */
+    public void setPlayerName(String playerName) {
+	this.playerName = playerName;
+    }
+
+    enum Difficulty {
+	EASY, NORMAL, HARD, FURIOUS
+    }
+
+    /**
+     * @return the difficulty
+     */
+    public Difficulty getDifficulty() {
+	return difficulty;
+    }
+
+    /**
+     * @param difficulty
+     *            the difficulty to set
+     */
+    public void setDifficulty(Difficulty difficulty) {
+	this.difficulty = difficulty;
+    }
+
+    private void startMissionSequence(String lvlFileName) {
+	recursiveMissionSequence(lvlFileName);
+    }
+
+    private void recursiveMissionSequence(String lvlFileName) {
+	try {
+	    currentMission = new Mission(tr, this, tr.getResourceManager()
+		    .getLVL(lvlFileName));
+	    Mission.Result result = currentMission.go();
+	    final String nextLVL = result.getNextLVL();
+	    if (nextLVL != null)
+		recursiveMissionSequence(nextLVL);
+	} catch (IllegalAccessException e) {
+	    tr.showStopper(e);
+	} catch (FileLoadException e) {
+	    tr.showStopper(e);
+	} catch (IOException e) {
+	    tr.showStopper(e);
+	}
+    }// end recursiveMissionSequence(...)
+
+    public void setLevel(String skipToLevel) {
+	final MissionLevel[] levs = vox.getLevels();
+	for (int index = 0; index < levs.length; index++) {
+	    if (levs[index].getLvlFile().toUpperCase()
+		    .contentEquals(skipToLevel.toUpperCase()))
+		setLevelIndex(index);
+	}// end for(levs)
+    }// end setLevel()
+
+    public void go() {
+	// Set up player, HUD, fonts...
+	System.out.println("Game.go()...");
+	System.out.println("Initializing general resources...");
+	// TODO: Player, fonts
+	startMissionSequence(vox.getLevels()[getLevelIndex()].getLvlFile());
+    }// end go()
+
+    public Mission getCurrentMission() {
+	return currentMission;
+    }
+}// end Game

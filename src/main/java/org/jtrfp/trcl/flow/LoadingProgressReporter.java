16837d4dcfa50d7482c63ce585fb4aff017b973b
Chuck Ritola <cjritola@gmail.com>
2014-07-03 22:24:20 -0400

    Initial creation of LoadingProgressReporter.

---
+++ src/main/java/org/jtrfp/trcl/flow/LoadingProgressReporter.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2014 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.flow;
+
+public interface LoadingProgressReporter {
+    public LoadingProgressReporter [] 	generateSubReporters(int numSubReporters);
+    public void 			complete();
+    
+    public static class Impl implements StemReporter{
+	private final		StemReporter parent;
+	private int 		totalSubReporters=1;
+	private int 		reportCount	 =0;
+	private boolean 	complete	 =false;
+	private UpdateHandler 	updateHandler;
+	
+	public static Impl createRoot(){
+	    return new Impl(null);
+	}
+	
+	Impl(StemReporter parent){
+	    this.parent=parent;
+	}//end Impl()
+	
+	@Override
+	public LoadingProgressReporter[] generateSubReporters(
+		final int numSubReporters) {
+	    LoadingProgressReporter [] result = new LoadingProgressReporter[numSubReporters];
+	    for(int i=0; i<numSubReporters; i++){
+		result[i]=new Impl(this);
+	    }//end for(reporters)
+	    totalSubReporters+=numSubReporters;
+	    parent.addSubReporters(numSubReporters);
+	    return result;
+	}//end constructor
+
+	@Override
+	public void complete() {
+	    if(!complete){
+		complete=true;
+		final int remainingReportCounts = totalSubReporters - reportCount;
+		for(int i=0; i<remainingReportCounts; i++)
+		    increment();
+	    }//end if(!complete)
+	}//end complete()
+
+	@Override
+	public void increment() {
+	    if(reportCount<totalSubReporters){
+		reportCount++;
+	    	if(parent!=null)	parent.increment();
+	    	if(updateHandler!=null)	updateHandler.update((double)reportCount/(double)totalSubReporters);
+	    }//end if(in range)
+	}//end increment()
+
+	@Override
+	public void addSubReporters(int delta) {
+	    totalSubReporters+=delta;
+	}//end addSubReporteres(...)
+    }//end Impl
+    
+    public static interface UpdateHandler{
+	public void update(double unitProgress);
+    }//end UpdateHandler
+    
+    static interface StemReporter extends LoadingProgressReporter{
+	public void increment();
+	public void addSubReporters(int delta);
+    }//end StemReporter
+}//end LoadingProgrsesReporter

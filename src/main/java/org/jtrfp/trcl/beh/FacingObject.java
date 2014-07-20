4cb50ef4e10a79a405cb64c652dd96c4b59af10e
Chuck Ritola <cjritola@gmail.com>
2014-07-20 06:35:37 -0400

    Kludge: FacingObject uses "UP" for top instead of pitching to target.

--- src/main/java/org/jtrfp/trcl/beh/FacingObject.java
+++ src/main/java/org/jtrfp/trcl/beh/FacingObject.java
@@ -32,7 +32,7 @@ public class FacingObject extends Behavior {
 	    parent.setHeading(new Vector3D(Vect3D.normalize(work,work)));
 	    Vect3D.cross(work, UP, perp);
 	    Vect3D.cross(perp, work, perp);
-	    parent.setTop(new Vector3D(perp));
+	    parent.setTop(new Vector3D(UP));
 	}//end if(!null)
     }//end _tick(...)
     /**

e5dcdf542bb77f005603e8dec91be46bfc479d5b
Chuck Ritola <cjritola@gmail.com>
2014-08-05 05:43:34 -0400

    Added stale page flushing capability to MemoryManager.

--- src/main/java/org/jtrfp/trcl/mem/MemoryManager.java
+++ src/main/java/org/jtrfp/trcl/mem/MemoryManager.java
@@ -13,7 +13,10 @@
 package org.jtrfp.trcl.mem;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.Callable;
 
 import org.jtrfp.trcl.core.TRFuture;
@@ -30,6 +33,8 @@ public final class MemoryManager {
     private final ByteBuffer [] 		physicalMemory 	= new ByteBuffer[1];
     private final ReallocatableGLTextureBuffer 	glPhysicalMemory;
     private final GPU				gpu;
+    private final ArrayList<WeakReference<PagedByteBuffer>>	
+    						pagedByteBuffers = new ArrayList<WeakReference<PagedByteBuffer>>(1024);
     
     public MemoryManager(GPU gpu){
 	this.gpu=gpu;
@@ -69,13 +74,43 @@ public final class MemoryManager {
     public void map(){
 	if((physicalMemory[0] = glPhysicalMemory.map())==null)throw new NullPointerException("Failed to map GPU memory. (returned null)");
     }
+    public void flushRange(int startPositionInBytes, int lengthInBytes){
+	glPhysicalMemory.flushRange(startPositionInBytes, lengthInBytes);
+    }
     public void unmap(){
 	physicalMemory[0]=null;//Make sure we don't start reading/writing somewhere bad.
 	glPhysicalMemory.unmap();
 	}
     public PagedByteBuffer createPagedByteBuffer(int initialSizeInBytes, String debugName){
-	return new PagedByteBuffer(physicalMemory, pageIndexPool, initialSizeInBytes, debugName);
+	return new PagedByteBuffer(gpu, physicalMemory, pageIndexPool, initialSizeInBytes, debugName);
     }
+    
+    void registerPagedByteBuffer(WeakReference<PagedByteBuffer> b){
+	synchronized(pagedByteBuffers){
+	pagedByteBuffers.add(b);}
+    }
+    
+    void deRegisterPagedByteBuffer(WeakReference<PagedByteBuffer> b){
+	synchronized(pagedByteBuffers){
+	pagedByteBuffers.remove(b);}
+    }
+    
+    public void flushStalePages(){
+	if(!glPhysicalMemory.isMapped())
+	    return;
+	synchronized(pagedByteBuffers){
+	final Iterator<WeakReference<PagedByteBuffer>> it = pagedByteBuffers.iterator();
+	while(it.hasNext()){
+	    final WeakReference<PagedByteBuffer> r = it.next();
+	    if(r.get()==null)
+		it.remove();
+	    else{
+		r.get().flushStalePages();
+	    }//end else{}
+	}//end while(hasNext)
+	}//end sync()
+    }//end flushStalePages()
+    
     public void bindToUniform(int textureUnit, GLProgram shaderProgram, GLUniform uniform) {
 	glPhysicalMemory.bindToUniform(textureUnit, shaderProgram, uniform);
     }

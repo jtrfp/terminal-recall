a20530881ee62571911092c9c19e38d708f77f53
Chuck Ritola <cjritola@gmail.com>
2014-11-03 07:29:14 -0500

    Initial creation of SoundEvent.

---
+++ src/main/java/org/jtrfp/trcl/snd/SoundEvent.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL 
+ * Copyright (c) 2012-2014 Chuck Ritola.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the COPYING and CREDITS files for more details.
+ * 
+ * Contributors:
+ *      chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.snd;
+
+import java.util.Collection;
+
+import javax.media.opengl.GL3;
+
+import org.jtrfp.trcl.core.TR;
+
+public interface SoundEvent {
+    public long getStartRealtimeSamples();
+    public long getEndRealtimeSamples();
+    public long getDurationRealtimeSamples();
+    public void apply(GL3 gl, long bufferStartTimeFrames);
+    public Factory getOrigin();
+    public boolean isActive();
+    public void activate();
+    public void deactivate();
+    public SoundEvent getParent();
+    public void destroy();
+    public boolean isDestroyed();
+    
+    public interface Factory{
+	public void apply(GL3 gl, Collection<SoundEvent> events, long bufferStartTimeFrames);
+	public TR getTR();
+    }//end Factory
+}//end PlaybackEvent

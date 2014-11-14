9fa64134ef4cb900c1c7a427520d7334b8cbc1c0
Chuck Ritola <cjritola@gmail.com>
2014-11-14 13:10:45 -0500

    Cleanup.

--- src/main/java/org/jtrfp/trcl/obj/ExplosionSystem.java
+++ src/main/java/org/jtrfp/trcl/obj/ExplosionSystem.java
@@ -19,9 +19,8 @@ import org.jtrfp.trcl.obj.Explosion.ExplosionType;
 import org.jtrfp.trcl.obj.Smoke.SmokeType;
 
 public class ExplosionSystem extends RenderableSpacePartitioningGrid {
-    	//private int billowIndex=0,blastIndex=0,bigExplosionIndex=0;
-    	private final TR tr;
-    	private final int MAX_EXPLOSIONS_PER_POOL=20;
+    	private final TR     tr;
+    	private final int    MAX_EXPLOSIONS_PER_POOL=20;
     	private final Explosion[][] allExplosions = new Explosion[ExplosionType.values().length][];
     	private final int [] indices = new int[ExplosionType.values().length];
 	public ExplosionSystem(TR tr){
@@ -30,9 +29,8 @@ public class ExplosionSystem extends RenderableSpacePartitioningGrid {
 	    int i;
 	    for(ExplosionType t:ExplosionType.values()){
 		allExplosions[t.ordinal()]=new Explosion[MAX_EXPLOSIONS_PER_POOL];
-		for(i=0; i<MAX_EXPLOSIONS_PER_POOL; i++){
+		for(i=0; i<MAX_EXPLOSIONS_PER_POOL; i++)
 			allExplosions[t.ordinal()][i]=new Explosion(tr,t);
-		    }
 	    }//end for(explosionTypes)
 	}//end constructor()
 	
@@ -42,9 +40,7 @@ public class ExplosionSystem extends RenderableSpacePartitioningGrid {
 	    result.destroy();
 	    result.resetExplosion();
 	    final double [] pos = result.getPosition();
-	    pos[0]=position[0];
-	    pos[1]=position[1];
-	    pos[2]=position[2];
+	    System.arraycopy(position, 0, pos, 0, 3);
 	    result.notifyPositionChange();
 	    final SmokeSystem sf = tr.getResourceManager().getSmokeSystem();
 	    final int NUM_PUFFS=1;
@@ -62,6 +58,5 @@ public class ExplosionSystem extends RenderableSpacePartitioningGrid {
 	    }//end for(i)
 	    add(result);
 	    return result;
-	    
-	}
+	}//end TriggerExplosion
 }//end ExplosionFactory

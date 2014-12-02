490fe83d94d377de3cab87c3a4ddc6dc1eed2725
Chuck Ritola <cjritola@gmail.com>
2014-12-02 05:23:00 -0500

    Initial creation of EngineTests.java.

---
+++ src/main/java/org/jtrfp/trcl/flow/EngineTests.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2014 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+package org.jtrfp.trcl.flow;
+
+import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.core.TextureDescription;
+import org.jtrfp.trcl.obj.Sprite2D;
+import org.jtrfp.trcl.obj.WorldObject;
+
+public class EngineTests {
+
+    public static void singlet(TR tr) {
+	final TextureDescription test = tr.getResourceManager().getTestTexture();
+	tr.abortCurrentGame();
+	tr.getWorld().removeAll();
+	WorldObject wo = new Sprite2D(tr, .5, 1, 1, test, false);
+	wo.setPosition(new double[]{0,0,.5});
+	wo.setActive(true);
+	wo.setVisible(true);
+	tr.getWorld().add(wo);
+    }//end singlet(tr)
+
+}//end EngineTests

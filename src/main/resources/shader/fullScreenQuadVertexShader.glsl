830db22cdab0cf2b39a675c464c58f86c0c9709e
Chuck Ritola <cjritola@gmail.com>
2014-12-13 18:33:10 -0500

    Refactor fullscreen quad to be more resistant to backface culling.

--- src/main/resources/shader/fullScreenQuadVertexShader.glsl
+++ src/main/resources/shader/fullScreenQuadVertexShader.glsl
@@ -31,8 +31,8 @@ vec2 pos[6] = vec2[]
 	vec2(1,-1),
 	vec2(-1,1),
 	vec2(-1,1),
-	vec2(1,1),
-	vec2(1,-1)
+	vec2(1,-1),
+	vec2(1,1)
 	);
 vec2 screenLocation[6] = vec2[]
 	(
@@ -40,8 +40,8 @@ vec2 screenLocation[6] = vec2[]
 	vec2(1,0),
 	vec2(0,1),
 	vec2(0,1),
-	vec2(1,1),
-	vec2(1,0)
+	vec2(1,0),
+	vec2(1,1)
 	);
 
 void main(){

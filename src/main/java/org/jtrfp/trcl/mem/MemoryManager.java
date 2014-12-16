d376a9cde83f9471f5387824dc181699a98c2b05
Chuck Ritola <cjritola@gmail.com>
2014-12-16 10:25:45 -0500

    Apply GL binding state resets in MemoryManager. Affects #56.

--- src/main/java/org/jtrfp/trcl/mem/MemoryManager.java
+++ src/main/java/org/jtrfp/trcl/mem/MemoryManager.java
@@ -103,6 +103,8 @@ public final class MemoryManager {
     }
     
     public void flushStalePages(){
+	flushRange(0, physicalMemory[0].capacity());//TODO DEBUG
+	if(1+1==2)return;
 	if(!glPhysicalMemory.isMapped())
 	    return;
 	synchronized(pagedByteBuffers){

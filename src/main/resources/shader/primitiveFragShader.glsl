0392296c6d0abd240a48d1dbabc530cd46922d87
Chuck Ritola <cjritola@gmail.com>
2014-12-30 23:37:55 -0500

    Add support for oversampling with new Primitive-based deferred stage. Affects #56.

--- src/main/resources/shader/primitiveFragShader.glsl
+++ src/main/resources/shader/primitiveFragShader.glsl
@@ -26,9 +26,27 @@ flat in mat4 nXnYnZQuad;
 layout(location = 0) out vec4	uvzwBuffer;
 layout(location = 1) out vec4	nXnYnZBuffer;
 
+const float OVERSAMPLING         = 4;
+
 void main(){
- uint quadrant = uint(gl_FragCoord.x)%2u+(uint(gl_FragCoord.y)%2u)*2u;
- uvzwBuffer = uvzwQuad[quadrant];
- nXnYnZBuffer = nXnYnZQuad[quadrant];
+ // +1 to invert the row order for the upside-down GL textures.
+ //uint quadrant = uint(gl_FragCoord.x)%2u+(uint(gl_FragCoord.y)%2u)*2u;
+ vec2 coord = (mod(floor(gl_FragCoord.xy),2*OVERSAMPLING))/(OVERSAMPLING);
+ 
+ // Appears both Intel and AMD aren't getting this var right.
+ // https://software.intel.com/en-us/forums/topic/286351
+ // https://www.opengl.org/discussion_boards/showthread.php/166384-gl_PointCoord-is-giving-me-junk-values!
+ // The AMD bug is quite old and supposedly fixed but I seem to be getting it in fglrx 14.501.1003.
+ // TODO: Set up a confirm case to verify that it isn't just a bug in this program.
+ //vec2 coord = gl_PointCoord.st;
+ 
+ uvzwBuffer = mix(
+ 				mix(uvzwQuad[0u],uvzwQuad[1u],coord.x),
+ 				mix(uvzwQuad[2u],uvzwQuad[3u],coord.x),
+ 				coord.y);
+ nXnYnZBuffer = mix(
+ 				mix(nXnYnZQuad[0u],nXnYnZQuad[1u],coord.x),
+ 				mix(nXnYnZQuad[2u],nXnYnZQuad[3u],coord.x),
+ 				coord.y);
  }
  

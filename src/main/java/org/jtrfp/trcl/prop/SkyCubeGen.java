484218f36e01dbb828c8bfe188a781c8fd261296
Chuck Ritola <cjritola@gmail.com>
2015-01-03 03:04:05 -0500

    Initial creation of SkyCubeGen interface.

---
+++ src/main/java/org/jtrfp/trcl/prop/SkyCubeGen.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL 
+ * Copyright (c) 2012-2015 Chuck Ritola and contributors.
+ * See Github project's commit log for contribution details.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the COPYING and CREDITS files for more details.
+ * 
+ ******************************************************************************/
+
+package org.jtrfp.trcl.prop;
+
+import java.nio.ByteBuffer;
+
+public interface SkyCubeGen {
+    public ByteBuffer getTop();
+    public ByteBuffer getBottom();
+    public ByteBuffer getNorth();
+    public ByteBuffer getSouth();
+    public ByteBuffer getWest();
+    public ByteBuffer getEast();
+}

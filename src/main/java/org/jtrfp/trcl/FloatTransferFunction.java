09da4bc05a61ed8fb4bb9f65fc3a377a1d49af16
Chuck Ritola <cjritola@gmail.com>
2015-03-01 00:48:13 -0500

    Initial creation of Byte/Float/Short transfer function.

---
+++ src/main/java/org/jtrfp/trcl/FloatTransferFunction.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2015 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+package org.jtrfp.trcl;
+
+public interface FloatTransferFunction {
+    public float transfer(int input);
+    
+    public static final class Zero implements FloatTransferFunction{
+	@Override
+	public float transfer(int input) {
+	    return 0;
+	}}
+}
---
+++ src/main/java/org/jtrfp/trcl/ShortTransferFunction.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2015 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+package org.jtrfp.trcl;
+
+public interface ShortTransferFunction {
+    public short transfer(int input);
+    
+    public static final class Zero implements ShortTransferFunction{
+	@Override
+	public short transfer(int input) {
+	    return 0;
+	}}
+}

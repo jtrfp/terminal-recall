ee5070b89d2b4d75d413510c13cf3ee443925282
Chuck Ritola <cjritola@gmail.com>
2015-03-27 16:19:00 -0400

    Add peek() and javadocs to ObjectFactory.

--- src/main/java/org/jtrfp/trcl/pool/ObjectFactory.java
+++ src/main/java/org/jtrfp/trcl/pool/ObjectFactory.java
@@ -19,6 +19,7 @@ import com.ochafik.util.Adapter;
 
 /**
  * Not related to the JNDI, but likely to be replaced by it in the future.
+ * Creates and caches objects of type V from keys of type K.
  * @author Chuck Ritola
  *
  * @param <K>
@@ -33,10 +34,35 @@ public class ObjectFactory<K,V> {
 	this.keyValueAdapter = keyValueAdapter;
     }
     
+    /**
+     * Obtain an instance of V based on the key K, creating one if its key isn't present in the currently-used map.
+     * @param key
+     * @return Instance of V created with the supplied adapter from K
+     * @since Mar 27, 2015
+     */
     public V get(K key){
 	V result = mapToUse.get(key);
 	if(result==null)
 	    mapToUse.put(key, result=keyValueAdapter.adapt(key));
 	return result;
-    }//end public(key)
+    }//end get(key)
+    
+    /**
+     * Query the currently-used map for already-created object matching the given key.
+     * @param key
+     * @return Teh pre-created object, if any, or null if none exists.
+     * @since Mar 27, 2015
+     */
+    public V peek(K key){
+	return mapToUse.get(key);
+    }//end peek(key)
+    
+    /**
+     * Query the map being used to back this ObjectFactory's caching of already-created objects.
+     * @return
+     * @since Mar 27, 2015
+     */
+    public Map<K,V> getMap(){
+	return mapToUse;
+    }
 }//end CachedObjectFactory

9b43b0b38fc3caa9d27dda3b942e60ca4fb952de
Chuck Ritola <cjritola@gmail.com>
2015-04-07 08:34:08 -0400

    Redesign: Improved sublists in ListActionDispatcher.

--- src/main/java/org/jtrfp/trcl/coll/ListActionDispatcher.java
+++ src/main/java/org/jtrfp/trcl/coll/ListActionDispatcher.java
@@ -21,19 +21,47 @@ import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.collections4.iterators.ArrayListIterator;
+
 public class ListActionDispatcher<E> implements List<E> {
     protected final List<E>             cache;
-    protected final Map<List<E>,Object> targetsMap = new IdentityHashMap<List<E>,Object>();
-    protected final Set<List<E>>        targets = targetsMap.keySet();
+    protected final Map<List<E>,Object> targetsMap;
+    protected final Set<List<E>>        targets;
+    private final int                   startIndex, endIndex;
+    
+    protected ListActionDispatcher(List<E> cache, Map<List<E>, Object> targetsMap, int startIndex, int endIndex){
+	this.cache=cache;
+	this.targetsMap=targetsMap;
+	this.targets=targetsMap.keySet();
+	this.startIndex = startIndex;
+	this.endIndex = endIndex;
+    }
     
     public ListActionDispatcher(List<E> cache){
+	this(cache,new IdentityHashMap<List<E>,Object>(), 0, Integer.MAX_VALUE);
 	if(cache==null)
 	    throw new NullPointerException("Supplied cache is intolerably null.");
-	this.cache=cache;
     }//end constructor
     
     public ListActionDispatcher() {
 	this(new ArrayList<E>());
+    }
+    
+    private boolean isRoot(){
+	return startIndex==0 && endIndex==Integer.MAX_VALUE;
+    }
+    
+    private List<E> getCache(){
+	if(isRoot())
+	    return cache;
+	return getCache();
+    }//end getCache()
+    
+    private List<E> getSubTarget(List<E> target){
+	if(isRoot())
+	    return target;
+	return target.subList(startIndex, endIndex);
     }
 
     /**
@@ -60,107 +88,124 @@ public class ListActionDispatcher<E> implements List<E> {
     
     @Override
     public boolean add(E e) {
+	final boolean result = cache.add(e);
 	for(List<E> targ:targets)
 	    targ.add(e);
-	return cache.add(e);
+	return result;
     }
     @Override
     public void add(int index, E element) {
+	index+=startIndex;
+	cache.add(index,element);
 	for(List<E> targ:targets)
 	    targ.add(index,element);
-	cache.add(index,element);
     }
     @Override
     public boolean addAll(Collection<? extends E> c) {
-	for(List<E> targ:targets)
-	    targ.addAll(c);
-	return cache.addAll(c);
+	return addAll(startIndex, c);
     }
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
+	index += startIndex;
+	final boolean result = cache.addAll(index,c);
 	for(List<E> targ:targets)
 	    targ.addAll(index,c);
-	return cache.addAll(index,c);
+	return result;
     }
     @Override
     public void clear() {
-	for(List<E> targ:targets)
-	    targ.clear();
-	cache.clear();
-    }
+	if(isRoot()){
+	    cache.clear();
+	    for(List<E> targ:targets)
+		targ.clear();
+	}//end if(root)
+	else{
+	    final int n = endIndex - startIndex;
+	    for(int i=0; i<n; i++)
+	     cache.remove(startIndex);
+	}//end if(!root)
+    }//end clear()
     @Override
     public boolean contains(Object o) {
-	return cache.contains(o);
+	return getCache().contains(o);
     }
     @Override
     public boolean containsAll(Collection<?> c) {
-	return cache.containsAll(c);
+	return getCache().containsAll(c);
     }
     @Override
     public E get(int index) {
+	index+=startIndex;
 	return cache.get(index);
     }
     @Override
     public int indexOf(Object o) {
-	return cache.indexOf(o);
+	return getCache().indexOf(o);
     }
     @Override
     public boolean isEmpty() {
-	return cache.isEmpty();
+	return getCache().isEmpty();
     }
     @Override
     public Iterator<E> iterator() {
-	return cache.iterator();
+	return getCache().iterator();
     }
     @Override
     public int lastIndexOf(Object o) {
-	return cache.lastIndexOf(o);
+	return getCache().lastIndexOf(o);
     }
     @Override
     public ListIterator<E> listIterator() {
-	return cache.listIterator();
+	return IteratorUtils.toListIterator(iterator());
     }
     @Override
     public ListIterator<E> listIterator(int index) {
-	return cache.listIterator(index);
+	return new ArrayListIterator<E>(index+startIndex);
     }
     @Override
     public boolean remove(Object o) {
+	final boolean result = getCache().remove(o);
 	for(List<E> targ:targets)
-	    targ.remove(o);
-	return cache.remove(o);
+	    getSubTarget(targ).remove(o);
+	return result;
     }
     @Override
     public E remove(int index) {
+	final E result = getCache().remove(index);
 	for(List<E> targ:targets)
-	    try{targ.remove(index);} catch(IndexOutOfBoundsException e){}//Ignore
-	return cache.remove(index);
+	    try{getSubTarget(targ).remove(index);} catch(IndexOutOfBoundsException e){}//Ignore
+	return result;
     }
     @Override
     public boolean removeAll(Collection<?> c) {
+	final boolean result = getCache().removeAll(c);
 	for(List<E> targ:targets)
-	    targ.removeAll(c);
-	return cache.removeAll(c);
+	    getSubTarget(targ).removeAll(c);
+	return result;
     }
     @Override
     public boolean retainAll(Collection<?> c) {
+	final boolean result = getCache().retainAll(c);
 	for(List<E> targ:targets)
-	    targ.retainAll(c);
-	return cache.retainAll(c);
+	    getSubTarget(targ).retainAll(c);
+	return result;
     }
     @Override
     public E set(int index, E element) {
+	index += startIndex;
+	final E result = cache.set(index, element);
 	for(List<E> targ:targets)
 	    targ.set(index,element);
-	return cache.set(index, element);
+	return result;
     }
     @Override
     public int size() {
-	return cache.size();
+	return getCache().size();
     }
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-	return cache.subList(fromIndex, toIndex);
+	return new ListActionDispatcher<E>(cache, targetsMap, startIndex+fromIndex, endIndex+toIndex);
+	//return cache.subList(fromIndex, toIndex);
     }
     @Override
     public Object[] toArray() {

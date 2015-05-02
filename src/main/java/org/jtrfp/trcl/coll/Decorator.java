f777ba3eac4f0845d833d11b7ab8c0fc731495e3
Chuck Ritola <cjritola@gmail.com>
2015-05-02 08:50:04 -0400

    Initial creation or Decorator interface.

---
+++ src/main/java/org/jtrfp/trcl/coll/Decorator.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2015 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+package org.jtrfp.trcl.coll;
+
+public interface Decorator<DELEGATE> {
+ public DELEGATE getDelegate();
+}

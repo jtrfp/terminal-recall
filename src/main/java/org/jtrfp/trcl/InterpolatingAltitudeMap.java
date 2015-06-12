d2443506fd611ed900ca7d22b08d9f373dc77d27
Chuck Ritola <cjritola@gmail.com>
2015-06-12 08:39:26 -0400

    Bugfix/Optimization: Negative sqrt results in NaN, new no-create method.

--- src/main/java/org/jtrfp/trcl/InterpolatingAltitudeMap.java
+++ src/main/java/org/jtrfp/trcl/InterpolatingAltitudeMap.java
@@ -13,6 +13,7 @@
 package org.jtrfp.trcl;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.math.Vect3D;
 
 public class InterpolatingAltitudeMap implements AltitudeMap {
     private AltitudeMap toWrap;
@@ -39,6 +40,8 @@ public class InterpolatingAltitudeMap implements AltitudeMap {
 	accumulator+=topRight*wTop*wRight;
 	accumulator+=bottomLeft*wBottom*wLeft;
 	accumulator+=bottomRight*wBottom*wRight;
+	assert accumulator != Double.NaN;
+	assert accumulator != Double.POSITIVE_INFINITY;
 	return accumulator;
     }
 
@@ -54,7 +57,24 @@ public class InterpolatingAltitudeMap implements AltitudeMap {
     public Vector3D normalAt(double x, double z){
 	final double magX=(heightAt(x-NUDGE,z)-heightAt(x+NUDGE,z))/NUDGE;
 	final double magZ=(heightAt(x,z-NUDGE)-heightAt(x,z+NUDGE))/NUDGE;
-	final double magY=Math.sqrt(1-(magX*magX+magZ*magZ));
+	final double magY=Math.sqrt(Math.max(1-(magX*magX+magZ*magZ),0));
 	return new Vector3D(magX,magY,magZ).normalize();
     }//end normalAt(...)
+    
+    /**
+     * Same as normalAt(x,z) except using triplet array with no object creation.
+     * @since Jun 12, 2015
+     */
+    public double [] normalAt(double x, double z, double [] dest){
+	final double magX=(heightAt(x-NUDGE,z)-heightAt(x+NUDGE,z))/NUDGE;
+	final double magZ=(heightAt(x,z-NUDGE)-heightAt(x,z+NUDGE))/NUDGE;
+	final double magY=Math.sqrt(Math.max(1-(magX*magX+magZ*magZ),0));
+	assert !Double.isNaN(magX);
+	assert !Double.isNaN(magY);
+	assert !Double.isNaN(magZ);
+	dest[0]=magX; dest[1]=magY; dest[2]=magZ;
+	assert dest.length==3:"Got "+dest.length;
+	assert !Vect3D.isAnyNaN(dest);
+	return Vect3D.normalize(dest, dest);
+    }
 }//end InterpolatingAltitudeMap

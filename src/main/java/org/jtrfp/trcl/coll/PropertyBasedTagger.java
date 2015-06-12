50e0fb77baeaed5705893fe478dceb4aa35df3bf
Chuck Ritola <cjritola@gmail.com>
2015-06-12 08:55:43 -0400

    Redesign: Adapter calculation based on PropertyChangeEvents.

--- src/main/java/org/jtrfp/trcl/coll/PropertyBasedTagger.java
+++ src/main/java/org/jtrfp/trcl/coll/PropertyBasedTagger.java
@@ -30,13 +30,13 @@ import com.ochafik.util.listenable.Pair;
 
 public class PropertyBasedTagger<E extends PropertyListenable,KEY,PROPERTY_TYPE> implements Collection<E>, Decorator<Collection<Pair<KEY,E>>> {
     private final Collection<Pair<KEY,E>>    delegate;
-    private final Adapter<PROPERTY_TYPE,KEY> propertyAdapter;
+    private final Adapter<PropertyChangeEvent,KEY> propertyAdapter;
     private final Map<E,PropertyChangeListener> listeners= new HashMap<E,PropertyChangeListener>();
     private final Map<E,Pair<KEY,E>> pairs               = new HashMap<E,Pair<KEY,E>>();
     private final String propertyName;
     private final ExecutorService executor;
     
-    public PropertyBasedTagger(Collection<Pair<KEY,E>> delegate, Adapter<PROPERTY_TYPE,KEY> nonCachedPropertyAdapter, String propertyName, ExecutorService executor){
+    public PropertyBasedTagger(Collection<Pair<KEY,E>> delegate, Adapter<PropertyChangeEvent,KEY> nonCachedPropertyAdapter, String propertyName, ExecutorService executor){
 	this.delegate       =delegate;
 	this.propertyAdapter=nonCachedPropertyAdapter;
 	this.propertyName   =propertyName;
@@ -45,37 +45,42 @@ public class PropertyBasedTagger<E extends PropertyListenable,KEY,PROPERTY_TYPE>
     
     @Override
     public boolean add(final E e) {
+	if(e==null)
+	    throw new NullPointerException("Passed element intolerably null.");
 	Pair<KEY,E>pair= pairs.get(e);
 	if(pair!=null)
 	    return false;
-	try{pair= new Pair<KEY,E>(propertyAdapter.adapt((PROPERTY_TYPE)PropertyUtils.getProperty(e, propertyName)),e);}
+	try{pair= new Pair<KEY,E>(propertyAdapter.adapt(new PropertyChangeEvent(e, propertyName, null, (PROPERTY_TYPE)PropertyUtils.getProperty(e, propertyName))),e);}
 	catch(NoSuchMethodException     ex){throw new IllegalArgumentException("Supplied element (bean) does not expose method for property: `"+propertyName+"`.");}
 	catch(InvocationTargetException ex){throw new RuntimeException(ex);}
 	catch(IllegalAccessException    ex){throw new IllegalArgumentException("Supplied element (bean) does not allow access for property `"+propertyName+"`.",ex);}
+	
 	PropertyChangeListener pcl = new PropertyChangeListener(){
 	    @Override
 	    public void propertyChange(final PropertyChangeEvent evt) {
 		final Runnable r = new Runnable(){
 		    @Override
 		    public void run() {
-			final KEY newKey     = propertyAdapter.adapt((PROPERTY_TYPE)evt.getNewValue());
+			final KEY newKey     = propertyAdapter.adapt(evt);
 			final Pair<KEY,E>pair= pairs.get(e);
+			assert pair!=null:"pair unexpectedly null.";
 			final KEY oldKey     = pair.getKey();
 			if(!newKey.equals(oldKey)){
 			    //Remove old
 			    delegate.remove(pair);
 			    pairs   .remove(e);
 			    //Add new
-			    final Pair<KEY,E> newPair = new Pair<KEY,E>(propertyAdapter.adapt((PROPERTY_TYPE)evt.getNewValue()),e);
+			    final Pair<KEY,E> newPair = new Pair<KEY,E>(newKey,e);
 			    assert newPair!=null:"pair unexpectedly null.";
-			    assert delegate.add(newPair):"Failed to add pair to delegate.";
+			    final boolean addResult = delegate.add(newPair); 
+			    assert addResult:"Failed to add pair to delegate.";
 			    pairs.put(e,newPair);
 			}//end if(changed key)
 		    }};
 		if(executor==null)
 		    r.run();
 		else
-		    try{executor.submit(r).get();}catch(Exception e){throw new RuntimeException(e);}
+		    try{executor.submit(r);}catch(Exception e){throw new RuntimeException(e);}
 	    }};
 	listeners.put(e,pcl);
 	e.addPropertyChangeListener(propertyName, pcl);

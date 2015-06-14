76b4b21c6499a4fef3aa4f07305b198da28489f4
Chuck Ritola <cjritola@gmail.com>
2015-06-14 05:43:34 -0400

    Remove superfluous finalizeModel().

--- src/main/java/org/jtrfp/trcl/obj/MeterBar.java
+++ src/main/java/org/jtrfp/trcl/obj/MeterBar.java
@@ -83,7 +83,6 @@ public class MeterBar extends WorldObject2DVisibleEverywhere {
 	m.setFrameDelayInMillis(1000);
 	m.setAnimateUV(true);
 	m.setController(controller);
-	m.finalizeModel();
 	setModel(m);
 	}//end orientation
     /**

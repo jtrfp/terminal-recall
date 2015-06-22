45be5674262f0fa7b2e3106b951e6390e1981c63
Chuck Ritola <cjritola@gmail.com>
2015-06-22 04:49:59 -0400

    Slowed down weapon velocities.

--- src/main/java/org/jtrfp/trcl/file/Weapon.java
+++ src/main/java/org/jtrfp/trcl/file/Weapon.java
@@ -33,10 +33,10 @@ public enum Weapon {
     purpleLaser(null, null,new BINModelingType("LASER.BIN"), 4096, ModelingType.MAP_SQUARE_SIZE * 6, -1, 2048,
 	    false, false), 
     PAC("PAC", "SKL", new BINModelingType("LASER3.BIN"), 4096, //LASER3.BIN
-	    ModelingType.MAP_SQUARE_SIZE * 6, 1, 2048, true, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 4, 1, 2048, true, false), 
     ION("ION",
 	    "DC1", new BINModelingType("LASER4.BIN"), 8192,
-	    ModelingType.MAP_SQUARE_SIZE * 6, 2, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 4, 2, 2048, false, false), 
     RTL(
 	    "RTL", "RFL20", new BINModelingType("LASER5.BIN"), 4096, ModelingType.MAP_SQUARE_SIZE * 8,
 	    3, 2048, true, false), 
@@ -44,15 +44,15 @@ public enum Weapon {
 	    "FIREBALL.BIN"), 8192, ModelingType.MAP_SQUARE_SIZE * 8, -1, 1024,
 	    false, false), 
     greenLaser(null, null, new BINModelingType("LASER7.BIN"), 8192,
-	    ModelingType.MAP_SQUARE_SIZE * 6, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 4, -1, 2048, false, false), 
     redLaser(
 	    null, null, new BINModelingType("LASER8.BIN"), 4096, ModelingType.MAP_SQUARE_SIZE * 7, -1, 2048,
 	    false, false), 
     blueLaser(null, null, new BINModelingType("LASER9.BIN"), 4096,
-	    ModelingType.MAP_SQUARE_SIZE * 7, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 5, -1, 2048, false, false), 
     bullet(
 	    null, null, new BINModelingType("BULLET.BIN"), 6554,
-	    ModelingType.MAP_SQUARE_SIZE * 6, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 4, -1, 2048, false, false), 
     purpleBall(
 	    null, null, new BillboardModelingType(new String[] { "PBALL1.RAW",
 		    "PBALL3.RAW", "PBALL4.RAW" }, 70, new Dimension(320000,
@@ -61,41 +61,41 @@ public enum Weapon {
     blueFireBall(null, null, new BillboardModelingType(
 	    new String[] { "BFIRJ0.RAW", "BFIRJ1.RAW", "BFIRJ2.RAW",
 		    "BFIRJ3.RAW" }, 100, new Dimension(320000, 320000)), 6554,
-	    ModelingType.MAP_SQUARE_SIZE * 6, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 3, -1, 2048, false, false), 
     goldBall(
 	    null, null, new BINModelingType("FIREBALL.BIN"), 8000,
 	    ModelingType.MAP_SQUARE_SIZE * 10, -1, 2048, false, false), 
     atomWeapon(
 	    null, null, new BillboardModelingType(new String[] { "ATM2.RAW",
 		    "ATM3.RAW" }, 70, new Dimension(320000, 320000)), 10000,
-	    ModelingType.MAP_SQUARE_SIZE * 4, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 3, -1, 2048, false, false), 
     purpleRing(
 	    null, null, new BillboardModelingType(new String[] {
 		    "PURFRIN0.RAW", "PURFRIN1.RAW", "PURFRIN2.RAW",
 		    "PURFRIN3.RAW" }, 45, new Dimension(320000, 320000)),
-	    8192, ModelingType.MAP_SQUARE_SIZE * 3, -1, 2048, false, false), 
+	    8192, ModelingType.MAP_SQUARE_SIZE * 4, -1, 2048, false, false), 
     bossW6(
 	    null, null, new BINModelingType("WBOSS6.BIN"), 6554,
-	    ModelingType.MAP_SQUARE_SIZE * 7, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 5, -1, 2048, false, false), 
     bossW7(
 	    null, null, new BINModelingType("WBOSS7.BIN"), 25,
 	    ModelingType.MAP_SQUARE_SIZE * 4, -1, 2048, false, false), 
     bossW8(
 	    null, null, new BINModelingType("WBOSS8.BIN"), 8192,
-	    ModelingType.MAP_SQUARE_SIZE * 6, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 4, -1, 2048, false, false), 
     enemyMissile(
 	    null, null, new BINModelingType("BRADMIS.BIN"), 8192,
-	    ModelingType.MAP_SQUARE_SIZE * 2, -1, 2048, false, false), 
+	    ModelingType.MAP_SQUARE_SIZE * 3, -1, 2048, false, false), 
     MAM(
 	    "MAM", "DOM", new BINModelingType("BRADMIS.BIN"), 16384,
-	    ModelingType.MAP_SQUARE_SIZE * 5, 4, 2048, false, false),
+	    ModelingType.MAP_SQUARE_SIZE * 4, 4, 2048, false, false),
     // ////// THESE ARE NOT PART OF THE ORDINAL ORDER OF A DEF FILE AND MAY BE
     // RE-ORDERED
     SAD("SAD", "VIP", new BINModelingType("BRADMIS.BIN"), 32768,
-	    ModelingType.MAP_SQUARE_SIZE * 5, 5, 2048, false, true), 
+	    ModelingType.MAP_SQUARE_SIZE * 4, 5, 2048, false, true), 
     SWT("SWT",
 	    "BFM", new BINModelingType("BRADMIS.BIN"), 65536,
-	    ModelingType.MAP_SQUARE_SIZE * 7, 6, 2048, false, true), 
+	    ModelingType.MAP_SQUARE_SIZE * 5, 6, 2048, false, true), 
     DAM("DAM",
 	    "FFF", new FlatModelingType("FIRBAL0.RAW","SQGLSR.RAW", new Dimension(80000,
 		    560000)), Integer.MAX_VALUE, 0, 7,

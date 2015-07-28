e8a5bcb0f2c0d07a9ede5087ce343ace2dca085f
Chuck Ritola <cjritola@gmail.com>
2015-07-28 10:10:18 -0400

    Add WriterState and PrimitiveType(s) to BasicModelTarget.

--- src/main/java/org/jtrfp/trcl/gpu/BasicModelTarget.java
+++ src/main/java/org/jtrfp/trcl/gpu/BasicModelTarget.java
@@ -17,18 +17,19 @@ import org.jtrfp.trcl.core.TextureDescription;
 public interface BasicModelTarget {
     /**
      * 
-     * @param index
-     * @param target X,Y,Z,nX,nY,nZ
-     * @since Jul 26, 2015
+     * @param target  { X Y Z U V nX nY nZ } values will be copied.
+     * @return The index of the set vertex
+     * @since Jul 27, 2015
      */
-    public void addVertex(int index, double [] target);
+    public void addVertex(double [] target);
     
-    public void addPrimitive(TriangleData data);
+    public void addPrimitive(WriterState data);
     
-    public static class TriangleData{
+    public static class WriterState{
 	private final int []       vertices = new int[3];
 	private TextureDescription texture;
 	private PrimitiveType      primitiveType;
+	private int                vertexIndex, primitiveIndex;
 
 	/**
 	 * @return the texture
@@ -64,7 +65,37 @@ public interface BasicModelTarget {
 	public void setPrimitiveType(PrimitiveType primitiveType) {
 	    this.primitiveType = primitiveType;
 	}
+
+	/**
+	 * @return the vertexIndex
+	 */
+	public int getVertexIndex() {
+	    return vertexIndex;
+	}
+
+	/**
+	 * @param vertexIndex the vertexIndex to set
+	 */
+	public void setVertexIndex(int vertexIndex) {
+	    this.vertexIndex = vertexIndex;
+	}
+
+	/**
+	 * @return the primitiveIndex
+	 */
+	public int getPrimitiveIndex() {
+	    return primitiveIndex;
+	}
+
+	/**
+	 * @param primitiveIndex the primitiveIndex to set
+	 */
+	public void setPrimitiveIndex(int primitiveIndex) {
+	    this.primitiveIndex = primitiveIndex;
+	}
     }//end TriangleData
     
     public interface PrimitiveType{}
+    public interface TriangleType extends PrimitiveType{}
+    public static final TriangleType SOLID_TRIANGLE = new TriangleType(){};
 }//end IModel

a450fa3473e20e6ea1e5ff30dce33aba87dbf81b
Chuck Ritola <cjritola@gmail.com>
2015-07-29 19:59:10 -0400

    Initial creation of Rotatable interface.

---
+++ src/main/java/org/jtrfp/trcl/obj/Rotatable.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2015 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+package org.jtrfp.trcl.obj;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+public interface Rotatable {
+ public Vector3D getTop();
+ public Vector3D getHeading();
+}

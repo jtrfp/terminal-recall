5c30dab8f35b0257c2f946d35f4c89eaf72f189c
Chuck Ritola <cjritola@gmail.com>
2015-07-31 23:19:56 -0400

    Initial creation of InterpolatedAnimatedModelSource.

---
+++ src/main/java/org/jtrfp/trcl/gpu/InterpolatedAnimatedModelSource.java
@@ -0,0 +1,86 @@
+package org.jtrfp.trcl.gpu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jtrfp.trcl.coll.Decorator;
+import org.jtrfp.trcl.gpu.BasicModelTarget.PrimitiveType;
+
+public class InterpolatedAnimatedModelSource implements BasicModelSource {
+    private double                      currentFrame = 0;
+    private final List<BasicModelSource>frames = new ArrayList<BasicModelSource>();
+    private Integer                     delayBetweenFramesMillis;
+
+    /**
+     * @return the currentFrame
+     */
+    public double getCurrentFrame() {
+	if(delayBetweenFramesMillis!=null)
+	    return ((double)System.currentTimeMillis())/delayBetweenFramesMillis.doubleValue() % frames.size();
+        return currentFrame;
+    }
+
+    /**
+     * @param currentFrame the currentFrame to set
+     */
+    public void setCurrentFrame(double currentFrame) {
+        this.currentFrame = currentFrame;
+    }
+    
+    public void setNormalizedCurrentFrame(double nCurrentFrame){
+	setCurrentFrame(nCurrentFrame*(double)frames.size());
+    }
+
+    /**
+     * @param index
+     * @return
+     * @see org.jtrfp.trcl.gpu.BasicModelSource#getVertex(int)
+     */
+    public double[] getVertex(int index) {
+	final double [] result = new double[8];
+	final int nFrames   = frames.size();
+	final int    rFrame = (int)getCurrentFrame();
+	final int    lFrame = rFrame==0?nFrames-1:rFrame-1;
+	final double mFrame = getCurrentFrame()%1.;
+	final double imFrame= 1-mFrame;
+	final double [] dR     = frames.get(rFrame).getVertex(index);
+	final double [] dL     = frames.get(lFrame).getVertex(index);
+	final int n = Math.min(dR.length,dL.length);
+	for(int i=0; i<n; i++)
+	    result[i]=mFrame*dR[i]+imFrame*dL[i];
+	return result;
+    }
+
+    /**
+     * @param index
+     * @return
+     * @see org.jtrfp.trcl.gpu.BasicModelSource#getPrimitiveVertexIDs(int)
+     */
+    public int[] getPrimitiveVertexIDs(int index) {
+	return frames.get((int)getCurrentFrame()).getPrimitiveVertexIDs(index);
+    }
+
+    /**
+     * @param index
+     * @return
+     * @see org.jtrfp.trcl.gpu.BasicModelSource#getPrimitiveType(int)
+     */
+    public PrimitiveType getPrimitiveType(int index) {
+	return frames.get((int)getCurrentFrame()).getPrimitiveType(index);
+    }
+    
+    public int addModelFrame(BasicModelSource frameToAdd){
+	frames.add(frameToAdd);
+	return frames.size()-1;
+    }
+
+    /**
+     * 
+     * @param delay Time in milliseconds between each frame, or null if manually animated with setCurrentFrame()
+     * @since Jul 30, 2015
+     */
+    public void setDelayBetweenFramesMillis(Integer delay) {
+	delayBetweenFramesMillis=delay;
+    }
+
+}//end InterpolatedAnimatedModelSource

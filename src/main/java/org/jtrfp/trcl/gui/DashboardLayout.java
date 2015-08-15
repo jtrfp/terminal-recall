523d179697dbe0bb564ab4fd0e6c7d33b9a1199d
Chuck Ritola <cjritola@gmail.com>
2015-08-15 06:03:04 -0400

    Initial creation of DashboardLayout interface.

---
+++ src/main/java/org/jtrfp/trcl/gui/DashboardLayout.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2015 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+package org.jtrfp.trcl.gui;
+import java.awt.geom.Point2D;
+
+public interface DashboardLayout {
+ public Point2D.Double getAmmoPosition();
+ public Point2D.Double getWeaponNamePosition();
+ public Point2D.Double getObjectivePosition();
+ public Point2D.Double getThrottlePosition();
+ public Point2D.Double getShieldPosition();
+ public Point2D.Double getDistancePosition();
+ public Point2D.Double getSectorPosition();
+ public Point2D.Double getDashboardPosition();
+ public Point2D.Double getDashboardDimensions();
+ public Point2D.Double getMiniMapPosition();
+}//end DashboardLayout

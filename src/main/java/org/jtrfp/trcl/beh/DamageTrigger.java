711015ac9a6daa6fe6ed7b496fdceafcf8b10042
Chuck Ritola <cjritola@gmail.com>
2015-12-22 01:14:04 -0500

    Add triggering bound property plus listener support to DamageTrigger.

--- src/main/java/org/jtrfp/trcl/beh/DamageTrigger.java
+++ src/main/java/org/jtrfp/trcl/beh/DamageTrigger.java
@@ -12,17 +12,20 @@
  ******************************************************************************/
 package org.jtrfp.trcl.beh;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public abstract class DamageTrigger extends Behavior {
+    public static final String TRIGGERED = "triggered";
 private boolean triggered=false;
 private int threshold=2048;
+private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
 	@Override
 	public void tick(long timeInMillis){
 	    if(!triggered){
 		if(getParent().probeForBehavior(DamageableBehavior.class).getHealth()<threshold){
-		    triggered=true;//Just in case disabling has latency
-		    setEnable(false);///zzz...
-		    healthBelowThreshold();
+		    setTriggered(true);//Just in case disabling has latency
 		}//end if(damaged)
 	    }//end if(!triggered)
 	}//end _tick(...)
@@ -42,5 +45,52 @@ private int threshold=2048;
 	public DamageTrigger setThreshold(int threshold) {
 	    this.threshold = threshold;
 	    return this;
+	}
+
+	public boolean isTriggered() {
+	    return triggered;
+	}
+
+	public void setTriggered(boolean triggered) {
+	    if(triggered==this.triggered)
+		return;
+	    final boolean oldValue = this.triggered;
+	    this.triggered = triggered;
+	    pcs.firePropertyChange(TRIGGERED, oldValue, triggered);
+	    if(triggered){
+		setEnable(false);///zzz...
+		healthBelowThreshold();
+	    }//end if(triggered)
+	}//end setTriggered()
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+	    pcs.addPropertyChangeListener(listener);
+	}
+
+	public void addPropertyChangeListener(String propertyName,
+		PropertyChangeListener listener) {
+	    pcs.addPropertyChangeListener(propertyName, listener);
+	}
+
+	public PropertyChangeListener[] getPropertyChangeListeners() {
+	    return pcs.getPropertyChangeListeners();
+	}
+
+	public PropertyChangeListener[] getPropertyChangeListeners(
+		String propertyName) {
+	    return pcs.getPropertyChangeListeners(propertyName);
+	}
+
+	public boolean hasListeners(String propertyName) {
+	    return pcs.hasListeners(propertyName);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+	    pcs.removePropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener(String propertyName,
+		PropertyChangeListener listener) {
+	    pcs.removePropertyChangeListener(propertyName, listener);
 	}
 }//end DamageTrigger

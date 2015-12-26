cba4121fa39e35437dd1441a32d1b19ecc4368e5
Chuck Ritola <cjritola@gmail.com>
2015-12-26 03:28:37 -0500

    Add reset support to SpinCrashDeathBehavior.

--- src/main/java/org/jtrfp/trcl/beh/SpinCrashDeathBehavior.java
+++ src/main/java/org/jtrfp/trcl/beh/SpinCrashDeathBehavior.java
@@ -13,6 +13,7 @@
 package org.jtrfp.trcl.beh;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.jtrfp.trcl.AbstractSubmitter;
 import org.jtrfp.trcl.beh.AutoLeveling.LevelingAxis;
 import org.jtrfp.trcl.beh.SpinAccellerationBehavior.SpinMode;
 import org.jtrfp.trcl.beh.phy.PulledDownByGravityBehavior;
@@ -46,7 +47,10 @@ public class SpinCrashDeathBehavior extends DamageTrigger {
 	tr.soundSystem.get().getPlaybackFactory().
 	create(tr.getResourceManager().soundTextures.get("EXP2.WAV"), new double[]{.5*SoundSystem.DEFAULT_SFX_VOLUME*2,.5*SoundSystem.DEFAULT_SFX_VOLUME*2});
 
-	parent.addBehavior(new PulledDownByGravityBehavior().setEnable(true));
+	if(!parent.hasBehavior(PulledDownByGravityBehavior.class))
+	    parent.addBehavior(new PulledDownByGravityBehavior());
+	parent.probeForBehavior(PulledDownByGravityBehavior.class).setEnable(true);
+	
 	//parent.probeForBehavior(Propelled.class).setMaxPropulsion(4096).setPropulsion(4096);
 	probeForBehavior(DamagedByCollisionWithSurface.class).setEnable(true);
 	probeForBehavior(CollidesWithTerrain.class).setNudgePadding(0);
@@ -57,7 +61,9 @@ public class SpinCrashDeathBehavior extends DamageTrigger {
 	probeForBehavior(UserInputThrottleControlBehavior.class).setEnable(false);
 	final Propelled propelled = probeForBehavior(Propelled.class);
 	propelled.setPropulsion(propelled.getMaxPropulsion());
-	parent.addBehavior(new AutoLeveling().setLevelingAxis(LevelingAxis.HEADING).setLevelingVector(Vector3D.MINUS_J).setRetainmentCoeff(.98, .98, .98));
+	if(!parent.hasBehavior(AutoLeveling.class))
+	 parent.addBehavior(new AutoLeveling().setLevelingAxis(LevelingAxis.HEADING).setLevelingVector(Vector3D.MINUS_J).setRetainmentCoeff(.98, .98, .98));
+	parent.probeForBehavior(AutoLeveling.class).setEnable(true);
 	
 	/*if(def.getThrustSpeed()<800000){
 	    probeForBehavior(HasPropulsion.class).setPropulsion(0);
@@ -65,16 +71,62 @@ public class SpinCrashDeathBehavior extends DamageTrigger {
 	}*/
 	//Catastrophy
 	final double spinSpeedCoeff=.5;
-	parent.addBehavior(new SpinAccellerationBehavior().setSpinMode(SpinMode.LATERAL).setSpinAccelleration(.009*spinSpeedCoeff));
-	parent.addBehavior(new SpinAccellerationBehavior().setSpinMode(SpinMode.EQUATORIAL).setSpinAccelleration(.006*spinSpeedCoeff));
-	parent.addBehavior(new SpinAccellerationBehavior().setSpinMode(SpinMode.POLAR).setSpinAccelleration(.007*spinSpeedCoeff));
+	if(!parent.hasBehavior(SpinAccellerationBehavior.class)){
+	    parent.addBehavior(new SpinAccellerationBehavior().setSpinMode(SpinMode.LATERAL).setSpinAccelleration(.009*spinSpeedCoeff));
+	    parent.addBehavior(new SpinAccellerationBehavior().setSpinMode(SpinMode.EQUATORIAL).setSpinAccelleration(.006*spinSpeedCoeff));
+	    parent.addBehavior(new SpinAccellerationBehavior().setSpinMode(SpinMode.POLAR).setSpinAccelleration(.007*spinSpeedCoeff));
+	}
+	parent.probeForBehaviors(new AbstractSubmitter<SpinAccellerationBehavior>(){
+	    @Override
+	    public void submit(SpinAccellerationBehavior item) {
+		item.setEnable(true);
+	    }}, SpinAccellerationBehavior.class);
+	
 	//TODO: Sparks, and other fun stuff.
-	parent.addBehavior(new SpawnsRandomExplosionsAndDebris(parent.getTr()));
+	if(!parent.hasBehavior(SpawnsRandomExplosionsAndDebris.class))
+	    parent.addBehavior(new SpawnsRandomExplosionsAndDebris(parent.getTr()));
+	
 	parent.addBehavior(new SpawnsRandomSmoke(parent.getTr()));
 	//Set up for ground explosion
+	parent.probeForBehavior(DamagedByCollisionWithSurface.class).getCollisionDamage();
 	parent.probeForBehavior(DamagedByCollisionWithSurface.class).setCollisionDamage(65535);
 	parent.probeForBehavior(RedFlashOnDamage.class).setEnable(false);
 	System.out.println("end healthBelowThreshold()");
     }//end healthBelowThreshold
+
+    public SpinCrashDeathBehavior reset() {
+	//Undo the results of damage triggering
+	final WorldObject 	parent 	= getParent();
+	
+	if(parent.hasBehavior(PulledDownByGravityBehavior.class))
+	 parent.probeForBehavior(PulledDownByGravityBehavior.class).setEnable(false);
+	
+	probeForBehavior(DamagedByCollisionWithSurface.class).setEnable(true);
+	probeForBehavior(CollidesWithTerrain.class).setNudgePadding(0);
+	probeForBehavior(DamageableBehavior.class).setAcceptsProjectileDamage(true);
+	probeForBehavior(ExplodesOnDeath.class).setExplosionType(ExplosionType.BigExplosion).setExplosionSound(DEFObject.BIG_EXP_SOUNDS[(int)(Math.random()*3)]);
+	probeForBehavior(AfterburnerBehavior.class).setEnable(true);
+	probeForBehavior(UserInputRudderElevatorControlBehavior.class).setEnable(true);
+	probeForBehavior(UserInputThrottleControlBehavior.class).setEnable(true);
+	final Propelled propelled = probeForBehavior(Propelled.class);
+	propelled.setPropulsion(0);
+	//parent.addBehavior(new AutoLeveling().setLevelingAxis(LevelingAxis.HEADING).setLevelingVector(Vector3D.MINUS_J).setRetainmentCoeff(.98, .98, .98));
+	
+	parent.probeForBehavior(AutoLeveling.class).setEnable(false);
+	
+	parent.probeForBehaviors(new AbstractSubmitter<SpinAccellerationBehavior>(){
+	    @Override
+	    public void submit(SpinAccellerationBehavior item) {
+		item.setEnable(false);
+	    }}, SpinAccellerationBehavior.class);
+	
+	parent.probeForBehavior(SpawnsRandomExplosionsAndDebris.class).setEnable(false);
+	parent.probeForBehavior(SpawnsRandomSmoke.class).setEnable(false);
+	//Set up for ground explosion
+	parent.probeForBehavior(DamagedByCollisionWithSurface.class).setCollisionDamage(6554);
+	parent.probeForBehavior(RedFlashOnDamage.class).setEnable(true);
+	super.setTriggered(false);
+	return this;
+    }//end reset()
 
 }//end SpinCrashBehavior

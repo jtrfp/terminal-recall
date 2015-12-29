a5f5db61baa9608e555f43557c86fd40fabf6300
Chuck Ritola <cjritola@gmail.com>
2015-12-29 15:39:54 -0500

    Bugfix: Superfluous null-checking causes needless abort of Player collision test for transparent models. Affects #128.

--- src/main/java/org/jtrfp/trcl/beh/CollidesWithPlayer.java
+++ src/main/java/org/jtrfp/trcl/beh/CollidesWithPlayer.java
@@ -16,39 +16,53 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.jtrfp.trcl.AbstractSubmitter;
 import org.jtrfp.trcl.Submitter;
 import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.gpu.Model;
 import org.jtrfp.trcl.math.Vect3D;
 import org.jtrfp.trcl.obj.Player;
 import org.jtrfp.trcl.obj.WorldObject;
 
 public class CollidesWithPlayer extends Behavior implements CollisionBehavior {
     private Player player;
+    private Double collisionRadius = null;
     public CollidesWithPlayer(){
     }
     @Override
     public void proposeCollision(WorldObject other){
+	//if(getParent() instanceof ProjectileObject3D && ((ProjectileObject3D)getParent()).getWeapon()==Weapon.ION)
+	//    System.out.println("PO3D "+other);
 	if(other instanceof Player){
+	    final WorldObject parent = getParent();
+	    final Model parentModel  = parent.getModel();
 	    if(other.getModel()==null)
 		return;
-	    if(other.getModel().getTriangleList()==null)
+	    if(parentModel==null)
 		return;
-	    final WorldObject parent = getParent();
-	    if(parent.getModel()==null)
-		return;
-	    if(parent.getModel().getTriangleList()==null)
-		return;
-	    final Vector3D pMax = parent.getModel().getTriangleList().getMaximumVertexDims();
-	    final Vector3D oMax = other .getModel().getTriangleList().getMaximumVertexDims();
+	    final Vector3D pMax = parent.getModel().getMaximumVertexDims();
+	    final Vector3D oMax = other .getModel().getMaximumVertexDims();
 	    
 	    double dXZ       = TR.rolloverDistance(Vect3D.distanceXZ(parent.getPositionWithOffset(), other.getPositionWithOffset()));
 	    final double dY  = Math.abs(parent.getPositionWithOffset()[1]-other.getPositionWithOffset()[1]);
-	    final boolean cXZ = dXZ < Math.sqrt(Math.pow(pMax.getX()+oMax.getX(),2) + Math.pow(pMax.getZ()+oMax.getZ(),2));
-	    final boolean cY = dY   < (pMax.getY() + oMax.getY());
-	    player=(Player)other;
-	    if(cXZ && cY){
-		getParent().probeForBehaviors(sub, PlayerCollisionListener.class);
-	    }//end if(close enough)
+	    //System.out.println("INSTANCE="+getParent().getClass().getName());
+	    /*if(getParent() instanceof ProjectileObject3D){
+		final ProjectileObject3D po3 = (ProjectileObject3D)getParent();
+		//if(po3.getWeapon() == Weapon.ION)
+		 System.out.println("instance="+getParent()+" dXZ="+dXZ+" dY="+dY+" Weapon="+po3.getWeapon());
+		 }*/
+	    if(collisionRadius==null){
+		final boolean cXZ = dXZ < Math.sqrt(Math.pow(pMax.getX()+oMax.getX(),2) + Math.pow(pMax.getZ()+oMax.getZ(),2));
+		final boolean cY = dY   < (pMax.getY() + oMax.getY());
+		player=(Player)other;
+		if(cXZ && cY){
+		    reportCollision();
+		}//end if(close enough)
+	    }else if(Math.sqrt(dXZ*dXZ+dY*dY) < collisionRadius)
+		    reportCollision();
 	}//end if(player)
     }//end _proposeCollision()
+    
+    private void reportCollision(){
+	getParent().probeForBehaviors(sub, PlayerCollisionListener.class);
+    }
     
     private final Submitter<PlayerCollisionListener> sub = new AbstractSubmitter<PlayerCollisionListener>(){
 	@Override
@@ -56,4 +70,10 @@ public class CollidesWithPlayer extends Behavior implements CollisionBehavior {
 	    l.collidedWithPlayer(player);
 	}//end submit(...)
     };//end Submitter
+    public Double getCollisionRadius() {
+        return collisionRadius;
+    }
+    public void setCollisionRadius(Double collisionRadius) {
+        this.collisionRadius = collisionRadius;
+    }
 }

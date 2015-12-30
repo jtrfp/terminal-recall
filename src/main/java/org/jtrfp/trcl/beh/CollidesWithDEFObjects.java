346c12276cc4f14f30af4c9fb00279fba21460d3
Chuck Ritola <cjritola@gmail.com>
2015-12-30 09:34:00 -0500

    Add custom hitbox support to CollidesWithDEFObjects.

--- src/main/java/org/jtrfp/trcl/beh/CollidesWithDEFObjects.java
+++ src/main/java/org/jtrfp/trcl/beh/CollidesWithDEFObjects.java
@@ -17,26 +17,56 @@ import java.lang.ref.WeakReference;
 import org.jtrfp.trcl.AbstractSubmitter;
 import org.jtrfp.trcl.Submitter;
 import org.jtrfp.trcl.core.TR;
+import org.jtrfp.trcl.gpu.BasicModelSource;
+import org.jtrfp.trcl.math.Vect3D;
 import org.jtrfp.trcl.obj.DEFObject;
+import org.jtrfp.trcl.obj.DEFObject.HitBox;
 import org.jtrfp.trcl.obj.WorldObject;
 
 public class CollidesWithDEFObjects extends Behavior implements CollisionBehavior {
     private final double boundingRadius;
     private WeakReference<DEFObject> otherDEF;
+    private final double [] workTriplet = new double[3];
     public CollidesWithDEFObjects(double boundingRadius){
 	this.boundingRadius=boundingRadius;
     }
     @Override
     public void proposeCollision(WorldObject other){
 	if(other instanceof DEFObject){
-	    final double distance=TR.twosComplimentDistance(
-		    other.getPositionWithOffset(), 
-		    getParent().getPositionWithOffset());
+	    final DEFObject def = (DEFObject)other;
+	    final HitBox [] hitBoxes = def.getHitBoxes();
+	    final WorldObject parent = getParent();
+	   
+	    final double [] defPos = def.getPositionWithOffset();
+	    final double [] pPos   = parent.getPositionWithOffset();
+	    
 	    otherDEF=new WeakReference<DEFObject>((DEFObject)other);
-	    final double maxVtx = otherDEF.get().getModel().getMaximumVertexValue();
-	    if(distance<(boundingRadius+maxVtx)){
-		getParent().probeForBehaviors(sub, DEFObjectCollisionListener.class);
-	    }
+	    if(hitBoxes == null){// No custom hitboxes
+		final double maxVtx = otherDEF.get().getModel().getMaximumVertexValue();
+		 final double distance=TR.twosComplimentDistance(
+			    other.getPositionWithOffset(), 
+			    parent.getPositionWithOffset());
+		    if(distance<(boundingRadius+maxVtx))
+			parent.probeForBehaviors(sub, DEFObjectCollisionListener.class);
+	    }else{//Custom hitboxes
+		boolean doCollision = false;
+		BasicModelSource mSource = def.getModelSource();
+		if(mSource == null)
+		    throw new NullPointerException("DEF's model source intolerably null.");
+		for(HitBox box:hitBoxes){
+		    final double limit = boundingRadius+box.getSize();
+		    final double [] vPos = Vect3D.add(mSource.getVertex(box.getVertexID()),defPos,workTriplet);
+		    Vect3D.subtract(workTriplet, pPos, vPos);
+		    Vect3D.abs(workTriplet, workTriplet);
+		    boolean localDoCollision = true;
+		    for(int i=0; i<3; i++)
+		     localDoCollision &= TR.rolloverDistance(workTriplet[i]) < limit;
+		    doCollision |= localDoCollision;
+		}//end for(hitBoxes)
+		System.out.println("Custom hitbox: ");
+		if(doCollision)
+		 parent.probeForBehaviors(sub, DEFObjectCollisionListener.class);
+	    }//end if(custom hitboxes)
 	}//end if(DEFObject)
     }//end _proposeCollision()
     

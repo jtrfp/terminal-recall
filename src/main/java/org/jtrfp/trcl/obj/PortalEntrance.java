1eac4e1ce8bb900c1ccab1aa261d312086ef0a01
Chuck Ritola <cjritola@gmail.com>
2016-02-22 10:09:05 -0500

    ✔Relative position must consider map-rollover. Affects #137.

--- src/main/java/org/jtrfp/trcl/obj/PortalEntrance.java
+++ src/main/java/org/jtrfp/trcl/obj/PortalEntrance.java
@@ -106,6 +106,8 @@ public class PortalEntrance extends WorldObject {
 
     public double[] getRelativePosition(double [] dest){
 	Vect3D.subtract(getApproachingObject().getPosition(), PortalEntrance.this.getPosition(), dest);
+	for(int i=0; i<3; i++)
+	 dest[i] = TR.deltaRollover(dest[i]);
 	return dest;
     }
     

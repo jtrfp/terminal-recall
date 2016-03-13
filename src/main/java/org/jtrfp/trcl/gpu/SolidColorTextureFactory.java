bf54f9811db082f66330804bfbd3f8230edb47e4
Chuck Ritola <cjritola@gmail.com>
2016-03-12 22:15:49 -0500

    ⟴SolidColorTextureFactory.

---
+++ src/main/java/org/jtrfp/trcl/gpu/SolidColorTextureFactory.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2016 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.gpu;
+
+import java.awt.Color;
+
+import org.jtrfp.trcl.core.ThreadManager;
+import org.jtrfp.trcl.img.vq.PalettedVectorList;
+import org.jtrfp.trcl.img.vq.VectorList;
+
+public class SolidColorTextureFactory {
+ public VQTexture newSolidColorTexture(GPU gpu, ThreadManager threadManager, Color c){
+     return new VQTexture(gpu,threadManager,new PalettedVectorList(colorZeroRasterVL(), colorVL(c)),null,"SolidColor r="+c.getRed()+" g="+c.getGreen()+" b="+c.getBlue(),false);
+ }//end newSolidColorTexture
+ 
+ private static VectorList colorZeroRasterVL(){
+	return new VectorList(){
+	    @Override
+	    public int getNumVectors() {
+		return 16;
+	    }
+
+	    @Override
+	    public int getNumComponentsPerVector() {
+		return 1;
+	    }
+
+	    @Override
+	    public double componentAt(int vectorIndex, int componentIndex) {
+		return 0;
+	    }
+
+	    @Override
+	    public void setComponentAt(int vectorIndex, int componentIndex,
+		    double value) {
+		throw new RuntimeException("Cannot write to Texture.colorZeroRasterVL VectorList");
+	    }};
+ }//end colorZeroRasterVL
+ 
+ private static VectorList colorVL(Color c){
+	final double [] color = new double[]{
+		c.getRed()/255.,c.getGreen()/255.,c.getBlue()/255.,c.getAlpha()/255.};
+	
+	return  new VectorList(){
+	    @Override
+	    public int getNumVectors() {
+		return 1;
+	    }
+
+	    @Override
+	    public int getNumComponentsPerVector() {
+		return 4;
+	    }
+
+	    @Override
+	    public double componentAt(int vectorIndex, int componentIndex) {
+		return color[componentIndex];
+	    }
+
+	    @Override
+	    public void setComponentAt(int vectorIndex, int componentIndex,
+		    double value) {
+		throw new RuntimeException("Static palette created by Texture(Color c, TR tr) cannot be written to.");
+	    }};
+ }//end colorVL(...)
+}//end SolidColorTextureFactory

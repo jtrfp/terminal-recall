4c4707dae5a7c50f2253f000256cd607a6d7c81c
Chuck Ritola <cjritola@gmail.com>
2016-03-17 05:16:09 -0400

    ❎SolidColorTextureFactory.

--- src/main/java/org/jtrfp/trcl/gpu/SolidColorTextureFactory.java
+++ src/main/java/org/jtrfp/trcl/gpu/SolidColorTextureFactory.java
@@ -19,18 +19,11 @@ import java.nio.ByteBuffer;
 
 import org.jtrfp.trcl.core.ThreadManager;
 import org.jtrfp.trcl.gpu.VQCodebookManager.RasterRowWriter;
-import org.jtrfp.trcl.img.vq.VectorList;
-import org.jtrfp.trcl.mem.VEC4Address;
 
 public class SolidColorTextureFactory {
     private final GPU gpu;
-    private final ThreadManager threadManager;
-    private final UncompressedVQTextureFactory vqtf;
-    
     public SolidColorTextureFactory(GPU gpu, ThreadManager threadManager){
 	this.gpu = gpu;
-	this.threadManager = threadManager;
-	vqtf = new UncompressedVQTextureFactory(gpu, threadManager, "SolidColorTextureFactory");
     }//end constructor
     
  public VQTexture newSolidColorTexture(Color c){
@@ -42,14 +35,14 @@ public class SolidColorTextureFactory {
      //Assign to subtexture 0 (we can assume there is only one)
      final int stid = result.getSubTextureIDs().get(0);
      final SubTextureWindow stw = result.getSubTextureWindow();
-     result.getSubTextureWindow().codeStartOffsetTable.setAt(stid, 0, codebook256*256);
+     //Set the code-start offset
+     stw.codeStartOffsetTable.setAt(stid, 0, codebook256*256);
      //Write the color to the codebook
      RasterRowWriter [] writers = new RasterRowWriter[256];
      //TODO: Use a tile pool for solid colors
      final SolidColorRowWriter rw = new SolidColorRowWriter(c);
      for(int i=0; i<256; i++)
 	 writers[i] = rw;
-     new VEC4Address(stw.getPhysicalAddressInBytes(stid)).intValue();
      gpu.textureManager.get().vqCodebookManager.get().setRGBABlock256(codebook256, writers);
      final byte codeID = 0x0;
      result.setCodeAt(0,0,codeID);
@@ -73,55 +66,4 @@ public class SolidColorTextureFactory {
 	}//end for(4)
     }//end applyRow
  }//end SolidColorRowWriter
- 
- private static VectorList colorZeroRasterVL(){
-	return new VectorList(){
-	    @Override
-	    public int getNumVectors() {
-		return 16;
-	    }
-
-	    @Override
-	    public int getNumComponentsPerVector() {
-		return 1;
-	    }
-
-	    @Override
-	    public double componentAt(int vectorIndex, int componentIndex) {
-		return 0;
-	    }
-
-	    @Override
-	    public void setComponentAt(int vectorIndex, int componentIndex,
-		    double value) {
-		throw new RuntimeException("Cannot write to Texture.colorZeroRasterVL VectorList");
-	    }};
- }//end colorZeroRasterVL
- 
- private static VectorList colorVL(Color c){
-	final double [] color = new double[]{
-		c.getRed()/255.,c.getGreen()/255.,c.getBlue()/255.,c.getAlpha()/255.};
-	
-	return  new VectorList(){
-	    @Override
-	    public int getNumVectors() {
-		return 1;
-	    }
-
-	    @Override
-	    public int getNumComponentsPerVector() {
-		return 4;
-	    }
-
-	    @Override
-	    public double componentAt(int vectorIndex, int componentIndex) {
-		return color[componentIndex];
-	    }
-
-	    @Override
-	    public void setComponentAt(int vectorIndex, int componentIndex,
-		    double value) {
-		throw new RuntimeException("Static palette created by Texture(Color c, TR tr) cannot be written to.");
-	    }};
- }//end colorVL(...)
 }//end SolidColorTextureFactory

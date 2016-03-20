1c23f33d8e9fb6bea27efe4991bec487a76fb8d9
Chuck Ritola <cjritola@gmail.com>
2016-03-20 04:33:01 -0400

    ⟴SettableTexture.

---
+++ src/main/java/org/jtrfp/trcl/gpu/SettableTexture.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2016 Chuck Ritola and contributors.
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.gpu;
+
+public class SettableTexture extends DynamicTexture {
+    private int currentTexturePage;
+
+    @Override
+    public int getCurrentTexturePage() {
+	return currentTexturePage;
+    }
+
+    protected void setCurrentTexturePage(int currentTexturePage) {
+        this.currentTexturePage = currentTexturePage;
+    }
+
+}//end SettableTexture

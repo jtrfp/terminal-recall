790c4b2d8cbf53a8398ed8da27bd2d1efd1a7e83
Chuck Ritola <cjritola@gmail.com>
2016-05-29 11:53:56 -0400

    ⟴CockpitLayoutTV.

---
+++ src/main/java/org/jtrfp/trcl/gui/CockpitLayoutTV.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2016 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.gui;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+public class CockpitLayoutTV implements CockpitLayout {
+
+    @Override
+    public Vector3D getMiniMapPosition() {
+	return new Vector3D(0, -1450, 8800);
+    }
+
+    @Override
+    public double getMiniMapRadius() {
+	return 1200;
+    }
+
+    @Override
+    public Vector3D getMiniMapNormal() {
+	return new Vector3D(0, 0.6706855765, 0.7417417727);
+    }
+
+    @Override
+    public Vector3D getMiniMapTopOrigin() {
+	return new Vector3D(0, 0.7417417727, -0.6706855765);
+    }
+}//end CockpitLayoutTV

45c0a188022d033b823ed8a8ca8a074a4334f65f
Chuck Ritola <cjritola@gmail.com>
2016-07-04 23:35:05 -0400

    ✔Refactor OpenGLIT to radeon omega specs.

--- src/test/java/org/jtrfp/trcl/gpu/OpenGLIT.java
+++ src/test/java/org/jtrfp/trcl/gpu/OpenGLIT.java
@@ -13,25 +13,16 @@
 
 package org.jtrfp.trcl.gpu;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
+import java.awt.Component;
+import java.awt.Frame;
 import java.awt.GraphicsEnvironment;
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
 
 import javax.media.opengl.GL3;
-import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLContext;
 import javax.media.opengl.GLProfile;
-import javax.media.opengl.GLRunnable;
 import javax.media.opengl.awt.GLCanvas;
-import javax.swing.JFrame;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -41,7 +32,7 @@ import org.junit.Test;
  *
  */
 public class OpenGLIT {
-    protected JFrame    frame;
+    //protected JFrame    frame;
     protected GLCanvas  canvas;
     protected GLContext context;
     protected volatile boolean   success = false;
@@ -54,101 +45,43 @@ public class OpenGLIT {
 	    "void main(){\n" +
 	    " gl_Position.x=gl_VertexID%2;\n" +
 	    " gl_Position.y=(gl_VertexID/2)%2;\n" +
-	    " gl_Position.z=1;\n"+
+	    " gl_Position.z=0;\n"+
+	    " gl_Position.w=1;\n"+
 	    "}\n";
     protected static final String FRAG_SOURCE = 
 	    "#version 330\n" +
 	    "void main(){\n" +
-	    " gl_FragColor.rgb = vec3(0,0,1);\n" +
+	    " gl_FragColor.rgba = vec4(0,0,1,1);\n" +
 	    "}\n";
 
     @Before
     public void setUp() throws Exception {
-	if(GraphicsEnvironment.isHeadless())
-	    return;
-	frame  = new JFrame("test");
-	canvas = new GLCanvas(new GLCapabilities(GLProfile.getGL2GL3()));
-	frame.getContentPane().add(canvas);
-	frame.setSize(640, 480);
-	frame.setVisible(true);
-	canvas.setRealized(true);
     }
 
     @After
     public void tearDown() throws Exception {
-	if(frame!=null)
-	 frame.setVisible(false);
-    }
+	if(canvas != null){
+	    Component cmp = canvas;
+	    while(!(cmp instanceof Frame))
+		cmp = cmp.getParent();
+	    Frame frame = (Frame)cmp;
+	    canvas.setVisible(false);
+	    frame.setVisible (false);
+	}
+    }//end tearDown()
 
     @Test
-    public void test() {
-	if(canvas==null)
+    public void test() throws Exception {
+	if(getCanvas()==null)
 	    return;//Headless
-	if(!canvas.invoke(true, new GLRunnable(){
-	    @Override
-	    public boolean run(GLAutoDrawable drawable) {
-		try{
-		final GL3 gl = drawable.getGL().getGL3();
-		final int program = gl.glCreateProgram();
-		final int vShader = gl.glCreateShader(GL3.GL_VERTEX_SHADER);
-		final int fShader = gl.glCreateShader(GL3.GL_FRAGMENT_SHADER);
-		gl.glShaderSource(vShader, 1, new String[]{VERTEX_SOURCE}, IntBuffer.wrap(new int []{VERTEX_SOURCE.length()}));
-		gl.glShaderSource(fShader, 1, new String[]{FRAG_SOURCE  }, IntBuffer.wrap(new int []{FRAG_SOURCE  .length()}));
-		gl.glCompileShader(vShader); 
-		final IntBuffer ib       = IntBuffer.wrap(new int[]{5});
-		final ByteBuffer infoLog = ByteBuffer.allocate(4096);
-		gl.glGetShaderiv(vShader, GL3.GL_COMPILE_STATUS, ib);
-		if(ib.get(0)!=1){
-		    StringBuilder log = new StringBuilder();
-		    gl.glGetShaderInfoLog(vShader, 4096, ib, infoLog);
-		    log.append(new CharSequence(){
-			@Override
-			public char charAt(int index) {
-			    return (char)infoLog.get(index);
-			}
-			@Override
-			public int length() {
-			    return infoLog.capacity();
-			}
-			@Override
-			public CharSequence subSequence(int arg0, int arg1) {
-			    throw new UnsupportedOperationException();
-			}});
-		    fail("Vertex Shader invalid. Returned "+ib.get(0)+" log follows:\n"+log);
-		    }
-		gl.glCompileShader(fShader);
-		gl.glAttachShader(program, vShader);
-		gl.glAttachShader(program, fShader);
-		gl.glLinkProgram (program);
-		gl.glValidateProgram(program);
-		gl.glGetProgramiv(program, GL3.GL_VALIDATE_STATUS, ib);
-		if(ib.get(0)!=1){
-		    StringBuilder log = new StringBuilder();
-		    gl.glGetProgramInfoLog(program, 4096, ib, infoLog);
-		    log.append(new CharSequence(){
-			@Override
-			public char charAt(int index) {
-			    return (char)infoLog.get(index);
-			}
-			@Override
-			public int length() {
-			    return infoLog.capacity();
-			}
-			@Override
-			public CharSequence subSequence(int arg0, int arg1) {
-			    throw new UnsupportedOperationException();
-			}});
-		    fail("Program invalid. Returned "+ib.get(0)+" log follows:\n"+log);
-		    }
-		gl.glUseProgram(program);
-		gl.glEnableVertexAttribArray(0);
-		gl.glDrawArrays(GL3.GL_TRIANGLES, 0, 3);
-		gl.glUseProgram(0);}
-		catch(Exception e){e.printStackTrace();Assert.fail("Failure due to exception "+e);}
-		success=true;
-		return true;
-	    }}))
-	    fail("Failed to submit GL Task");
-	assertTrue(success);
+	GLTestUtils.runAttributelessShader(getCanvas(), VERTEX_SOURCE, FRAG_SOURCE, GL3.GL_TRIANGLES, 3);
     }//end test()
+
+    public GLCanvas getCanvas() throws Exception {
+	if(canvas == null){
+	    if(!GraphicsEnvironment.isHeadless())
+		canvas = GLTestUtils.newCompleteMiniCanvas();
+	}//end if(null)
+	return canvas;
+    }//end getCanvas()
 }//end OpenGLTest

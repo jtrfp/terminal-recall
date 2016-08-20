b05fd855a30203d758c5b301a122a3757507a6b4
Chuck Ritola <cjritola@gmail.com>
2016-08-20 03:08:42 -0400

    ⎘Refactor FeaturesImplTest against previous commit (fix CI error)

--- src/test/java/org/jtrfp/trcl/core/FeaturesImplTest.java
+++ src/test/java/org/jtrfp/trcl/core/FeaturesImplTest.java
@@ -32,7 +32,8 @@ public class FeaturesImplTest {
 	final DummyType1 dummyTarget1 = mock(DummyType1.class);
 	final DummyF1 dummyFeature1   = mock(DummyF1.class);
 	
-	when(factory.newInstance(dummyTarget1)).thenReturn (dummyFeature1);
+	try{when(factory.newInstance(dummyTarget1)).thenReturn (dummyFeature1);}
+	catch(FeatureNotApplicableException e){}
 	subject.registerFeature(factory);
 	
 	subject.init(dummyTarget1);
@@ -40,7 +41,8 @@ public class FeaturesImplTest {
 	//Ensure multiple calls works
 	assertEquals(dummyFeature1, subject.get(dummyTarget1, DummyF1.class));
 	assertEquals(dummyFeature1, subject.get(dummyTarget1, DummyF1.class));
-	verify(factory,times(1)).newInstance(dummyTarget1);
+	try{verify(factory,times(1)).newInstance(dummyTarget1);}
+	catch(FeatureNotApplicableException e){}
     }//end testGetFeature()
     
     private static interface DummyType1 {

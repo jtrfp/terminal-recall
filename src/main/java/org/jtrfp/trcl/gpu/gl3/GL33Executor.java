f6c733be876d933ca37a2b14b3f6e6e37374f7d1
Chuck Ritola <cjritola@gmail.com>
2016-08-26 08:16:28 -0400

    ⟴GL33Executor.

---
+++ src/main/java/org/jtrfp/trcl/gpu/gl3/GL33Executor.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2016 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.gpu.gl3;
+
+import javax.media.opengl.GL3;
+
+import org.jtrfp.trcl.gpu.GLExecutor;
+
+public interface GL33Executor extends GLExecutor<GL3> {
+
+}

2bd7f87fef6d3374a448fd9a19cb11b4c35c9e1d
Chuck Ritola <cjritola@gmail.com>
2016-09-13 23:56:59 -0400

    ⟴VertexModifierContext.

---
+++ src/main/java/org/jtrfp/trcl/gpu/VertexModifierContext.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2016 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+
+package org.jtrfp.trcl.gpu;
+
+public interface VertexModifierContext extends ModelModifierContext {
+ public void setVertex(int id, double x, double y, double z);
+}

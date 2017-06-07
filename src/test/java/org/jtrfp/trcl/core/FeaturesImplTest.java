f8d8f9648a03757aaa0770abcfcee075648a3112
Chuck Ritola <cjritola@gmail.com>
2017-06-06 23:25:04 -0400

    ⟴Tests for getByPath()

--- src/test/java/org/jtrfp/trcl/core/FeaturesImplTest.java
+++ src/test/java/org/jtrfp/trcl/core/FeaturesImplTest.java
@@ -1,7 +1,10 @@
 package org.jtrfp.trcl.core;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -93,6 +96,35 @@ public class FeaturesImplTest {
 	verify(impostorFactory,times(1)).newInstance(dummyTarget1);
     }
     
+    @Test
+    public void testGetFeatureByPath() throws Throwable {
+	final FeaturesImpl subject = getSubject();
+	final DummyType1 dummyTarget0 = mock(DummyType1.class);
+	
+	final FeatureFactory level0Factory = Mockito.mock(FeatureFactory.class);
+	when(level0Factory.getFeatureClass()).thenReturn (DummyF3.class);
+	when(level0Factory.getTargetClass()) .thenReturn (DummyType1.class);
+	final DummyF3 level0Feature        = mock(DummyF3.class);
+	when(level0Factory.newInstance(dummyTarget0)).thenReturn (level0Feature);
+	
+	final FeatureFactory level1Factory = Mockito.mock(FeatureFactory.class);
+	when(level1Factory.getFeatureClass()).thenReturn (DummyF3F1.class);
+	when(level1Factory.getTargetClass()) .thenReturn (DummyF3.class);
+	final DummyF3F1 level1Feature   = mock(DummyF3F1.class);
+	when(level1Factory.newInstance(level0Feature)).thenReturn (level1Feature);
+	
+	subject.registerFeature(level0Factory);
+	subject.registerFeature(level1Factory);
+	
+	subject.init(dummyTarget0);
+	
+	assertEquals(level1Feature, subject.getByPath(dummyTarget0, DummyF3F1.class, DummyF3.class));
+	//assertEquals(desiredFeature1, subject.get(dummyTarget1, DummyF3.class));
+	//assertEquals(impostorFeature1, subject.get(dummyTarget1, DummyF1.class));
+	verify(level0Factory,times(1)).newInstance(dummyTarget0);
+	verify(level1Factory,times(1)).newInstance(level0Feature);
+    }//end testGetFeatureByPath()
+    
     private static interface DummyType1 {
     }//end DummyType1
 
@@ -103,6 +135,9 @@ public class FeaturesImplTest {
     }//end DummyF2
     
     public static interface DummyF3 extends Feature<DummyType1> {
+    }//end DummyF3
+    
+    public static interface DummyF3F1 extends Feature<DummyF3> {
     }//end DummyF3
     
     public static interface DummyMultiIfaceFeature1 extends DummyF2, DummyF3 {

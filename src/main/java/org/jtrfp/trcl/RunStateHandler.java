f6e74fef7ff3c617bbbfcf267c5e50b2bc016b42
Chuck Ritola <cjritola@gmail.com>
2018-01-30 14:13:44 -0500

    ⟴RunStateHandler.

---
+++ src/main/java/org/jtrfp/trcl/RunStateHandler.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2012-2017 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public abstract class RunStateHandler implements PropertyChangeListener {
+    private boolean isInState = false;
+    public abstract boolean isValidRunState( Object oldRunSatte, Object newRunState);
+
+    public abstract void enteredRunState(Object oldState, Object newState);
+    public abstract void exitedRunState (Object oldState, Object newState);
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+	final Object oldState = evt.getOldValue();
+	final Object newState = evt.getNewValue();
+	final boolean newInState = isValidRunState(oldState, newState);
+	
+	if(isInState && !newInState)
+	    exitedRunState(oldState, newState);
+	if(!isInState && newInState)
+	    enteredRunState(oldState, newState);
+	
+	isInState = newInState;
+    }//end propertyChange(...)
+}//end RunStateHandler

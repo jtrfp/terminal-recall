bccf82837dc787d5ce9e2ba523bac4d47c002cd3
Chuck Ritola <cjritola@gmail.com>
2020-12-02 23:50:52 -0500

    ⟴GodModeCheatFactory

---
+++ src/main/java/org/jtrfp/trcl/game/cht/GodModeCheatFactory.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2020 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.game.cht;
+
+import org.jtrfp.trcl.UpfrontDisplay;
+import org.jtrfp.trcl.beh.DamageableBehavior;
+import org.jtrfp.trcl.core.Feature;
+import org.jtrfp.trcl.core.Features;
+import org.jtrfp.trcl.core.TRFactory.TR;
+import org.jtrfp.trcl.ext.tr.SoundSystemFactory.SoundSystemFeature;
+import org.jtrfp.trcl.obj.Player;
+import org.jtrfp.trcl.snd.SoundSystem;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GodModeCheatFactory extends AbstractCheatFactory {
+    private static final String CHEAT_NAME = "Invincibility (Toggle)";
+
+    protected GodModeCheatFactory() {
+	super(CHEAT_NAME);
+    }
+
+    @Override
+    public Class<? extends Feature<?>> getFeatureClass() {
+	return GodModeCheat.class;
+    }
+    
+    public class GodModeCheat extends AbstractCheatItem {
+	
+	public GodModeCheat() {super();}
+
+	@Override
+	protected void invokeCheat() {
+	    System.out.println(CHEAT_NAME+" cheat invoked.");
+	    final UpfrontDisplay disp = getTarget().getUpfrontDisplay();
+	    
+	    final Player player = getTarget().getPlayer();
+	    DamageableBehavior db = player.probeForBehavior(DamageableBehavior.class);
+	    
+	    //Ding
+	    final TR tr = getTarget().getTr();
+	    final SoundSystemFeature ssf = Features.get(tr, SoundSystemFeature.class);
+	    ssf.enqueuePlaybackEvent(ssf.getPlaybackFactory().create(tr.getResourceManager().soundTextures.get("POWER-1.WAV"), SoundSystem.DEFAULT_SFX_VOLUME_STEREO));
+	    
+	    //Toggle
+	    db.setEnable(!db.isEnabled());
+	    //Inform
+	    disp.submitMomentaryUpfrontMessage("Invincibility "+(db.isEnabled()?"Off":"On"));
+	}//end invokeCheat()
+	
+    }//end AllAmmoCheat
+
+}//end AllAmmoCheatFactory

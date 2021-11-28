a2bb62e42edfad89643024d212f9e40444d78232
Chuck Ritola <cjritola@gmail.com>
2021-11-28 04:46:12 -0500

    ⟴Javadoc for GraphStabilizationListener.

--- src/main/java/org/jtrfp/trcl/core/GraphStabilizationListener.java
+++ src/main/java/org/jtrfp/trcl/core/GraphStabilizationListener.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * This file is part of TERMINAL RECALL
- * Copyright (c) 2017 Chuck Ritola
+ * Copyright (c) 2021 Chuck Ritola
  * Part of the jTRFP.org project
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
@@ -13,6 +13,16 @@
 
 package org.jtrfp.trcl.core;
 
+/**
+ * If a Feature probes for other Features without knowing exactly what it is looking 
+ * for, such as getAllFeaturesOf(...), it may not get all available Features if called early.
+ * GraphStabilizationListeners are notified when the Feature graph in which they are
+ * present becomes stabilized, i.e. when all Features are present in the tree.<br><br>
+ * Calling Features.get() only assures that the subfeatures of the specified feature are
+ * present in the graph, but not its peer features.
+ * @author Chuck Ritola
+ *
+ */
 public interface GraphStabilizationListener {
  public void graphStabilized(Object target);
 }//end GraphStabilizationListener

1ff7267c8744bea8127a47fa466276485b0cc0e0
Chuck Ritola <cjritola@gmail.com>
2021-11-30 17:12:53 -0500

    ✔Tunnel iris slightly less forgiving.

--- src/main/java/org/jtrfp/trcl/beh/tun/IrisBehavior.java
+++ src/main/java/org/jtrfp/trcl/beh/tun/IrisBehavior.java
@@ -23,8 +23,8 @@ import org.jtrfp.trcl.obj.WorldObject;
 
 public class IrisBehavior extends Behavior implements CollisionBehavior {
 public static final int     DAMAGE_ON_IMPACT=6554;
-private static final double X_FLUFF         =20000;
-private static final double RADIUS_SKEW     =1.75;
+private static final double X_FLUFF         =10000;
+private static final double RADIUS_SKEW     =1.5;//1 = fair, 1+ =wider, 0-1 =narrower,  0 =wall (how could you?!)
 private final Controller controller;
 private final double     maxRadius;
 
@@ -34,16 +34,17 @@ private final double     maxRadius;
 	this.maxRadius =maxRadius;
     }//end constructor
     @Override
-    public void proposeCollision(WorldObject wo){
+    public void proposeCollision(WorldObject wo){// 0=closed, 1=open 2=closed
 	if(wo instanceof Player){
 	    final WorldObject p = getParent();
 	    final double [] thisPos = p.getPosition();
 	    final double [] playerPos = wo.getPosition();
+	    
 	    if(playerPos[0]>thisPos[0]&&playerPos[0]<thisPos[0]+X_FLUFF){
 		final double dY=thisPos[1]-playerPos[1];
 		final double dZ=thisPos[2]-playerPos[2];
 		final double dist=Math.sqrt(dY*dY+dZ*dZ);
-		final double currentRadius=maxRadius*(1.-Math.abs(1-controller.getCurrentFrame()))*RADIUS_SKEW;
+		final double currentRadius=maxRadius*(1.-Math.abs(1.-controller.getCurrentFrame()))*RADIUS_SKEW;
 		if(dist>currentRadius){
 		    wo.probeForBehaviors(new AbstractSubmitter<DamageableBehavior>(){
 			    @Override

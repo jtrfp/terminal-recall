d6199f06b2686462f72dcf8c630cb1e40c9dadeb
Chuck Ritola <cjritola@gmail.com>
2021-12-20 03:30:30 -0500

    ⟴TRBeanUtils.camelCaseToSentence()

--- src/main/java/org/jtrfp/trcl/gui/TRBeanUtils.java
+++ src/main/java/org/jtrfp/trcl/gui/TRBeanUtils.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * This file is part of TERMINAL RECALL
- * Copyright (c) 2012-2014 Chuck Ritola
+ * Copyright (c) 2012-2021 Chuck Ritola
  * Part of the jTRFP.org project
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
@@ -21,10 +21,32 @@ import org.jtrfp.trcl.gpu.GLFrameBuffer;
 
 public class TRBeanUtils {
     public static PropertyEditor getDefaultPropertyEditor(Object o){
-	    try{final PropertyEditorSupport pe = (PropertyEditorSupport)PropertyEditorManager.findEditor(o.getClass());
-	    pe.setSource(o);
-	    return pe;}
+	try{final PropertyEditorSupport pe = (PropertyEditorSupport)PropertyEditorManager.findEditor(o.getClass());
+	pe.setSource(o);
+	return pe;}
 	catch(Exception e){e.printStackTrace();}
 	return null;
     }//end getDefaultPropertyEditor(...)
+
+    public static String camelCaseToSentence(String camelCase) {
+	String sentence = "";
+	final char [] camelChars = camelCase.toCharArray();
+	for( int i = 0; i < camelChars.length; i++ ) {
+	    boolean addSpace = ( Character.isUpperCase(camelChars[i]));
+
+	    if( i+1 < camelChars.length) {
+		if( Character.isUpperCase(camelChars[i+1]) || Character.isDigit(camelChars[i+1]) )
+		    addSpace = false;
+	    } else if( Character.isUpperCase(camelChars[i]) || Character.isDigit(camelChars[i]))
+		addSpace = false;
+	    if( i > 0 && Character.isLowerCase(camelChars[i-1]) && Character.isUpperCase(camelChars[i]))
+		addSpace = true;
+	    if( i == 0)
+		addSpace = false;
+	    if(addSpace)
+		sentence += " ";
+	    sentence += camelChars[i];
+	}//end for(chars)
+	return sentence;
+    }//end camelCaseToSentence()
 }//end TRBeanUtils

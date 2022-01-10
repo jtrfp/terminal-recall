2ac1697f7b4a19d24b3e1b5604330dd2c24545cb
Chuck Ritola <cjritola@gmail.com>
2022-01-09 22:40:52 -0500

    ⟴UIProvider

---
+++ src/main/java/org/jtrfp/trcl/conf/ui/UIProvider.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2022 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.conf.ui;
+
+import javax.swing.JComponent;
+
+public interface UIProvider {
+    public JComponent getUIComponent();
+}//end UIProvider

8a79b87f1996b965f297c534fcd8ada527a241bc
Chuck Ritola <cjritola@gmail.com>
2022-01-24 21:17:51 -0500

    ⎘MavenBuildInformation -> InformationAboutThisBuild

--- src/main/java/org/jtrfp/trcl/InformationAboutThisBuildFactory.java
+++ src/main/java/org/jtrfp/trcl/InformationAboutThisBuildFactory.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * This file is part of TERMINAL RECALL
- * Copyright (c) 2017 Chuck Ritola
+ * Copyright (c) 2017-2022 Chuck Ritola
  * Part of the jTRFP.org project
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
@@ -13,6 +13,8 @@
 
 package org.jtrfp.trcl;
 
+import org.jtrfp.trcl.conf.ui.ConfigByUI;
+import org.jtrfp.trcl.conf.ui.ReadOnlyStringUI;
 import org.jtrfp.trcl.core.Feature;
 import org.jtrfp.trcl.core.FeatureFactory;
 import org.jtrfp.trcl.core.FeatureNotApplicableException;
@@ -20,13 +22,13 @@ import org.jtrfp.trcl.core.Features;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MavenBuildInformationFactory
+public class InformationAboutThisBuildFactory
 	implements FeatureFactory<Features> {
     
     @Override
     public Feature<Features> newInstance(Features target)
 	    throws FeatureNotApplicableException {
-	return new BuildInformationFeature();
+	return new InformationAboutThisBuild();
     }
 
     @Override
@@ -35,11 +37,11 @@ public class MavenBuildInformationFactory
     }
 
     @Override
-    public Class<? extends Feature> getFeatureClass() {
-	return BuildInformationFeature.class;
+    public Class<? extends Feature<?>> getFeatureClass() {
+	return InformationAboutThisBuild.class;
     }
     
-    public static class BuildInformationFeature extends MavenBuildInformation implements Feature<Features> {
+    public static class InformationAboutThisBuild extends MavenBuildInformation implements Feature<Features> {
 	
 	@Override
 	public void apply(Features target) {
@@ -49,6 +51,16 @@ public class MavenBuildInformationFactory
 	@Override
 	public void destruct(Features target) {
 	    //Do nothing
+	}
+	
+	@ConfigByUI(editorClass=ReadOnlyStringUI.class)
+	public String getBuildId() {
+	    return super.getUniqueBuildId();
+	}
+	
+	@ConfigByUI(editorClass=ReadOnlyStringUI.class)
+	public String getBranch() {
+	    return super.getBranch();
 	}
     }//end MavenBuildInformationFeature
 

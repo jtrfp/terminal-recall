fc263638d952c2bceeaaa9e89fd773ba4277b04e
Chuck Ritola <cjritola@gmail.com>
2022-03-01 04:16:58 -0500

    ⟴IntegerFormat.

---
+++ src/main/java/org/jtrfp/trcl/conf/ui/IntegerFormat.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2022 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.conf.ui;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface IntegerFormat {
+    int min() default Integer.MIN_VALUE;
+    int max() default Integer.MAX_VALUE;
+    int stepSize() default 1;
+}//end IntegerFormat

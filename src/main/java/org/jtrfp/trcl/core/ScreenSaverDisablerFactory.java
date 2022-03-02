53f2edf4c7afac32dc664ac6d55a56cc9fa3968f
Chuck Ritola <cjritola@gmail.com>
2022-03-02 06:17:47 -0500

    ⟴GUI config to ScreenSaverDisabler.

--- src/main/java/org/jtrfp/trcl/core/ScreenSaverDisablerFactory.java
+++ src/main/java/org/jtrfp/trcl/core/ScreenSaverDisablerFactory.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * This file is part of TERMINAL RECALL
- * Copyright (c) 2016 Chuck Ritola and contributors.
+ * Copyright (c) 2016-2022 Chuck Ritola and contributors.
  * Part of the jTRFP.org project
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
@@ -16,11 +16,48 @@ package org.jtrfp.trcl.core;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.jtrfp.trcl.conf.ui.CheckboxUI;
+import org.jtrfp.trcl.conf.ui.ComboBoxUI;
+import org.jtrfp.trcl.conf.ui.ConfigByUI;
+import org.jtrfp.trcl.conf.ui.IntegerFormat;
+import org.jtrfp.trcl.conf.ui.IntegerSpinnerUI;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScreenSaverDisablerFactory implements FeatureFactory<Features> {
+    //private static final Map<String, Integer> KEY_EVENTS;
+    private static final DefaultMutableTreeNode KEY_EVENT_ROOT;
+    private static final DefaultMutableTreeNode KEY_EVENT_DEFAULT;
+    static {
+	final int MODS = Modifier.FINAL | Modifier.PUBLIC | Modifier.STATIC;
+	final HashMap<String, Integer> result = new HashMap<>();
+	final Field [] fields = KeyEvent.class.getFields();
+
+	KEY_EVENT_ROOT = new DefaultMutableTreeNode();
+	DefaultMutableTreeNode defaultKey = null;
+
+	for(Field field : fields) {
+	    if(((field.getModifiers() & MODS) != 0 && field.getName().startsWith("VK_"))) {
+		try {
+		    final int val = field.getInt(null);
+		    final String key = field.getName().substring(3);
+		    result.put(key, val);
+		    final DefaultMutableTreeNode node = new DefaultMutableTreeNode(key); 
+		    KEY_EVENT_ROOT.add(node);
+		    if( key.contentEquals("SHIFT"))
+			defaultKey = node;
+		} catch(IllegalAccessException e) {e.printStackTrace();}
+	    }//end if(matches)
+	}//end for(fields)
+	KEY_EVENT_DEFAULT = defaultKey;
+	//KEY_EVENTS = Collections.unmodifiableMap(result);
+    }//end static
 
     @Override
     public Feature<Features> newInstance(Features target) {
@@ -33,12 +70,29 @@ public class ScreenSaverDisablerFactory implements FeatureFactory<Features> {
     }
 
     @Override
-    public Class<? extends Feature> getFeatureClass() {
+    public Class<? extends Feature<Features>> getFeatureClass() {
 	return ScreenSaverDisabler.class;
     }
     
-    private class ScreenSaverDisabler implements Feature<Features>{
+    public static class ScreenSaverDisabler implements Feature<Features>{
 	private volatile boolean running = true;
+	private volatile int keyIntervalInSeconds = 60;
+	private volatile DefaultMutableTreeNode keyEventToUse = KEY_EVENT_DEFAULT;
+	private volatile boolean enabled = true;
+	
+	public PackedTreeNode getPackedKeyEventToUse() {
+	    PackedTreeNode result = new PackedTreeNode();
+	    result.setNode(keyEventToUse);
+	    result.setRoot((DefaultMutableTreeNode)keyEventToUse.getRoot());
+	    return result;
+	}//end getPackedKeyEventToUse(...)
+	
+	public void setPackedKeyEventToUse(PackedTreeNode newEvent) {
+	    if( newEvent == null )
+		keyEventToUse = KEY_EVENT_DEFAULT;
+	    else
+		keyEventToUse = newEvent.getNode();
+	}//end setPakedKeyEventToUse(...)
 	
 	/**
 	 * Adapted from <a href='http://willcode4beer.com/tips.jsp?set=disable_screensaver'>This page</a>
@@ -47,14 +101,16 @@ public class ScreenSaverDisablerFactory implements FeatureFactory<Features> {
 	    @Override
 	    public void run(){
 		try{
-		    this.setName("SuspendScreenSaver");
+		    this.setName("ScreenSaverDisabler");
 		  final Robot robot = new Robot();
 		  while (running) {
-		    Thread.sleep(60 * 1000);
-		    robot.keyPress(KeyEvent.VK_SHIFT);
-		    Thread.sleep(50);
-		    robot.keyRelease(KeyEvent.VK_SHIFT);
-		  }
+		      Thread.sleep(getKeyIntervalInSeconds()*1000);
+		      if(isEnabled()) {
+			  robot.keyPress(KeyEvent.VK_SHIFT);
+			  Thread.sleep(5);
+			  robot.keyRelease(KeyEvent.VK_SHIFT);
+		      }//end if(enabled)
+		  }//end while(running)
 		}catch(InterruptedException e){}
 		catch(AWTException e){e.printStackTrace();}
 	    }//end run()
@@ -73,6 +129,45 @@ public class ScreenSaverDisablerFactory implements FeatureFactory<Features> {
 	public void destruct(Features target) {
 	    running = false;
 	    robotThread.interrupt();
+	}
+
+	@ConfigByUI(editorClass = IntegerSpinnerUI.class)
+	@IntegerFormat(min=30,max=3600,stepSize=30)
+	public int getKeyIntervalInSeconds() {
+	    return keyIntervalInSeconds;
+	}
+
+	public void setKeyIntervalInSeconds(int keyIntervalInSeconds) {
+	    this.keyIntervalInSeconds = keyIntervalInSeconds;
+	}
+
+	@ConfigByUI(editorClass = CheckboxUI.class)
+	public boolean isEnabled() {
+	    return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+	    this.enabled = enabled;
+	}
+
+	@ConfigByUI(editorClass = ComboBoxUI.class)
+	public DefaultMutableTreeNode getKeyEventToUse() {
+	    return keyEventToUse;
+	}
+
+	public void setKeyEventToUse(DefaultMutableTreeNode keyEventToUse) {
+	    this.keyEventToUse = keyEventToUse;
+	}
+	
+	public PackedTreeNode getKeyEventToUsePacked() {
+	    final PackedTreeNode result = new PackedTreeNode();
+	    result.setNode(keyEventToUse);
+	    result.setRoot((DefaultMutableTreeNode)(keyEventToUse.getRoot()));
+	    return result;
+	}
+	
+	public void setKeyEventToUsePacked(PackedTreeNode node) {
+	    keyEventToUse = node.getNode();
 	}
     }//ScreenSaverDisabler
 }//end ScreenSaverDisablerFactory

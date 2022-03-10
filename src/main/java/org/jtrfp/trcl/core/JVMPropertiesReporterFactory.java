c6e0a4b70160965846bdc70ae6a926a05d6dda86
Chuck Ritola <cjritola@gmail.com>
2022-03-10 00:41:36 -0500

    ⟴JVMPropertiesReporterFactory.

---
+++ src/main/java/org/jtrfp/trcl/core/JVMPropertiesReporterFactory.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * This file is part of TERMINAL RECALL
+ * Copyright (c) 2022 Chuck Ritola
+ * Part of the jTRFP.org project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     chuck - initial API and implementation
+ ******************************************************************************/
+
+package org.jtrfp.trcl.core;
+
+import org.jtrfp.trcl.gui.ReporterFactory.Reporter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JVMPropertiesReporterFactory implements FeatureFactory<Reporter> {
+    public static class JVMPropertiesReporter implements Feature<Reporter> {
+
+	public JVMPropertiesReporter() {}
+
+	@Override
+	public void apply(Reporter target) {
+	    System.getProperties().forEach(
+		    (path,value)->{target.report(JVMPropertiesReporter.class.getName()+"."+path.toString(), value.toString());
+		    });
+	}
+
+	@Override
+	public void destruct(Reporter target) {
+	    // TODO Auto-generated method stub
+
+	}
+
+    }//end JVMPropertiesReporter
+
+    @Override
+    public Feature<Reporter> newInstance(Reporter target)
+	    throws FeatureNotApplicableException {
+	return new JVMPropertiesReporter();
+    }
+
+    @Override
+    public Class<Reporter> getTargetClass() {
+	return Reporter.class;
+    }
+
+    @Override
+    public Class<? extends Feature> getFeatureClass() {
+	return JVMPropertiesReporter.class;
+    }
+}//end JVMPropertiesReporterFactory
